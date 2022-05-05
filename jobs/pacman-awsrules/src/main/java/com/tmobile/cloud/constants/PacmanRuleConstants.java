e15f1a7d480b01d55dc3372eb0bd081b4771b752
ranadheer-bolli <ranadheer.bolli@zemosolabs.com>
2022-05-05 12:38:50 +0530

    feature/Mvp-95/check-for-unrestricted-sqlDatabase-access

--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/constants/PacmanRuleConstants.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/constants/PacmanRuleConstants.java
@@ -361,4 +361,7 @@ public class PacmanRuleConstants {
 	public static final String DETAILS = "details";
 	public static final String AZURE_SUBSCRIPTION = "subscription";
     public static final String AZURE_SUBSCRIPTION_NAME = "subscriptionName";
+
+    public static final String START_IP_ADDRESS="startIPAddress";
+    public static final String FIREWALL_RULE_DETAILS="firewallRuleDetails";
 }
