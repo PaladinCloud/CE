18282ae512c6f35aaf02257fb8a511e921a8b3fb
ranadheer-b <103981914+ranadheer-b@users.noreply.github.com>
2022-05-06 13:30:41 +0530

    feature/Mvp-95/check-for-unrestricted-sqlDatabase-access (#8)
    * feature/Mvp-95/check-for-unrestricted-sqlDatabase-access
    
    * mvp-95-refactor():Refactored repeating logs to constant string and skipped validation for no resource data
    
    * mvp-95-refactor():Added 'RESOURCE_DATA_NOT_FOUND' String Constant in PacManRuleConstants file
    
    Co-authored-by: ranadheer-bolli <ranadheer.bolli@zemosolabs.com>

--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/constants/PacmanRuleConstants.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/constants/PacmanRuleConstants.java
@@ -361,6 +361,9 @@ public class PacmanRuleConstants {
     public static final String DETAILS = "details";
     public static final String AZURE_SUBSCRIPTION = "subscription";
     public static final String AZURE_SUBSCRIPTION_NAME = "subscriptionName";
+
+    public static final String START_IP_ADDRESS="startIPAddress";
+    public static final String FIREWALL_RULE_DETAILS="firewallRuleDetails";
     public static final String AZURE_INBOUNDARYSECURITYRULES = "inBoundSecurityRules";
     public static final String SECURITY_RULE_SOURCEADDRESSPREFIXES = "sourceAddressPrefixes";
     public static final String PROTOCOL = "protocol";
@@ -370,5 +373,7 @@ public class PacmanRuleConstants {
     public static final String INTERNET = "internet";
     public static final String ANY = "any";
     public static final String PROTOCOL_TCP = "TCP";
+    public static final String RESOURCE_DATA_NOT_FOUND = "Resource data not found!! Skipping this validation";
+
 
 }
