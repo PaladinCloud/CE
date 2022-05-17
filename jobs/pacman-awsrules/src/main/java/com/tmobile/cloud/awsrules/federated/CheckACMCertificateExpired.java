af652fefba0f2c303f4ec8c197b9db7deaf309e4
Arun Kotratil <arun.kotratil@paladincloud.io>
2022-05-17 09:57:14 +0000

    formated class

--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/awsrules/federated/CheckACMCertificateExpired.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/awsrules/federated/CheckACMCertificateExpired.java
@@ -20,13 +20,12 @@ import com.tmobile.pacman.commons.rule.PacmanRule;
 import com.tmobile.pacman.commons.rule.RuleResult;
 
 @PacmanRule(key = "check-for-expired-acm-certificate", desc = "This Rule look for the SSL(ACM) certificate expired", severity = PacmanSdkConstants.SEV_HIGH, category = PacmanSdkConstants.GOVERNANCE)
-public class CheckACMCertificateExpired extends BaseRule{
-
+public class CheckACMCertificateExpired extends BaseRule {
 
 	private static final Logger logger = LoggerFactory.getLogger(CheckACMCertificateExpired.class);
 
-	private static final String  STATUS_VALUE_EXP = "EXPIRED";
-	
+	private static final String STATUS_VALUE_EXP = "EXPIRED";
+
 	/**
 	 * The method will get triggered from Rule Engine with following parameters
 	 *
@@ -44,8 +43,7 @@ public class CheckACMCertificateExpired extends BaseRule{
 	 * @param resourceAttributes this is a resource in context which needs to be scanned this is provided by execution engine
 	 *
 	 */
-
-	public RuleResult execute(final Map<String, String> ruleParam,Map<String, String> resourceAttributes) {
+	public RuleResult execute(final Map<String, String> ruleParam, Map<String, String> resourceAttributes) {
 		logger.debug("========CheckACMCertificateExpired started=========");
 		Annotation annotation = null;
 		String status = resourceAttributes.get("status");
@@ -55,39 +53,33 @@ public class CheckACMCertificateExpired extends BaseRule{
 		MDC.put("executionId", ruleParam.get("executionId"));
 		MDC.put("ruleId", ruleParam.get(PacmanSdkConstants.RULE_ID));
 
-		List<LinkedHashMap<String,Object>>issueList = new ArrayList<>();
-		LinkedHashMap<String,Object>issue = new LinkedHashMap<>();
+		List<LinkedHashMap<String, Object>> issueList = new ArrayList<>();
+		LinkedHashMap<String, Object> issue = new LinkedHashMap<>();
 
-		if (!PacmanUtils.doesAllHaveValue(severity,category)) {
+		if (!PacmanUtils.doesAllHaveValue(severity, category)) {
 			logger.info(PacmanRuleConstants.MISSING_CONFIGURATION);
 			throw new InvalidInputException(PacmanRuleConstants.MISSING_CONFIGURATION);
 		}
 		if (status != null && STATUS_VALUE_EXP.equalsIgnoreCase(status)) {
-
-			
-					annotation = Annotation.buildAnnotation(ruleParam,Annotation.Type.ISSUE);
-					annotation.put(PacmanSdkConstants.DESCRIPTION,"SSL(ACM) Expired  found!!");
-					annotation.put(PacmanRuleConstants.SEVERITY, severity);
-					annotation.put(PacmanRuleConstants.CATEGORY, category);
-
-					issue.put(PacmanRuleConstants.VIOLATION_REASON, "SSL(ACM) Expired found!!");
-					issueList.add(issue);
-					annotation.put("issueDetails",issueList.toString());
-					logger.debug("========CheckACMCertificateExpired ended with annotation {} : =========",annotation);
-					return new RuleResult(PacmanSdkConstants.STATUS_FAILURE,PacmanRuleConstants.FAILURE_MESSAGE, annotation);
-			} else {
-				logger.info("SSL(ACM) certificate not expired");
-			}
+			annotation = Annotation.buildAnnotation(ruleParam, Annotation.Type.ISSUE);
+			annotation.put(PacmanSdkConstants.DESCRIPTION, "SSL(ACM) Expired  found!!");
+			annotation.put(PacmanRuleConstants.SEVERITY, severity);
+			annotation.put(PacmanRuleConstants.CATEGORY, category);
+			issue.put(PacmanRuleConstants.VIOLATION_REASON, "SSL(ACM) Expired found!!");
+			issueList.add(issue);
+			annotation.put("issueDetails", issueList.toString());
+			logger.debug("========CheckACMCertificateExpired ended with annotation {} : =========", annotation);
+			return new RuleResult(PacmanSdkConstants.STATUS_FAILURE, PacmanRuleConstants.FAILURE_MESSAGE, annotation);
+		} else {
+			logger.info("SSL(ACM) certificate not expired");
+		}
 
 		logger.debug("========CheckACMCertificateExpired ended=========");
-		return new RuleResult(PacmanSdkConstants.STATUS_SUCCESS,PacmanRuleConstants.SUCCESS_MESSAGE);
+		return new RuleResult(PacmanSdkConstants.STATUS_SUCCESS, PacmanRuleConstants.SUCCESS_MESSAGE);
 	}
 
 	public String getHelpText() {
 		return "This Rule should look for the SSL(ACM) certificate expired";
 	}
-
-	
-
 
 }
