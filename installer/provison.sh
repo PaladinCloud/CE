25218619bca12a7f358a01167d3f547d1abd931a
Nithin R <nithin>
2022-05-20 16:29:13 +0530

    Update_Provisional_Shell_Script_with_all_dependencies

--- installer/provison.sh
+++ installer/provison.sh
@@ -6,100 +6,108 @@ sudo yum -y install git
 
 ## Install python3
 sudo yum -y update
-sudo yum -y install yum-utils
-sudo yum -y groupinstall development
-sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
-sudo yum -y install python3
-sudo yum -y install python-pip
-pip install virtualenv
-
+sudo amazon-linux-extras install epel -y
+sudo yum install python3-pip -y
 
 ## Install Terraform
 sudo yum -y install unzip
-wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip .
-unzip terraform_0.11.10_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.11.15/terraform_0.11.15_linux_amd64.zip
+sudo unzip terraform_0.11.15_linux_amd64.zip
 sudo mv terraform /usr/bin
 
 ## INSTALL NODEJS and dependencies
-sudo yum install -y gcc-c++ make
-# curl -sL https://rpm.nodesource.com/setup_8.x | sudo bash
-curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install curl -y
+sudo yum -y update
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum install -y nodejs
 sudo npm install -g yarn
 sudo npm install -g @angular/cli
-# sudo npm audit fix
-# npm install --save-dev @angular/cli@7.0.6
+sudo npm install -g bower
 
-## Install Maven
+
+## Install openjdk 
 sudo yum -y install java-1.8.0-openjdk
+
 ## iNSTALL MAVEN
 sudo yum install -y maven
 
 
 ## Install docker
-# sudo yum install -y epel-release
-sudo yum -y install amazon-linux-extras install docker
-sudo amazon-linux-extras install -y docker
+sudo yum -y docker
 sudo systemctl start docker
 
-
-## Install mysql
+# Install mysql
 sudo yum -y install mysql
 
-echo alias cdd=\"cd $(pwd)\" >> ~/.bashrc
-echo alias cdt=\"cd $(pwd)/data/terraform\" >> ~/.bashrc
-echo alias cdl=\"cd $(pwd)/log\" >> ~/.bashrc
-source ~/.bashrc
+#installing python packages
+sudo pip3 install -r requirements.txt
 
 
-## Install virtualenv
-mkdirs ~/envs/
-virtualenv ~/envs/pacbot_env --python=python3
-source ~/envs/pacbot_env/bin/activate
-echo source ~/envs/pacbot_env/bin/activate >> ~/.bashrc
-pip install -r requirements.txt
+# echo alias cdd=\"cd $(pwd)\" >> ~/.bashrc
+# echo alias cdt=\"cd $(pwd)/data/terraform\" >> ~/.bashrc
+# echo alias cdl=\"cd $(pwd)/log\" >> ~/.bashrc
+# source ~/.bashrc
+
+
+# ## Install virtualenv
+# mkdirs ~/envs/
+# virtualenv ~/envs/pacbot_env --python=python3
+# source ~/envs/pacbot_env/bin/activate
+# echo source ~/envs/pacbot_env/bin/activate >> ~/.bashrc
+# pip install -r requirements.txt
 
 
 ---------------------------------
 -----Ubuntu-----
 ---------------------------------
 
+#install openjdk
 sudo apt -y update
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt install -y openjdk-8-jdk
 sudo  update-java-alternatives --set openjdk-8-jdk
 
+#install maven
 sudo apt install -y maven
+#install docker
 sudo apt install -y docker
 sudo apt install -y docker.io
-sudo systemctl start docker
+
+# install python
 sudo apt install -y python3
-sudo apt install -y python3-venv
+sudo apt install -y python3-pip
+
+# install mysql
 sudo apt install -y mysql-client
 
+## Install Terraform
 sudo apt -y install unzip
-wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
-unzip terraform_0.11.10_linux_amd64.zip
+wget wget https://releases.hashicorp.com/terraform/0.11.15/terraform_0.11.15_linux_amd64.zip
+sudo unzip terraform_0.11.15_linux_amd64.zip
 sudo mv terraform /usr/bin
 
-echo alias cdd=\"cd $(pwd)\" >> ~/.bashrc
-echo alias cdt=\"cd $(pwd)/data/terraform\" >> ~/.bashrc
-echo alias cdl=\"cd $(pwd)/log\" >> ~/.bashrc
-source ~/.bashrc
-
+# INSTALL NODEJS and dependencies
 sudo apt install -y curl
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt -y update
 sudo apt install -y nodejs
-sudo apt install -y npm
 sudo npm install -g yarn
 sudo npm install -g @angular/cli
+sudo npm install -g bower
+
+#installing python packages
+sudo pip3 install -r requirements.txt
+
+# echo alias cdd=\"cd $(pwd)\" >> ~/.bashrc
+# echo alias cdt=\"cd $(pwd)/data/terraform\" >> ~/.bashrc
+# echo alias cdl=\"cd $(pwd)/log\" >> ~/.bashrc
+# source ~/.bashrc
 
 ## Install virtualenv
-mkdir ~/envs/
-python3 -m venv ~/envs/pacbot_env
-source ~/envs/pacbot_env/bin/activate
-echo source ~/envs/pacbot_env/bin/activate >> ~/.bashrc
-pip install -r requirements.txt
+# mkdir ~/envs/
+# python3 -m venv ~/envs/pacbot_env
+# source ~/envs/pacbot_env/bin/activate
+# echo source ~/envs/pacbot_env/bin/activate >> ~/.bashrc
+# pip install -r requirements.txt
 
