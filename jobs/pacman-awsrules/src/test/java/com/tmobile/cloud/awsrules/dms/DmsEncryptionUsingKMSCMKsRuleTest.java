817af6133e32a0357aa5ef77e057c9e7170af2e3
Amisha Vijayakumar <amisha.vijayakumar@paladincloud.io>
2022-06-08 07:50:42 +0000

    MVP-686-MVP-690-Dms-public-access-and-encryption

--- jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/dms/DmsEncryptionUsingKMSCMKsRuleTest.java
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/dms/DmsEncryptionUsingKMSCMKsRuleTest.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
-package com.tmobile.cloud.awsrules.documentdb;
+package com.tmobile.cloud.awsrules.dms;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.CoreMatchers.is;
@@ -38,10 +38,10 @@ import com.tmobile.pacman.commons.exception.RuleExecutionFailedExeption;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ PacmanUtils.class})
-public class DocumentDbEncryptionUsingKMSCMKsTest {
+public class DmsEncryptionUsingKMSCMKsRuleTest {
 
     @InjectMocks
-    DocumentDbEncryptionUsingKMSCMKs documentDbEncryptionUsingKMSCMKs;
+    DmsEncryptionUsingKMSCMKsRule dmsEncryptionUsingKMSCMKsRule;
  
     @Test
     public void executeTest() throws Exception {
@@ -49,34 +49,34 @@ public class DocumentDbEncryptionUsingKMSCMKsTest {
         when(PacmanUtils.doesAllHaveValue(anyString(),anyString(),anyString())).thenReturn(true);
         
         when(PacmanUtils.checkIfResourceEncryptedWithKmsCmks(anyString(), anyString(), anyString())).thenReturn(true);
-        assertThat(documentDbEncryptionUsingKMSCMKs.execute(getMapString("r_123 "),getMapString("r_123 ")), is(notNullValue()));
+        assertThat(dmsEncryptionUsingKMSCMKsRule.execute(getMapString("r_123 "),getMapString("r_123 ")), is(notNullValue()));
         
         when(PacmanUtils.checkIfResourceEncryptedWithKmsCmks(anyString(), anyString(), anyString())).thenReturn(false);
-        assertThat(documentDbEncryptionUsingKMSCMKs.execute(getMapString("r_123 "),getMapString("r_123 ")), is(notNullValue()));
+        assertThat(dmsEncryptionUsingKMSCMKsRule.execute(getMapString("r_123 "),getMapString("r_123 ")), is(notNullValue()));
         
         when(PacmanUtils.checkIfResourceEncryptedWithKmsCmks(anyString(), anyString(), anyString())).thenThrow(new Exception());
-        assertThatThrownBy(() -> documentDbEncryptionUsingKMSCMKs.execute(getMapString("r_123 "),getMapString("r_123 "))).isInstanceOf(RuleExecutionFailedExeption.class);
+        assertThatThrownBy(() -> dmsEncryptionUsingKMSCMKsRule.execute(getMapString("r_123 "),getMapString("r_123 "))).isInstanceOf(RuleExecutionFailedExeption.class);
         
         when(PacmanUtils.doesAllHaveValue(anyString(),anyString(),anyString())).thenReturn(false);
         assertThatThrownBy(
-                () -> documentDbEncryptionUsingKMSCMKs.execute(getMapString("r_123 "),getMapString("r_123 "))).isInstanceOf(InvalidInputException.class);
+                () -> dmsEncryptionUsingKMSCMKsRule.execute(getMapString("r_123 "),getMapString("r_123 "))).isInstanceOf(InvalidInputException.class);
         
     }
     
     @Test
     public void getHelpTextTest(){
-        assertThat(documentDbEncryptionUsingKMSCMKs.getHelpText(), is(notNullValue()));
+        assertThat(dmsEncryptionUsingKMSCMKsRule.getHelpText(), is(notNullValue()));
     }
     
     public static Map<String, String> getMapString(String passRuleResourceId) {
         Map<String, String> commonMap = new HashMap<>();
         commonMap.put("executionId", "1234");
         commonMap.put("_resourceid", passRuleResourceId);
-        commonMap.put("severity", "critical");
+        commonMap.put("severity", "high");
         commonMap.put("ruleCategory", "security");
         commonMap.put("accountid", "12345");
-        commonMap.put("ruleId", "Aws_DocumentDB_should_be_encrypted_with_kms_cmks_version-1_aws_kms_cmks_encryption_should_be_enabled_documentdb");
-        commonMap.put("policyId", "Aws_DocumentDB_should_be_encrypted_with_kms_cmks_version-1");
+        commonMap.put("ruleId", "Aws_dms_encryption_using_KMS_CMKs_version-1_aws_KMS_CMKs_Encryption_dms");
+        commonMap.put("policyId", "Aws_dms_encryption_using_KMS_CMKs_version-1");
         commonMap.put("policyVersion", "version-1");
         commonMap.put("storageencrypted", "true");
         commonMap.put("kmskeyid", "arn:aws:kms:us-east-2a:123456789012:key/8c5b2c63-b9bc-45a3-a87a-5513eEXAMPLE");
