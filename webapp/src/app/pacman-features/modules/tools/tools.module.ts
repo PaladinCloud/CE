94f3ba18fe2ad537c20ce411a724258b4a0ebf65
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-06-10 18:15:12 +0530

    feat:navigation and header changes

--- webapp/src/app/pacman-features/modules/tools/tools.module.ts
+++ webapp/src/app/pacman-features/modules/tools/tools.module.ts
@@ -3,29 +3,23 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); You may not use
  * this file except in compliance with the License. A copy of the License is located at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * or in the "license" file accompanying this file. This file is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or
  * implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SharedModule } from '../../../shared/shared.module';
-import { ToolsComponent } from './tools.component';
-import { ToolsLandingPageComponent } from './tools-landing-page/tools-landing-page.component';
-import { ToolsRoutingModule } from './tools-routing.module';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SharedModule } from "../../../shared/shared.module";
+import { ToolsLandingPageComponent } from "./tools-landing-page/tools-landing-page.component";
+import { ToolsRoutingModule } from "./tools-routing.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    SharedModule,
-    ToolsRoutingModule
-  ],
-  declarations: [ToolsComponent, ToolsLandingPageComponent]
+  imports: [CommonModule, SharedModule, ToolsRoutingModule],
+  declarations: [ToolsLandingPageComponent],
 })
-
-export class ToolsModule { }
+export class ToolsModule {}
--- webapp/src/app/post-login-app/default-asset-group/default-asset-group.component.ts
+++ webapp/src/app/post-login-app/default-asset-group/default-asset-group.component.ts
@@ -12,56 +12,79 @@
  * limitations under the License.
  */
 
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
-import { DataCacheService } from '../../core/services/data-cache.service';
-import { AssetGroupObservableService } from '../../core/services/asset-group-observable.service';
-import { LoggerService } from '../../shared/services/logger.service';
-import { AssetTilesService } from '../../core/services/asset-tiles.service';
-import { environment } from './../../../environments/environment';
-import { DomainTypeObservableService } from '../../core/services/domain-type-observable.service';
-import {ActivatedRoute, NavigationExtras, Router } from '@angular/router';
-import {RouterUtilityService} from '../../shared/services/router-utility.service';
-import {Subscription} from 'rxjs/Subscription';
-import {WorkflowService} from '../../core/services/workflow.service';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnDestroy,
+  OnChanges,
+  SimpleChanges,
+} from "@angular/core";
+import { DataCacheService } from "../../core/services/data-cache.service";
+import { AssetGroupObservableService } from "../../core/services/asset-group-observable.service";
+import { LoggerService } from "../../shared/services/logger.service";
+import { AssetTilesService } from "../../core/services/asset-tiles.service";
+import { environment } from "./../../../environments/environment";
+import { DomainTypeObservableService } from "../../core/services/domain-type-observable.service";
+import { ActivatedRoute, NavigationExtras, Router } from "@angular/router";
+import { RouterUtilityService } from "../../shared/services/router-utility.service";
+import { Subscription } from "rxjs/Subscription";
+import { WorkflowService } from "../../core/services/workflow.service";
+import { FetchResourcesService } from "../../pacman-features/services/fetch-resources.service";
 
 @Component({
-  selector: 'app-default-asset-group',
-  templateUrl: './default-asset-group.component.html',
-  styleUrls: ['./default-asset-group.component.css'],
-  providers: [DataCacheService]
+  selector: "app-default-asset-group",
+  templateUrl: "./default-asset-group.component.html",
+  styleUrls: ["./default-asset-group.component.css"],
+  providers: [DataCacheService, FetchResourcesService],
 })
-
 export class DefaultAssetGroupComponent implements OnInit, OnDestroy {
-
-  constructor(private dataStore: DataCacheService,
-              private assetGroupObservableService: AssetGroupObservableService,
-              private logger: LoggerService,
-              private assetTileService: AssetTilesService,
-              private domainTypeObservableService: DomainTypeObservableService,
-              private router: Router,
-              private activatedRoute: ActivatedRoute,
-              private routingUtilityService: RouterUtilityService,
-              private workflowService: WorkflowService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private fetchResourcesService: FetchResourcesService,
+    private dataStore: DataCacheService,
+    private assetGroupObservableService: AssetGroupObservableService,
+    private logger: LoggerService,
+    private assetTileService: AssetTilesService,
+    private domainTypeObservableService: DomainTypeObservableService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private routingUtilityService: RouterUtilityService,
+    private workflowService: WorkflowService
+  ) {}
 
   clicked = false;
 
+  assetCount;
   @Input() defaultAssetGroup: string;
   @Input() provider = [];
   @Input() dataLoaded;
   private assetGroupSubscription: Subscription;
   private domainSubscription: Subscription;
 
-  public assetGroupDisplayName = '';
+  public assetGroupDisplayName = "";
   private assetGroupName;
   private assetGroupList;
   private domainName: string;
+  private agAndDomain; //: { ag: string; domain: string };
   ngOnInit() {
     try {
       this.subscribeToAssetGroupChange();
       this.subscribeToDomainChange();
-
+      this.route.queryParams.subscribe((params) => {
+        this.agAndDomain = params;
+        this.fetchResourcesService
+          .getResourceTypesAndCount(this.agAndDomain)
+          .subscribe((results) => {
+            try {
+              this.assetCount = results[1].totalassets;
+            } catch (error) {
+              console.log(error);
+            }
+          });
+      });
     } catch (error) {
-      this.logger.log('error', error);
+      this.logger.log("error", error);
     }
   }
 
@@ -74,56 +97,59 @@ export class DefaultAssetGroupComponent implements OnInit, OnDestroy {
   }
 
   subscribeToAssetGroupChange() {
-
     this.assetGroupName = this.dataStore.getCurrentSelectedAssetGroup();
     if (this.assetGroupName) {
       const navigationExtras: NavigationExtras = {
         queryParams: {
-          'ag': this.assetGroupName
+          ag: this.assetGroupName,
         },
-        queryParamsHandling: 'merge'
+        queryParamsHandling: "merge",
       };
       this.router.navigate([], navigationExtras);
     }
 
-    this.assetGroupSubscription = this.assetGroupObservableService.getAssetGroup().subscribe(assGroupName => {
-      if (assGroupName) {
-        this.assetGroupName = assGroupName;
-        this.getAssetGroupDisplayName(this.assetGroupName);
-      }
-    });
+    this.assetGroupSubscription = this.assetGroupObservableService
+      .getAssetGroup()
+      .subscribe((assGroupName) => {
+        if (assGroupName) {
+          this.assetGroupName = assGroupName;
+          this.getAssetGroupDisplayName(this.assetGroupName);
+        }
+      });
   }
 
   subscribeToDomainChange() {
-    this.domainSubscription = this.domainTypeObservableService.getDomainType().subscribe(domain => {
-      this.domainName = domain;
-    });
+    this.domainSubscription = this.domainTypeObservableService
+      .getDomainType()
+      .subscribe((domain) => {
+        this.domainName = domain;
+      });
   }
 
   getAssetGroupDisplayName(assetGroupName) {
-
     /* To check if asset group list is available in cache */
     const assetGroupList = this.dataStore.getListOfAssetGroups();
 
     if (!assetGroupList) {
-
       /* Asset group list not available, getting the list */
 
       const assetUrl = environment.assetTiles.url;
       const assetMethod = environment.assetTiles.method;
 
       this.assetTileService.getAssetTiles(assetUrl, assetMethod).subscribe(
-          response => {
-            this.assetGroupList = response[0];
-            if (this.assetGroupList) {
-              /* Store the list in stringify format */
-              this.dataStore.setListOfAssetGroups(JSON.stringify(this.assetGroupList));
-              this.fetchAssetGroupDisplayName(assetGroupName);
-            }
-          },
-          error => {
-            this.logger.log('error', error);
+        (response) => {
+          this.assetGroupList = response[0];
+          if (this.assetGroupList) {
+            /* Store the list in stringify format */
+            this.dataStore.setListOfAssetGroups(
+              JSON.stringify(this.assetGroupList)
+            );
+            this.fetchAssetGroupDisplayName(assetGroupName);
           }
+        },
+        (error) => {
+          this.logger.log("error", error);
+        }
       );
     } else {
       /* If list is availbe then get asset group display name, domain list from a matchign asset group name */
@@ -135,7 +161,7 @@ export class DefaultAssetGroupComponent implements OnInit, OnDestroy {
   fetchAssetGroupDisplayName(assetGroupName) {
     if (this.assetGroupList) {
       let allAssetGroups;
-      if (typeof(this.assetGroupList) === 'string') {
+      if (typeof this.assetGroupList === "string") {
         allAssetGroups = JSON.parse(this.assetGroupList);
       } else {
         allAssetGroups = this.assetGroupList;
@@ -143,7 +169,9 @@ export class DefaultAssetGroupComponent implements OnInit, OnDestroy {
 
       let isAgPresent = false;
 
-      const filteredArray = allAssetGroups.filter(element => element.name === assetGroupName);
+      const filteredArray = allAssetGroups.filter(
+        (element) => element.name === assetGroupName
+      );
 
       if (filteredArray.length) {
         isAgPresent = true;
@@ -153,34 +181,54 @@ export class DefaultAssetGroupComponent implements OnInit, OnDestroy {
       }
 
       if (!isAgPresent) {
-        this.router.navigate(['/pl', {outlets: { modal: ['change-default-asset-group'] } }]);
+        this.router.navigate([
+          "/pl",
+          { outlets: { modal: ["change-default-asset-group"] } },
+        ]);
       }
     }
   }
 
   getUpdatedDomain(assetGroupName, assetGroupObject) {
+    if (assetGroupObject.domains && assetGroupObject.domains.length > 0) {
+      this.domainTypeObservableService.updateListOfDomains(
+        assetGroupObject.domains.join("~")
+      );
 
-    if (assetGroupObject.domains && assetGroupObject.domains.length > 0 ) {
-      this.domainTypeObservableService.updateListOfDomains(assetGroupObject.domains.join('~'));
+      const newDomain = this.dataStore.getCurrentSelectedDomain(assetGroupName)
+        ? this.dataStore.getCurrentSelectedDomain(assetGroupName)
+        : assetGroupObject.domains[0];
 
-      const newDomain = this.dataStore.getCurrentSelectedDomain(assetGroupName) ? this.dataStore.getCurrentSelectedDomain(assetGroupName) : assetGroupObject.domains[0];
+      const deepestUrl = this.routingUtilityService.getJointDeepestPageUrl(
+        this.activatedRoute.snapshot
+      );
 
-      const deepestUrl = this.routingUtilityService.getJointDeepestPageUrl(this.activatedRoute.snapshot);
+      const isActiveUrlACommonModulePage =
+        this.routingUtilityService.checkIfCurrentRouteBelongsToCommonPages(
+          deepestUrl
+        );
 
-      const isActiveUrlACommonModulePage = this.routingUtilityService.checkIfCurrentRouteBelongsToCommonPages(deepestUrl);
+      const currentModule =
+        this.routingUtilityService.getModuleNameFromCurrentRoute(
+          this.activatedRoute.snapshot
+        );
 
-      const currentModule = this.routingUtilityService.getModuleNameFromCurrentRoute(this.activatedRoute.snapshot);
-
-      const landingPageUrl = this.routingUtilityService.getLandingPageInAModule(currentModule);
+      const landingPageUrl =
+        this.routingUtilityService.getLandingPageInAModule(currentModule);
 
       const navigationExtras: NavigationExtras = {
         queryParams: {
-          'domain': newDomain
+          domain: newDomain,
         },
-        queryParamsHandling: 'merge'
+        queryParamsHandling: "merge",
       };
 
-      if (!isActiveUrlACommonModulePage && this.domainName && landingPageUrl && newDomain !== this.domainName) {
+      if (
+        !isActiveUrlACommonModulePage &&
+        this.domainName &&
+        landingPageUrl &&
+        newDomain !== this.domainName
+      ) {
         /* Clears the saved url from module as different module may not have that page */
         this.workflowService.clearDataOfOpenedPageInModule();
         this.router.navigate([landingPageUrl], navigationExtras);
@@ -188,12 +236,12 @@ export class DefaultAssetGroupComponent implements OnInit, OnDestroy {
         this.router.navigate([], navigationExtras);
       }
     } else {
-      this.domainTypeObservableService.updateListOfDomains('');
+      this.domainTypeObservableService.updateListOfDomains("");
       const navigationExtras: NavigationExtras = {
         queryParams: {
-          'domain': ''
+          domain: "",
         },
-        queryParamsHandling: 'merge'
+        queryParamsHandling: "merge",
       };
 
       this.router.navigate([], navigationExtras);
@@ -202,10 +250,14 @@ export class DefaultAssetGroupComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     try {
-      if (this.assetGroupSubscription) { this.assetGroupSubscription.unsubscribe(); }
-      if (this.assetGroupSubscription) { this.domainSubscription.unsubscribe(); }
+      if (this.assetGroupSubscription) {
+        this.assetGroupSubscription.unsubscribe();
+      }
+      if (this.assetGroupSubscription) {
+        this.domainSubscription.unsubscribe();
+      }
     } catch (error) {
-      this.logger.log('error', error);
+      this.logger.log("error", error);
     }
   }
 }
