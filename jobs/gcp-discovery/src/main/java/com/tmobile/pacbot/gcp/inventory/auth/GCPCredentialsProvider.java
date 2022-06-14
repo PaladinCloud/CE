2ac109248d7ebd2dcf184f8ab4ba34aa3259df9f
anjali <nakerakanti.anjali@gmail.com>
2022-06-14 20:13:47 +0530

    written collector for firewall
    added rule to ensure vpc firewall RDP port is not accesible publically
    
    added rule to ensure vpc firewall SSH port is not accesible publically
    
    added rule to ensure vpc firewall DNS port is not accesible publically
    
    added test cases

--- jobs/gcp-discovery/src/main/java/com/tmobile/pacbot/gcp/inventory/auth/GCPCredentialsProvider.java
+++ jobs/gcp-discovery/src/main/java/com/tmobile/pacbot/gcp/inventory/auth/GCPCredentialsProvider.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.util.Value;
 import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.cloud.compute.v1.FirewallsClient;
+import com.google.cloud.compute.v1.FirewallsSettings;
 import com.google.cloud.compute.v1.InstancesClient;
 import com.google.cloud.compute.v1.InstancesSettings;
 import com.google.common.collect.Lists;
@@ -27,14 +29,17 @@ public class GCPCredentialsProvider {
     private static final Logger logger = LoggerFactory.getLogger(GCPCredentialsProvider.class);
 
     private InstancesClient instancesClient;
+    private FirewallsClient firewallsClient;
 
-    // If you don't specify credentials when constructing the client, the client library will
-    // look for credentials via the environment variable GOOGLE_APPLICATION_CREDENTIALS.
-
+    // If you don't specify credentials when constructing the client, the client
+    // library will
+    // look for credentials via the environment variable
+    // GOOGLE_APPLICATION_CREDENTIALS.
 
     private GoogleCredentials getCredentials() throws IOException {
         // You can specify a credential file by providing a path to GoogleCredentials.
-        // Otherwise, credentials are read from the GOOGLE_APPLICATION_CREDENTIALS environment variable.
+        // Otherwise, credentials are read from the GOOGLE_APPLICATION_CREDENTIALS
+        // environment variable.
 
         // print the path to the credential file
         String cred = System.getProperty("gcp.credentials");
@@ -50,13 +55,23 @@ public class GCPCredentialsProvider {
     public InstancesClient getInstancesClient() throws IOException {
         if (instancesClient == null) {
             // pass authentication credentials to the client
-            InstancesSettings instancesSettings =
-                    InstancesSettings.newBuilder()
-                            .setCredentialsProvider(FixedCredentialsProvider.create(this.getCredentials()))
-                            .build();
+            InstancesSettings instancesSettings = InstancesSettings.newBuilder()
+                    .setCredentialsProvider(FixedCredentialsProvider.create(this.getCredentials()))
+                    .build();
             instancesClient = InstancesClient.create(instancesSettings);
         }
         return instancesClient;
+    }
+
+    public FirewallsClient getFirewallsClient() throws IOException {
+        if (firewallsClient == null) {
+            // pass authentication credentials to the client
+            FirewallsSettings firewallsSettings = FirewallsSettings.newBuilder()
+                    .setCredentialsProvider(FixedCredentialsProvider.create(this.getCredentials()))
+                    .build();
+            firewallsClient = FirewallsClient.create(firewallsSettings);
+        }
+        return firewallsClient;
     }
 
     // close the client in destroy method
--- jobs/gcp-discovery/src/main/java/com/tmobile/pacbot/gcp/inventory/file/FileManager.java
+++ jobs/gcp-discovery/src/main/java/com/tmobile/pacbot/gcp/inventory/file/FileManager.java
@@ -1,9 +1,23 @@
+/*******************************************************************************
+ * Copyright 2018 T Mobile, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package com.tmobile.pacbot.gcp.inventory.file;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-
 import com.tmobile.pacbot.gcp.inventory.vo.*;;
 
 /**
@@ -21,24 +35,37 @@ public class FileManager {
     /**
      * Initialise.
      *
-     * @param folderName the folder name
-     * @throws IOException Signals that an I/O exception has occurred.
+     * @param folderName
+     *                   the folder name
+     * @throws IOException
+     *                     Signals that an I/O exception has occurred.
      */
     public static void initialise(String folderName) throws IOException {
         FileGenerator.folderName = folderName;
         new File(folderName).mkdirs();
 
         FileGenerator.writeToFile("gcp-vminstance.data", "[", false);
+        FileGenerator.writeToFile("gcp-vpcfirewall.data", "[", false);
+
     }
 
     public static void finalise() throws IOException {
 
         FileGenerator.writeToFile("gcp-vminstance.data", "]", true);
+        FileGenerator.writeToFile("gcp-vpcfirewall.data", "]", true);
+
     }
 
     public static void generateVMFiles(List<VirtualMachineVH> vmMap) throws IOException {
 
         FileGenerator.generateJson(vmMap, "gcp-vminstance.data");
+
+    }
+
+    public static void generateFireWallFiles(List<FireWallVH> vmMap) throws IOException {
+
+        FileGenerator.generateJson(vmMap, "gcp-vpcfirewall.data");
+
     }
 
 }
