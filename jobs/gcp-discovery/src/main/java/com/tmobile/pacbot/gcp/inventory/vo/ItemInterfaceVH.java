b5747122bb2426b293d412fc57826600f8b8720c
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2022-06-21 18:44:32 +0530

    feat:Two-factor Authentication for VM Instance

---
+++ jobs/gcp-discovery/src/main/java/com/tmobile/pacbot/gcp/inventory/vo/ItemInterfaceVH.java
@@ -0,0 +1,22 @@
+package com.tmobile.pacbot.gcp.inventory.vo;
+
+public class ItemInterfaceVH extends GCPVH{
+    private String key;
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
