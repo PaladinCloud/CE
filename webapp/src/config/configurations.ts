0d8ed69263189f0d62ff866f3d077f4b251c2af6
ranadheer-bolli <ranadheer.bolli@zemosolabs.com>
2022-06-21 18:54:42 +0530

    fix: Removed Domains and double quotes

--- webapp/src/config/configurations.ts
+++ webapp/src/config/configurations.ts
@@ -12,18 +12,18 @@
  * limitations under the License.
  */
 
-export const AZURE_SSO = "azuresso";
-const DB = "db";
-const LDAP = "ldap";
+export const AZURE_SSO = 'azuresso';
+const DB = 'db';
+const LDAP = 'ldap';
 
 export const CONFIGURATIONS = {
   required: {
-    APP_NAME: "PacBot",
+    APP_NAME: 'Paladin Cloud',
     domains: {
-      PROD_BASE_URL: "http://pacbot-548201713.us-east-1.elb.amazonaws.com/api", // Expected values: domain where the API is deployed, ex: http://beta.pacbot.com/api
-      STG_BASE_URL: "http://pacbot-548201713.us-east-1.elb.amazonaws.com/api", // Expected values: domain where the API is deployed, ex: http://stgbeta.pacbot.com/api
-      DEV_BASE_URL: "http://pacbot-548201713.us-east-1.elb.amazonaws.com/api", // Expected values: domain where the API is deployed, ex: http://devbeta.pacbot.com/api
-      CLOUD_BASE_URL: "", // Expected values: domain where the API is deployed
+      PROD_BASE_URL: '', // Expected values: domain where the API is deployed, ex: http://beta.pacbot.com/api
+      STG_BASE_URL: '', // Expected values: domain where the API is deployed, ex: http://stgbeta.pacbot.com/api
+      DEV_BASE_URL: '', // Expected values: domain where the API is deployed, ex: http://devbeta.pacbot.com/api
+      CLOUD_BASE_URL: '', // Expected values: domain where the API is deployed
     },
     featureModules: {
       COMPLIANCE_MODULE: true, // Expected values: true || false
@@ -37,31 +37,31 @@ export const CONFIGURATIONS = {
     auth: {
       AUTH_TYPE: DB, // AZURE_SSO | DB | LDAP
       adConfig: {
-        tenant: "", // Expected values: Value expected if 'AD_AUTHENTICATION' is true
-        clientId: "", // Expected values: Value expected if 'AD_AUTHENTICATION' is true
-      },
+        tenant: '', // Expected values: Value expected if 'AD_AUTHENTICATION' is true
+        clientId: '' // Expected values: Value expected if 'AD_AUTHENTICATION' is true
+      }
     },
     pacmanIssue: {
       CREATE_JIRA_TICKET_FOR_PACMAN_ISSUE: false, // Expected values: true || false || ''
       emailPacManIssue: {
-        ISSUE_MAIL_TEMPLATE_URL: "",
-        ISSUE_EMAIL_FROM_ID: "violations.support@pacbot.org",
-      },
+        ISSUE_MAIL_TEMPLATE_URL: '',
+        ISSUE_EMAIL_FROM_ID: 'violations.support@pacbot.org',
+      }
     },
     assetDetails: {
-      ASSET_DETAILS_TEMPLATE_URL: "",
-      ASSET_DETAILS_FROM_ID: "",
+      ASSET_DETAILS_TEMPLATE_URL: '',
+      ASSET_DETAILS_FROM_ID: '',
     },
     general: {
-      ACCESS_MANAGEMENT_PORTAL_URL: "",
+      ACCESS_MANAGEMENT_PORTAL_URL: '',
       e2e: {
-        DOMAIN: "http://localhost:4200",
-        EMAIL_ID: "",
-        NT_ID: "", // Add NT ID for e2e login
-        NT_PASSWORD: "", // Add respective password for e2e login
+        DOMAIN: 'http://localhost:4200',
+        EMAIL_ID: '',
+        NT_ID: '', // Add NT ID for e2e login
+        NT_PASSWORD: '' // Add respective password for e2e login
       },
       qualysEnabled: false,
-      OSS: true,
-    },
-  },
+      OSS: true
+    }
+  }
 };
