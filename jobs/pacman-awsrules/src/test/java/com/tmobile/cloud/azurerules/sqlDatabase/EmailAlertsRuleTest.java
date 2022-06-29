2f287a69a33e9e89a0a6408e9e389c01697e9adb
anjali <nakerakanti.anjali@gmail.com>
2022-06-29 11:33:16 +0530

    added test cases for azure polices

--- jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/azurerules/sqlDatabase/EmailAlertsRuleTest.java
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/azurerules/sqlDatabase/EmailAlertsRuleTest.java
@@ -4,11 +4,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.*;
+import static org.mockito.Matchers.anyInt;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.tmobile.cloud.awsrules.utils.RulesElasticSearchRepositoryUtil;
+import com.tmobile.pacman.commons.PacmanSdkConstants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -25,49 +30,56 @@ import com.tmobile.pacman.commons.rule.BaseRule;
 
 @PowerMockIgnore({"javax.net.ssl.*", "javax.management.*"})
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({PacmanUtils.class, BaseRule.class})
+@PrepareForTest({PacmanUtils.class, BaseRule.class,RulesElasticSearchRepositoryUtil.class})
 public class EmailAlertsRuleTest {
 
     @InjectMocks
     EmailAlertsRule emailAlertsRule;
 
+    public JsonObject getFailureJsonArrayForemailAlertsRule() {
+        Gson gson = new Gson();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("hits", gson.fromJson("{\n    \"hits\": [\n        {\n            \"_source\": {\n                \"discoverydate\": \"2022-06-28 06:00:00+0000\",\n                \"_cloudType\": \"Azure\",\n                \"subscription\": \"f4d319d8-7eac-4e15-a561-400f7744aa81\",\n                \"region\": \"centralus\",\n                \"subscriptionName\": \"dev-paladincloud\",\n                \"resourceGroupName\": \"dev-paladincloud\",\n                \"id\": \"subscriptions/f4d319d8-7eac-4e15-a561-400f7744aa81/resourceGroups/dev-paladincloud/providers/Microsoft.Network/networkSecurityGroups/testing-nsg\",\n                \"key\": \"ccb7e20e-47c3-478b-a960-580c7a6b9d1e\",\n                \"name\": \"testing-nsg\",\n                \"tags\": {},\n                \"passwordBasedAuthenticationDisabled\":\"false\",\n                \"firewallRuleDetails\": [\n                    {\n                        \"startIPAddress\": \"0.0.0.0\"\n                    }\n                ],\n\n\"notificationRecipientsEmails\":\"\",\n                \"excludedDetectionTypes\": [\n                    \"Access_Anomaly\",\n                    \"Data_Exfiltration\",\n                    \"Unsafe_Action\"\n                ]\n            }\n        }\n    ]\n}", JsonElement.class));
+        return jsonObject;
+    }
+    public  JsonObject getHitJsonArrayForemailAlertsRule() {
+        Gson gson = new Gson();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("hits", gson.fromJson("{\n    \"hits\": [\n        {\n            \"_source\": {\n                \"discoverydate\": \"2022-06-28 06:00:00+0000\",\n                \"_cloudType\": \"Azure\",\n                \"subscription\": \"f4d319d8-7eac-4e15-a561-400f7744aa81\",\n                \"region\": \"centralus\",\n                \"subscriptionName\": \"dev-paladincloud\",\n                \"resourceGroupName\": \"dev-paladincloud\",\n                \"id\": \"subscriptions/f4d319d8-7eac-4e15-a561-400f7744aa81/resourceGroups/dev-paladincloud/providers/Microsoft.Network/networkSecurityGroups/testing-nsg\",\n                \"key\": \"ccb7e20e-47c3-478b-a960-580c7a6b9d1e\",\n                \"name\": \"testing-nsg\",\n                \"tags\": {},\n                \"passwordBasedAuthenticationDisabled\":\"false\",\n                \"firewallRuleDetails\": [\n                    {\n                        \"startIPAddress\": \"0.0.0.0\"\n                    }\n                ],\n\n\"notificationRecipientsEmails\":\"sqlserver\",\n                \"excludedDetectionTypes\": [\n                    \"Access_Anomaly\",\n                    \"Data_Exfiltration\",\n                    \"Unsafe_Action\"\n                ]\n            }\n        }\n    ]\n}", JsonElement.class));
+
+
+        return jsonObject;
+    }
+
     @Test
-    public void executeTest() throws Exception {
+    public void executeSucessTest() throws Exception {
         mockStatic(PacmanUtils.class);
-        when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString(), anyString(), anyString(), anyString()))
+        mockStatic(RulesElasticSearchRepositoryUtil.class);
+        when(PacmanUtils.doesAllHaveValue(anyString(), anyString()))
                 .thenReturn(
                         true);
-        when(PacmanUtils.formatUrl(anyObject(), anyString())).thenReturn("host");
-        when(PacmanUtils.checkAccessibleToAll(anyObject(), anyString(), anyString(), anyString(), anyString(),
-                anyString())).thenReturn(CommonTestUtils.getMapBoolean("r_123 "));
+
+        when(RulesElasticSearchRepositoryUtil.getQueryDetailsFromES(anyString(), anyObject(), anyObject(),
+                anyObject(), anyObject(), anyInt(), anyObject(), anyObject(), anyObject())).thenReturn(getHitJsonArrayForemailAlertsRule());
         assertThat(emailAlertsRule.execute(CommonTestUtils.getMapString("r_123 "),
-                CommonTestUtils.getMapString("r_123 ")), is(notNullValue()));
+                CommonTestUtils.getMapString("r_123 ")).getStatus(), is(PacmanSdkConstants.STATUS_SUCCESS));
 
-        when(PacmanUtils.checkAccessibleToAll(anyObject(), anyString(), anyString(), anyString(), anyString(),
-                anyString())).thenReturn(CommonTestUtils.getEmptyMapBoolean("r_123 "));
-        assertThat(emailAlertsRule.execute(CommonTestUtils.getMapString("r_123 "),
-                CommonTestUtils.getMapString("r_123 ")), is(notNullValue()));
-
-        when(PacmanUtils.checkAccessibleToAll(anyObject(), anyString(), anyString(), anyString(), anyString(),
-                anyString())).thenThrow(new Exception());
-        assertThatThrownBy(
-                () -> emailAlertsRule.execute(CommonTestUtils.getMapString("r_123 "),
-                        CommonTestUtils.getMapString("r_123 ")))
-                .isInstanceOf(RuleExecutionFailedExeption.class);
-
-        assertThatThrownBy(
-                () -> emailAlertsRule.execute(CommonTestUtils.getOneMoreMapString("r_123 "),
-                        CommonTestUtils.getMapString("r_123 ")))
-                .isInstanceOf(RuleExecutionFailedExeption.class);
-
-        when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString(), anyString(), anyString(), anyString()))
-                .thenReturn(
-                        false);
-        assertThatThrownBy(
-                () -> emailAlertsRule.execute(CommonTestUtils.getMapString("r_123 "),
-                        CommonTestUtils.getMapString("r_123 ")))
-                .isInstanceOf(InvalidInputException.class);
     }
+    @Test
+    public void executeFailureTest() throws Exception {
+        mockStatic(PacmanUtils.class);
+        mockStatic(RulesElasticSearchRepositoryUtil.class);
+        when(PacmanUtils.doesAllHaveValue(anyString(), anyString()))
+                .thenReturn(
+                        true);
+        when(RulesElasticSearchRepositoryUtil.getQueryDetailsFromES(anyString(),anyObject(),
+                anyObject(),
+                anyObject(), anyObject(), anyInt(), anyObject(), anyObject(), anyObject())).thenReturn(getFailureJsonArrayForemailAlertsRule());
+        assertThat(emailAlertsRule.execute(CommonTestUtils.getMapString("r_123 "),
+                CommonTestUtils.getMapString("r_123 ")).getStatus(), is(PacmanSdkConstants.STATUS_FAILURE));
+
+    }
+
 
     @Test
     public void getHelpTextTest() {
