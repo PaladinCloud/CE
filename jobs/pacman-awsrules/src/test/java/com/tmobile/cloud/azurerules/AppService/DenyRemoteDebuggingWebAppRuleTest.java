9b39b7aebd300317c610d96aa125f420105c880a
kushZemoso <kushagra.jain@zemosolabs.com>
2022-06-29 19:48:50 +0530

    Unit Test for App Service

--- jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/azurerules/AppService/DenyRemoteDebuggingWebAppRuleTest.java
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/azurerules/AppService/DenyRemoteDebuggingWebAppRuleTest.java
@@ -1,9 +1,15 @@
 package com.tmobile.cloud.azurerules.AppService;
 
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.tmobile.cloud.awsrules.utils.CommonTestUtils;
 import com.tmobile.cloud.awsrules.utils.PacmanUtils;
+import com.tmobile.cloud.awsrules.utils.RulesElasticSearchRepositoryUtil;
 import com.tmobile.cloud.constants.PacmanRuleConstants;
 import com.tmobile.pacman.commons.PacmanSdkConstants;
+import com.tmobile.pacman.commons.rule.BaseRule;
 import com.tmobile.pacman.commons.rule.RuleResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,10 +25,17 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.*;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
-@PowerMockIgnore("jdk.internal.reflect.*")
+@PowerMockIgnore({"javax.net.ssl.*", "javax.management.*"})
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ PacmanUtils.class })
+@PrepareForTest({PacmanUtils.class, BaseRule.class})
+@PrepareForTest({PacmanUtils.class, BaseRule.class, RulesElasticSearchRepositoryUtil.class})
+
 public class DenyRemoteDebuggingWebAppRuleTest {
     @InjectMocks
     DenyRemoteDebuggingWebAppRule denyRemoteDebuggingWebAppRule;
@@ -31,6 +44,45 @@ public class DenyRemoteDebuggingWebAppRuleTest {
     public void getHelpTextTest() {
         assertThat(denyRemoteDebuggingWebAppRule.getHelpText(), is(notNullValue()));
     }
+
+    public JsonObject getFailureJsonArrayForRemoteDebugging(){
+        Gson gson=new Gson();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("hits", gson.fromJson("{\n    \"hits\": [\n        {\n            \"_source\": {\n                \"discoverydate\": \"2022-06-28 06:00:00+0000\",\n                \"_cloudType\": \"Azure\",\n                \"subscription\": \"f4d319d8-7eac-4e15-a561-400f7744aa81\",\n                \"region\": \"centralus\",\n                \"subscriptionName\": \"dev-paladincloud\",\n                \"resourceGroupName\": \"dev-paladincloud\",\n                \"id\": \"subscriptions/f4d319d8-7eac-4e15-a561-400f7744aa81/resourceGroups/dev-paladincloud/providers/Microsoft.Network/networkSecurityGroups/testing-nsg\",\n                \"key\": \"ccb7e20e-47c3-478b-a960-580c7a6b9d1e\",\n                \"name\": \"testing-nsg\",\n                \"tags\": {},\n                \"excludedDetectionTypes\": [\n                    \"Access_Anomaly\",\n                    \"Data_Exfiltration\",\n                    \"Unsafe_Action\"\n                ]\n            }\n        }\n    ]\n}", JsonElement.class));
+        return jsonObject;
+    }
+    public  JsonObject getHitJsonArrayForRemoteDebugging() {
+        Gson gson = new Gson();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("hits", gson.fromJson("{\n    \"hits\": [\n        {\n            \"_source\": {\n                \"discoverydate\": \"2022-06-28 06:00:00+0000\",\n                \"_cloudType\": \"Azure\",\n                \"subscription\": \"f4d319d8-7eac-4e15-a561-400f7744aa81\",\n                \"region\": \"centralus\",\n                \"subscriptionName\": \"dev-paladincloud\",\n                \"resourceGroupName\": \"dev-paladincloud\",\n                \"id\": \"subscriptions/f4d319d8-7eac-4e15-a561-400f7744aa81/resourceGroups/dev-paladincloud/providers/Microsoft.Network/networkSecurityGroups/testing-nsg\",\n                \"key\": \"ccb7e20e-47c3-478b-a960-580c7a6b9d1e\",\n                \"name\": \"testing-nsg\",\n                \"tags\": {},\n                \"excludedDetectionTypes\": [\n           \n                ]\n            }\n        }\n    ]\n}", JsonElement.class));
+        return jsonObject;
+    }
+
+
+    @Test
+    public void executeTest() throws Exception {
+        public void executeSucessTest() throws Exception {
+            mockStatic(PacmanUtils.class);
+            when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString(), anyString(), anyString(),
+                    anyString()))
+            mockStatic(RulesElasticSearchRepositoryUtil.class);
+            when(PacmanUtils.doesAllHaveValue(anyString(), anyString()))
+                    .thenReturn(
+                            true);
+            when(PacmanUtils.formatUrl(anyObject(), anyString())).thenReturn("host");
+            when(PacmanUtils.checkAccessibleToAll(anyObject(), anyString(), anyString(), anyString(), anyString(),
+                    anyString())).thenReturn(CommonTestUtils.getMapBoolean("r_123 "));
+            when(RulesElasticSearchRepositoryUtil.getQueryDetailsFromES(anyString(),anyObject(),
+                    anyObject(),
+                    anyObject(), anyObject(), anyInt(), anyObject(), anyObject(), anyObject())).thenReturn(getHitJsonArrayForRemoteDebugging());
+            assertThat(denyRemoteDebuggingWebAppRule.execute(CommonTestUtils.getMapString("r_123 "),
+                    CommonTestUtils.getMapString("r_123 ")), is(notNullValue()));
+            CommonTestUtils.getMapString("r_123 ")).getStatus(), is(PacmanSdkConstants.STATUS_SUCCESS));
+
+            when(PacmanUtils.checkAccessibleToAll(anyObject(), anyString(), anyString(), anyString(), anyString(),
+                    anyString())).thenReturn(CommonTestUtils.getEmptyMapBoolean("r_123 "));
+        }
+
 
     @Test
     public void appServiceRemoteDebuggingEnabled() {
