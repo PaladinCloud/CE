8240c5082ef3779410925355dd6f63fa848c3123
kushZemoso <kushagra.jain@zemosolabs.com>
2022-07-05 09:51:27 +0530

    Added Unit Test cases for Azure POlicies

--- jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/azurerules/storageAccount/StorageAccountCMKEncryptionRuleTest.java
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/azurerules/storageAccount/StorageAccountCMKEncryptionRuleTest.java
@@ -4,11 +4,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.anyObject;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.*;
+import static org.mockito.Matchers.anyInt;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.tmobile.cloud.awsrules.utils.RulesElasticSearchRepositoryUtil;
+import com.tmobile.pacman.commons.PacmanSdkConstants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -19,60 +24,53 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import com.tmobile.cloud.awsrules.utils.CommonTestUtils;
 import com.tmobile.cloud.awsrules.utils.PacmanUtils;
 import com.tmobile.cloud.azurerules.StorageAccount.StorageAccountCMKEncryptionRule;
-import com.tmobile.pacman.commons.exception.InvalidInputException;
-import com.tmobile.pacman.commons.exception.RuleExecutionFailedExeption;
 import com.tmobile.pacman.commons.rule.BaseRule;
 
-@PowerMockIgnore({ "javax.net.ssl.*", "javax.management.*" })
+@PowerMockIgnore({"javax.net.ssl.*", "javax.management.*"})
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ PacmanUtils.class, BaseRule.class })
+@PrepareForTest({PacmanUtils.class, BaseRule.class, RulesElasticSearchRepositoryUtil.class})
 public class StorageAccountCMKEncryptionRuleTest {
         @InjectMocks
         StorageAccountCMKEncryptionRule storageAccountCMKEncryptionRule;
 
+        public JsonObject getFailureJsonArrayForStorageAccountCMKEncryption(){
+                Gson gson=new Gson();
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.add("hits", gson.fromJson("{\n    \"hits\": [\n        {\n            \"_source\": {\n                \"discoverydate\":\"2022-07-03 11:00:00+0000\",\n                \"_cloudType\": \"Azure\",\n                \"subscription\":\"f4d319d8-7eac-4e15-a561-400f7744aa81\",\n                \"region\":\"null\",\n                \"subscriptionName\": \"dev-paladincloud\",\n                \"resourceGroupName\": \"dev-paladincloud\",\n                \"id\":\"subscriptions/f4d319d8-7eac-4e15-a561-400f7744aa81/resourceGroups/dev-rg-1/providers/Microsoft.Storage/storageAccounts/sqlvaisndaov4vhjgg\",\n                \"key\": \"ccb7e20e-47c3-478b-a960-580c7a6b9d1e\",\n                \"canAccessFromAzureServices\":\"true\",\n                \"name\":\"sqlvaisndaov4vhjgg\",\n                \"regionName\":\"eastus\",\n                \"customerManagedKey\":\"null\",\n                \"systemAssignedManagedServiceIdentityPrincipalId\":\"null\",\n                \"systemAssignedManagedServiceIdentityTenantId\":\"null\",\n                \"endPoints\":\"null\",\n                \"ipAddressesWithAccess\":[],\n                \"ipAddressRangesWithAccess\":[],\n                \"networkSubnetsWithAccess\":[],\n                \"tags\":{},\n                \"kind\":\"StorageV2\"\n            }\n        }\n    ]\n\n}\n}", JsonElement.class));
+                return jsonObject;
+        }
+        public  JsonObject getHitJsonArrayForStorageAccountCMKEncryption() {
+                Gson gson = new Gson();
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.add("hits", gson.fromJson("{\n    \"hits\": [\n        {\n            \"_source\": {\n                \"discoverydate\":\"2022-07-03 11:00:00+0000\",\n                \"_cloudType\": \"Azure\",\n                \"subscription\":\"f4d319d8-7eac-4e15-a561-400f7744aa81\",\n                \"region\":\"null\",\n                \"subscriptionName\": \"dev-paladincloud\",\n                \"resourceGroupName\": \"dev-paladincloud\",\n                \"id\":\"subscriptions/f4d319d8-7eac-4e15-a561-400f7744aa81/resourceGroups/dev-rg-1/providers/Microsoft.Storage/storageAccounts/sqlvaisndaov4vhjgg\",\n                \"key\": \"ccb7e20e-47c3-478b-a960-580c7a6b9d1e\",\n                \"canAccessFromAzureServices\":\"true\",\n                \"name\":\"sqlvaisndaov4vhjgg\",\n                \"regionName\":\"eastus\",\n                \"customerManagedKey\":\"ddsd123\",\n                \"systemAssignedManagedServiceIdentityPrincipalId\":\"null\",\n                \"systemAssignedManagedServiceIdentityTenantId\":\"null\",\n                \"endPoints\":\"null\",\n                \"ipAddressesWithAccess\":[],\n                \"ipAddressRangesWithAccess\":[],\n                \"networkSubnetsWithAccess\":[],\n                \"tags\":{},\n                \"kind\":\"StorageV2\"\n            }\n        }\n    ]\n\n}\n}", JsonElement.class));
+                return jsonObject;
+        }
         @Test
-        public void executeTest() throws Exception {
+        public void executeSucessTest() throws Exception {
                 mockStatic(PacmanUtils.class);
-                when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString(), anyString(), anyString(),
-                                anyString()))
-                                .thenReturn(
-                                                true);
-                when(PacmanUtils.formatUrl(anyObject(), anyString())).thenReturn("host");
-                when(PacmanUtils.checkAccessibleToAll(anyObject(), anyString(), anyString(), anyString(), anyString(),
-                                anyString())).thenReturn(CommonTestUtils.getMapBoolean("r_123 "));
-                assertThat(storageAccountCMKEncryptionRule.execute(
-                                CommonTestUtils.getMapString("r_123 "),
-                                CommonTestUtils.getMapString("r_123 ")), is(notNullValue()));
+                mockStatic(RulesElasticSearchRepositoryUtil.class);
+                when(PacmanUtils.doesAllHaveValue(anyString(), anyString()))
+                        .thenReturn(
+                                true);
+                when(RulesElasticSearchRepositoryUtil.getQueryDetailsFromES(anyString(),anyObject(),
+                        anyObject(),
+                        anyObject(), anyObject(), anyInt(), anyObject(), anyObject(), anyObject())).thenReturn(getHitJsonArrayForStorageAccountCMKEncryption());
+                assertThat(storageAccountCMKEncryptionRule.execute(CommonTestUtils.getMapString("r_123 "),
+                        CommonTestUtils.getMapString("r_123 ")).getStatus(), is(PacmanSdkConstants.STATUS_SUCCESS));
+        }
 
-                when(PacmanUtils.checkAccessibleToAll(anyObject(), anyString(), anyString(), anyString(), anyString(),
-                                anyString())).thenReturn(CommonTestUtils.getEmptyMapBoolean("r_123 "));
-                assertThat(storageAccountCMKEncryptionRule.execute(
-                                CommonTestUtils.getMapString("r_123 "),
-                                CommonTestUtils.getMapString("r_123 ")), is(notNullValue()));
-
-                when(PacmanUtils.checkAccessibleToAll(anyObject(), anyString(), anyString(), anyString(), anyString(),
-                                anyString())).thenThrow(new Exception());
-                assertThatThrownBy(
-                                () -> storageAccountCMKEncryptionRule.execute(
-                                                CommonTestUtils.getMapString("r_123 "),
-                                                CommonTestUtils.getMapString("r_123 ")))
-                                .isInstanceOf(RuleExecutionFailedExeption.class);
-
-                assertThatThrownBy(
-                                () -> storageAccountCMKEncryptionRule.execute(
-                                                CommonTestUtils.getOneMoreMapString("r_123 "),
-                                                CommonTestUtils.getMapString("r_123 ")))
-                                .isInstanceOf(RuleExecutionFailedExeption.class);
-
-                when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString(), anyString(), anyString(),
-                                anyString()))
-                                .thenReturn(
-                                                false);
-                assertThatThrownBy(
-                                () -> storageAccountCMKEncryptionRule.execute(
-                                                CommonTestUtils.getMapString("r_123 "),
-                                                CommonTestUtils.getMapString("r_123 ")))
-                                .isInstanceOf(InvalidInputException.class);
+        @Test
+        public void executeFailureTest() throws Exception {
+                mockStatic(PacmanUtils.class);
+                mockStatic(RulesElasticSearchRepositoryUtil.class);
+                when(PacmanUtils.doesAllHaveValue(anyString(), anyString()))
+                        .thenReturn(
+                                true);
+                when(RulesElasticSearchRepositoryUtil.getQueryDetailsFromES(anyString(),anyObject(),
+                        anyObject(),
+                        anyObject(), anyObject(), anyInt(), anyObject(), anyObject(), anyObject())).thenReturn(getFailureJsonArrayForStorageAccountCMKEncryption());
+                assertThat(storageAccountCMKEncryptionRule.execute(CommonTestUtils.getMapString("r_123 "),
+                        CommonTestUtils.getMapString("r_123 ")).getStatus(), is(PacmanSdkConstants.STATUS_FAILURE));
         }
 
         @Test
--- jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/azurerules/storageAccount/StorageAccountPublicAcessRuleTest.java
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/azurerules/storageAccount/StorageAccountPublicAcessRuleTest.java
@@ -1,9 +1,15 @@
 package com.tmobile.cloud.azurerules.storageAccount;
 
 import com.amazonaws.services.identitymanagement.model.InvalidInputException;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.tmobile.cloud.awsrules.utils.CommonTestUtils;
 import com.tmobile.cloud.awsrules.utils.PacmanUtils;
+import com.tmobile.cloud.awsrules.utils.RulesElasticSearchRepositoryUtil;
 import com.tmobile.cloud.azurerules.StorageAccount.StorageAccountCMKEncryptionRule;
 import com.tmobile.cloud.azurerules.StorageAccount.StorageAccountPublicAcessRule;
+import com.tmobile.pacman.commons.PacmanSdkConstants;
 import com.tmobile.pacman.commons.rule.BaseRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,62 +25,58 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.*;
+import static org.mockito.Matchers.anyObject;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
-@PowerMockIgnore({ "javax.net.ssl.*", "javax.management.*" })
+@PowerMockIgnore({"javax.net.ssl.*", "javax.management.*"})
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ PacmanUtils.class, BaseRule.class })
+@PrepareForTest({PacmanUtils.class, BaseRule.class, RulesElasticSearchRepositoryUtil.class})
 public class StorageAccountPublicAcessRuleTest {
 
     @InjectMocks
     StorageAccountPublicAcessRule storageAccountPublicAcessRule;
 
+
+    public JsonObject getFailureJsonArrayForStorageAccountPublicAccess(){
+        Gson gson=new Gson();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("hits", gson.fromJson("{\n    \"hits\": [\n        {\n            \"_source\": {\n                \"discoverydate\":\"2022-07-03 11:00:00+0000\",\n                \"_cloudType\": \"Azure\",\n                \"subscription\":\"f4d319d8-7eac-4e15-a561-400f7744aa81\",\n                \"region\":\"null\",\n                \"subscriptionName\": \"dev-paladincloud\",\n                \"resourceGroupName\": \"dev-paladincloud\",\n                \"id\":\"subscriptions/f4d319d8-7eac-4e15-a561-400f7744aa81/resourceGroups/dev-rg-1/providers/Microsoft.Storage/storageAccounts/sqlvaisndaov4vhjgg\",\n                \"key\": \"ccb7e20e-47c3-478b-a960-580c7a6b9d1e\",\n                \"canAccessFromAzureServices\":\"true\",\n                \"name\":\"sqlvaisndaov4vhjgg\",\n                \"regionName\":\"eastus\",\n                \"customerManagedKey\":\"dds123n3\",\n                \"blobPublicAccessAllowed\":\"false\",\n                \"tags\":{},\n                \"kind\":\"StorageV2\"\n            }\n        }\n    ]\n\n}\n}", JsonElement.class));
+        return jsonObject;
+    }
+    public  JsonObject getHitJsonArrayForStorageAccountPublicAccess() {
+        Gson gson = new Gson();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("hits", gson.fromJson("{\n    \"hits\": [\n        {\n            \"_source\": {\n                \"discoverydate\":\"2022-07-03 11:00:00+0000\",\n                \"_cloudType\": \"Azure\",\n                \"subscription\":\"f4d319d8-7eac-4e15-a561-400f7744aa81\",\n                \"region\":\"null\",\n                \"subscriptionName\": \"dev-paladincloud\",\n                \"resourceGroupName\": \"dev-paladincloud\",\n                \"id\":\"subscriptions/f4d319d8-7eac-4e15-a561-400f7744aa81/resourceGroups/dev-rg-1/providers/Microsoft.Storage/storageAccounts/sqlvaisndaov4vhjgg\",\n                \"key\": \"ccb7e20e-47c3-478b-a960-580c7a6b9d1e\",\n                \"canAccessFromAzureServices\":\"true\",\n                \"name\":\"sqlvaisndaov4vhjgg\",\n                \"regionName\":\"eastus\",\n                \"customerManagedKey\":\"dds123n3\",\n                \"blobPublicAccessAllowed\":\"true\",\n                \"tags\":{},\n                \"kind\":\"StorageV2\"\n            }\n        }\n    ]\n\n}\n}", JsonElement.class));
+        return jsonObject;
+    }
     @Test
-    public void executeTest() throws Exception {
+    public void executeSucessTest() throws Exception {
         mockStatic(PacmanUtils.class);
-        when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString())).thenReturn(true);
-        assertThat(storageAccountPublicAcessRule.execute(getMapNotExistString("r_123 "), getMapNotExistString("r_123 ")),
-                is(notNullValue()));
-
-        when(PacmanUtils.getPacmanHost(anyString())).thenReturn("host");
-
-        when(PacmanUtils.checkISResourceIdExistsFromElasticSearch("_resourceid", "azure_storageaccount/_search", "", ""))
-                .thenReturn(true);
-        when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString())).thenReturn(false);
-        assertThatThrownBy(
-                () -> storageAccountPublicAcessRule.execute(getMapString("r_123 "), getMapString("r_123 ")))
-                .isInstanceOf(InvalidInputException.class);
-
+        mockStatic(RulesElasticSearchRepositoryUtil.class);
+        when(PacmanUtils.doesAllHaveValue(anyString(), anyString()))
+                .thenReturn(
+                        true);
+        when(RulesElasticSearchRepositoryUtil.getQueryDetailsFromES(anyString(),anyObject(),
+                anyObject(),
+                anyObject(), anyObject(), anyInt(), anyObject(), anyObject(), anyObject())).thenReturn(getHitJsonArrayForStorageAccountPublicAccess());
+        assertThat(storageAccountPublicAcessRule.execute(CommonTestUtils.getMapString("r_123 "),
+                CommonTestUtils.getMapString("r_123 ")).getStatus(), is(PacmanSdkConstants.STATUS_SUCCESS));
     }
 
-    public static Map<String, String> getMapString(String passRuleResourceId) {
-        Map<String, String> commonMap = new HashMap<>();
-        commonMap.put("executionId", "1234");
-        commonMap.put("_resourceid", passRuleResourceId);
-        commonMap.put("severity", "high");
-        commonMap.put("ruleCategory", "security");
-        commonMap.put("type", "Task");
-        commonMap.put("ruleId",
-                "Azure_Check_Public_Access_For_Storage_Account");
-        commonMap.put("policyId", "Azure_Check_Public_Access_For_Storage_Account");
-        commonMap.put("policyVersion", "version-1");
-        return commonMap;
-    }
-
-    public static Map<String, String> getMapNotExistString(String passRuleResourceId) {
-        Map<String, String> commonMap = new HashMap<>();
-        commonMap.put("executionId", "1234");
-        commonMap.put("_resourceid", passRuleResourceId);
-        commonMap.put("severity", "high");
-        commonMap.put("ruleCategory", "security");
-        commonMap.put("type", "Task");
-        commonMap.put("ruleId",
-                "Azure_Check_Public_Access_For_Storage_Account");
-        commonMap.put("policyId", "Azure_Check_Public_Access_For_Storage_Account");
-        commonMap.put("policyVersion", "version-1");
-        return commonMap;
+    @Test
+    public void executeFailureTest() throws Exception {
+        mockStatic(PacmanUtils.class);
+        mockStatic(RulesElasticSearchRepositoryUtil.class);
+        when(PacmanUtils.doesAllHaveValue(anyString(), anyString()))
+                .thenReturn(
+                        true);
+        when(RulesElasticSearchRepositoryUtil.getQueryDetailsFromES(anyString(),anyObject(),
+                anyObject(),
+                anyObject(), anyObject(), anyInt(), anyObject(), anyObject(), anyObject())).thenReturn(getFailureJsonArrayForStorageAccountPublicAccess());
+        assertThat(storageAccountPublicAcessRule.execute(CommonTestUtils.getMapString("r_123 "),
+                CommonTestUtils.getMapString("r_123 ")).getStatus(), is(PacmanSdkConstants.STATUS_FAILURE));
     }
 
     @Test
