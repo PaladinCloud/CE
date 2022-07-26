731039486362d4f8858c54037f640a5e5652f7ee
Amisha Vijayakumar <amisha.vijayakumar@paladincloud.io>
2022-07-26 07:26:08 +0000

    fix:updating opensearch policy to check the version for encryption

--- jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/elasticsearch/ESEncryptionUsingKMSCMKsRuleTest.java
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/elasticsearch/ESEncryptionUsingKMSCMKsRuleTest.java
@@ -64,6 +64,19 @@ public class ESEncryptionUsingKMSCMKsRuleTest {
 		RuleResult ruleResult = esEncryptionUsingKMSCMKsRule.execute(ruleParam, resourceAttribute);
 		assertEquals(PacmanSdkConstants.STATUS_FAILURE, ruleResult.getStatus());
 	}
+	
+	@Test
+	public void esWithLowerVersion() {
+		Map<String, String> ruleParam = new HashMap<>();
+		ruleParam.put(PacmanSdkConstants.EXECUTION_ID, "exectionid");
+		ruleParam.put(PacmanSdkConstants.RULE_ID, "esEncryptionAtRestRule");
+		ruleParam.put(PacmanRuleConstants.CATEGORY, PacmanSdkConstants.SECURITY);
+		ruleParam.put(PacmanRuleConstants.SEVERITY, PacmanSdkConstants.SEV_HIGH);
+
+		Map<String, String> resourceAttribute = getResourceWithLowerVersion("ES1234");
+		RuleResult ruleResult = esEncryptionUsingKMSCMKsRule.execute(ruleParam, resourceAttribute);
+		assertEquals(PacmanSdkConstants.STATUS_FAILURE, ruleResult.getStatus());
+	}
 
 	@Test
 	public void getHelpTextTest() {
@@ -77,6 +90,7 @@ public class ESEncryptionUsingKMSCMKsRuleTest {
 		clusterObj.put("creationTimestamp", "2022-01-10T13:00:38.628-08:00");
 		clusterObj.put("encryptionenabled", "true");
 		clusterObj.put("encryptionkmskey", "(Default) aws/es");
+		clusterObj.put("elasticsearchversion", "5.5");
 
 		return clusterObj;
 	}
@@ -88,6 +102,7 @@ public class ESEncryptionUsingKMSCMKsRuleTest {
 		clusterObj.put("creationTimestamp", "2022-01-10T13:00:38.628-08:00");
 		clusterObj.put("encryptionenabled", "true");
 		clusterObj.put("encryptionkmskey", "true");
+		clusterObj.put("elasticsearchversion", "5.5");
 
 		return clusterObj;
 	}
@@ -98,6 +113,19 @@ public class ESEncryptionUsingKMSCMKsRuleTest {
 		clusterObj.put("name", "AWS-ES");
 		clusterObj.put("creationTimestamp", "2022-01-10T13:00:38.628-08:00");
 		clusterObj.put("encryptionenabled", "false");
+		clusterObj.put("elasticsearchversion", "5.5");
+
+		return clusterObj;
+	}
+	
+	private Map<String, String> getResourceWithLowerVersion(String clusterID) {
+		Map<String, String> clusterObj = new HashMap<>();
+		clusterObj.put("_resourceid", clusterID);
+		clusterObj.put("name", "AWS-ES");
+		clusterObj.put("creationTimestamp", "2022-01-10T13:00:38.628-08:00");
+		clusterObj.put("encryptionenabled", "true");
+		clusterObj.put("encryptionkmskey", "(Default) aws/es");
+		clusterObj.put("elasticsearchversion", "5.0");
 
 		return clusterObj;
 	}
