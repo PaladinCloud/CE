9aa5e082f89a0c6b72cbcb8cd2334c8a13060b5a
Santhosh Challa <santhosh.challa@zemosolabs.com>
2022-08-08 12:03:27 +0530

    feat: Added new service for job scheduler

---
+++ jobs/job-scheduler/src/main/java/com/paladincloud/jobscheduler/config/SchedulerConfig.java
@@ -0,0 +1,11 @@
+package com.paladincloud.jobscheduler.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
+public class SchedulerConfig {
+}
