b3bad87a39e82fbd77b8f12fc1093dd84f090634
Santhosh Challa <santhosh.challa@zemosolabs.com>
2022-08-08 12:03:27 +0530

    feat: Added new service for job scheduler

---
+++ jobs/job-scheduler/src/main/java/com/paladincloud/jobscheduler/JobSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.paladincloud.jobscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSchedulerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JobSchedulerApplication.class, args);
+	}
+}
