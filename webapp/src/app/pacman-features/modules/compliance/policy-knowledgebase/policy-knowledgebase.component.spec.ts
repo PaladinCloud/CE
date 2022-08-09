31c858264d7ecb981cb9c88396b4960aa2c772ce
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-08-10 00:51:13 +0530

    chore: written tests for table & table-wrapper components and fixed errors in policy knowledgebase spec file

--- webapp/src/app/pacman-features/modules/compliance/policy-knowledgebase/policy-knowledgebase.component.spec.ts
+++ webapp/src/app/pacman-features/modules/compliance/policy-knowledgebase/policy-knowledgebase.component.spec.ts
@@ -12,24 +12,68 @@
  * limitations under the License.
  */
 
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { Renderer2 } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AssetGroupObservableService } from 'src/app/core/services/asset-group-observable.service';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { DataCacheService } from 'src/app/core/services/data-cache.service';
+import { DomainTypeObservableService } from 'src/app/core/services/domain-type-observable.service';
+import { WorkflowService } from 'src/app/core/services/workflow.service';
+import { BackNavigationComponent } from 'src/app/shared/back-navigation/back-navigation.component';
+import { CommonResponseService } from 'src/app/shared/services/common-response.service';
+import { ErrorHandlingService } from 'src/app/shared/services/error-handling.service';
+import { HttpService } from 'src/app/shared/services/http-response.service';
+import { LoggerService } from 'src/app/shared/services/logger.service';
+import { RefactorFieldsService } from 'src/app/shared/services/refactor-fields.service';
+import { RouterUtilityService } from 'src/app/shared/services/router-utility.service';
+import { UtilsService } from 'src/app/shared/services/utils.service';
+import { TableWrapperComponent } from 'src/app/shared/table-wrapper/table-wrapper.component';
+import { TableComponent } from 'src/app/shared/table/table.component';
 
 import { PolicyKnowledgebaseComponent } from './policy-knowledgebase.component';
 
-describe('PolicyKnowledgebaseComponent', () => {
+
+describe('Policyknowledgebase', () => {
   let component: PolicyKnowledgebaseComponent;
   let fixture: ComponentFixture<PolicyKnowledgebaseComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PolicyKnowledgebaseComponent ]
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, MatSelectModule, MatTableModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule],
+      declarations: [ PolicyKnowledgebaseComponent, TableWrapperComponent, TableComponent, BackNavigationComponent ],
+      providers: [CommonResponseService, 
+        LoggerService, ErrorHandlingService, AssetGroupObservableService, 
+        DataCacheService, UtilsService, RefactorFieldsService, Renderer2,
+                  WorkflowService,
+                  DomainTypeObservableService,
+                  RouterUtilityService,
+                  HttpService, HttpClient, HttpHandler
+                  ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PolicyKnowledgebaseComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    component.knowledgebaseData = [
+      {col1:"row1 col1", col2:"row1 col2"},
+      {col1:"row2 col1", col2:"row2 col2"},
+    ];
+
+    // component.columnNamesMap = {col1: "col1", col2: "col2"};
+
+    // component.columnWidths = {col1:2, col2:1};
+
     fixture.detectChanges();
   });
 
--- webapp/src/app/shared/table/table.component.spec.ts
+++ webapp/src/app/shared/table/table.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTableModule } from '@angular/material/table';
 
 import { TableComponent } from './table.component';
 
@@ -8,6 +9,7 @@ describe('Table', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[ MatTableModule ],
       declarations: [ TableComponent ]
     })
     .compileComponents();
@@ -17,15 +19,37 @@ describe('Table', () => {
     fixture = TestBed.createComponent(TableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    component.dataSource = [
+      {col1:"row1 col1", col2:"row1 col2"},
+      {col1:"row2 col1", col2:"row2 col2"},
+    ];
+
+    component.displayedColumns = ["col1", "col2"];
+
+    component.columnNamesMap = {col1: "col1", col2: "col2"};
+
+    component.columnWidths = {col1:2, col2:1};
+
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should display a different test title', () => {
-    // component.dataSource = 'Test Title';
-    // fixture.detectChanges();
-    expect("Test Title").toContain('Test Title');
+  it('should have 2 rows', () => {
+    let tableRows = fixture.nativeElement.querySelectorAll('tr');
+    expect(tableRows.length-1).toBe(2);
+  });
+
+  it("should have 1st col name as col1", () => {
+    let colName = fixture.nativeElement.querySelectorAll('tr > th')[0];
+    expect(colName.textContent).toContain("col1");
+  });
+
+  it("should have 1st cell as row1 col1", () => {
+    let firstCell = fixture.nativeElement.querySelectorAll('tr > td')[0];
+    expect(firstCell.textContent).toContain("row1 col1");
   });
 });
