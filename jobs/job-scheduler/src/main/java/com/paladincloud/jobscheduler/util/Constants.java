44501cee9495320ef985156bbdeb22b7b8c64a84
Santhosh Challa <santhosh.challa@zemosolabs.com>
2022-08-23 17:02:32 +0530

    fix: modified the configurations

--- jobs/job-scheduler/src/main/java/com/paladincloud/jobscheduler/util/Constants.java
+++ jobs/job-scheduler/src/main/java/com/paladincloud/jobscheduler/util/Constants.java
@@ -1,11 +1,9 @@
 package com.paladincloud.jobscheduler.util;
 
 public class Constants {
-    public static final String APPLICATION = "application";
-    public static final String BATCH = "batch";
-    public static final String INVENTORY = "inventory";
     public static final String NAME = "name";
     public static final String SOURCE = "source";
     public static final String CONFIG_URL = "CONFIG_URL";
     public static final String CONFIG_CREDS = "CONFIG_CREDS";
+    public static final String JOB_SCHEDULER = "job-scheduler";
 }
