0dc06e5256ed8d6822896b1cac7fc468b6c985d8
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2022-08-29 17:44:33 +0530

    fix: remote access database flag

---
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/gcprules/cloudsql/DisableRemoteAccessDBFlagRuleTest.java
@@ -0,0 +1,104 @@
+package com.tmobile.cloud.gcprules.cloudsql;
+
+import com.tmobile.cloud.awsrules.utils.CommonTestUtils;
+import com.tmobile.cloud.awsrules.utils.PacmanUtils;
+import com.tmobile.cloud.gcprules.utils.GCPUtils;
+import com.tmobile.pacman.commons.PacmanSdkConstants;
+import com.tmobile.pacman.commons.exception.InvalidInputException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.tmobile.cloud.gcprules.utils.TestUtils.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({PacmanUtils.class, GCPUtils.class})
+@PowerMockIgnore({"javax.net.ssl.*", "javax.management.*","jdk.internal.reflect.*"})
+public class DisableRemoteAccessDBFlagRuleTest {
+    @InjectMocks
+    DisableContainedDBAuthenticationRule disableContainedDBAuthenticationRule;
+    @Before
+    public void setUp() {
+        mockStatic(PacmanUtils.class);
+        mockStatic(GCPUtils.class);
+    }
+
+    @Test
+    public void executeSuccessTest() throws Exception {
+
+
+        when(PacmanUtils.getPacmanHost(anyString())).thenReturn("host");
+        when(GCPUtils.getHitsArrayFromEs(anyObject(), anyObject())).thenReturn(getHitjsonArrayForRemoteAccessDBFlag());
+
+        when(PacmanUtils.createAnnotation(anyString(), anyObject(), anyString(), anyString(), anyString())).thenReturn(CommonTestUtils.getAnnotation("123"));
+        when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString())).thenReturn(
+                true);
+        assertThat(disableContainedDBAuthenticationRule.execute(getMapString("r_123 "), getMapString("r_123 ")).getStatus(), is(PacmanSdkConstants.STATUS_SUCCESS));
+
+    }
+
+    @Test
+    public void executeFailureTest() throws Exception {
+
+
+        when(PacmanUtils.getPacmanHost(anyString())).thenReturn("host");
+        when(GCPUtils.getHitsArrayFromEs(anyObject(), anyObject())).thenReturn(getFailurejsonArrayForRemoteAccessDBFlag());
+
+        when(PacmanUtils.createAnnotation(anyString(), anyObject(), anyString(), anyString(), anyString())).thenReturn(CommonTestUtils.getAnnotation("123"));
+        when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString())).thenReturn(
+                true);
+        assertThat(disableContainedDBAuthenticationRule.execute(getMapString("r_123 "), getMapString("r_123 ")).getStatus(), is(PacmanSdkConstants.STATUS_FAILURE));
+    }
+
+    @Test
+    public void executeFailureTestWithInvalidInputException() throws Exception {
+
+        when(PacmanUtils.getPacmanHost(anyString())).thenReturn("host");
+        when(GCPUtils.getHitsArrayFromEs(anyObject(), anyObject())).thenReturn(getHitjsonArrayForDBOwnerFlagChanging());
+
+        when(PacmanUtils.createAnnotation(anyString(), anyObject(), anyString(), anyString(), anyString())).thenReturn(CommonTestUtils.getAnnotation("123"));
+        when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString())).thenReturn(
+                false);
+        assertThatThrownBy(() -> disableContainedDBAuthenticationRule.execute(getMapString("r_123 "), getMapString("r_123 "))).isInstanceOf(InvalidInputException.class);
+    }
+
+    public static Map<String, String> getMapString(String passRuleResourceId) {
+        Map<String, String> commonMap = new HashMap<>();
+        commonMap.put("executionId", "1234");
+        commonMap.put("_resourceid", passRuleResourceId);
+        commonMap.put("severity", "medium");
+        commonMap.put("ruleCategory", "security");
+        commonMap.put("accountid", "12345");
+        commonMap.put("ruleId", "Disable_Remote_Access_Flag_for_SQL_Server_DB_Instances");
+        commonMap.put("policyId", "Disable_Remote_Access_Flag_for_SQL_Server_DB_Instances");
+        commonMap.put("policyVersion", "version-1");
+        commonMap.put("dataBaseType", "SQLSERVER");
+        commonMap.put("dbFlagName", "remote access");
+        commonMap.put("description", "Ensure that remote access database flag is disabled for your Google Cloud SQL Server database instances");
+        commonMap.put("violationReason", "remote access flag is enabled for your Google Cloud SQL Server database instances");
+
+        return commonMap;
+    }
+
+    @Test
+    public void getHelpTextTest() {
+        assertThat(disableContainedDBAuthenticationRule.getHelpText(), is(notNullValue()));
+    }
+
+
+}
