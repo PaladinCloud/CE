3e022527dc6acbe2d3f6033dfa5c9ac94228abd3
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2022-09-12 18:01:22 +0530

    feat: subnet mode should not be legacy

--- jobs/gcp-discovery/src/main/java/com/tmobile/pacbot/gcp/inventory/vo/NetworkVH.java
+++ jobs/gcp-discovery/src/main/java/com/tmobile/pacbot/gcp/inventory/vo/NetworkVH.java
@@ -2,9 +2,18 @@ package com.tmobile.pacbot.gcp.inventory.vo;
 
 public class NetworkVH extends GCPVH{
     private String name;
+    private boolean autoCreateSubnetworks;
 
     public String getName() {
         return name;
+    }
+
+    public boolean isAutoCreateSubnetworks() {
+        return autoCreateSubnetworks;
+    }
+
+    public void setAutoCreateSubnetworks(boolean autoCreateSubnetworks) {
+        this.autoCreateSubnetworks = autoCreateSubnetworks;
     }
 
     public void setName(String name) {
