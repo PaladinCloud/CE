dbbe6657a132655deef5a36d8155c412cf4c6185
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-09-27 18:13:21 +0530

    added graph

--- webapp/src/app/pacman-features/modules/assets/asset-dashboard/asset-dashboard.component.ts
+++ webapp/src/app/pacman-features/modules/assets/asset-dashboard/asset-dashboard.component.ts
@@ -24,12 +24,13 @@ import { ErrorHandlingService } from '../../../../shared/services/error-handling
 import { WorkflowService } from '../../../../core/services/workflow.service';
 import { AssetGroupObservableService } from '../../../../core/services/asset-group-observable.service';
 import { CONFIGURATIONS } from './../../../../../config/configurations';
+import { MultilineChartService } from 'src/app/pacman-features/services/multilinechart.service';
 
 @Component({
   selector: 'app-asset-dashboard',
   templateUrl: './asset-dashboard.component.html',
   styleUrls: ['./asset-dashboard.component.css'],
-  providers: [LoggerService, ErrorHandlingService]
+  providers: [LoggerService, ErrorHandlingService, MultilineChartService]
 })
 export class AssetDashboardComponent implements OnInit, OnDestroy {
 
@@ -42,13 +43,50 @@ export class AssetDashboardComponent implements OnInit, OnDestroy {
   public backButtonRequired;
   public config;
   public oss;
+  assetGroupName;
+  domainName;
+  totalAssetsCountData = [];
+  totalAssetsCountDataError = '';
+  handleGraphIntervalSelection = (e) => {
+    let date = new Date();
+    e = e.toLowerCase();
+    let queryParamObj = {};
+    switch(e){
+      case "1 week":
+        date.setDate(date.getDate() - 7);
+        break;
+      case "1 month":
+        date.setMonth(date.getMonth() - 1);
+        break;
+      case "6 months":
+        date.setMonth(date.getMonth() - 6);
+        break;
+      case "12 months":
+        date.setFullYear(date.getFullYear() - 1);
+        break;
+    }
+
+    if(e != "all time" && e != "custom"){
+      const offset = date.getTimezoneOffset()
+      let fromDate = new Date(date.getTime() - (offset*60*1000)).toISOString().split('T')[0]
+      queryParamObj["from"] = fromDate;
+    }        
+    this.getAssetsCountData(queryParamObj);
+  }
+  card = {
+      id: 3,
+      header: "Total Assets",
+      footer: "View Asset Distribution",
+      onSelectGraphInterval: this.handleGraphIntervalSelection,
+    }
 
   constructor(
     private dataStore: DataCacheService,
     private router: Router,
     private logger: LoggerService,
     private workflowService: WorkflowService,
-    private assetGroupObservableService: AssetGroupObservableService
+    private assetGroupObservableService: AssetGroupObservableService,
+    private multilineChartService: MultilineChartService
   ) {
     this.config = CONFIGURATIONS;
 
@@ -61,10 +99,54 @@ export class AssetDashboardComponent implements OnInit, OnDestroy {
 
   }
 
+  massageAssetTrendGraphData(graphData){
+    let data = [];
+    data.push({"key":"TotalAssetCount", "values":[]})
+    graphData.trend.forEach(e => {
+       data[0].values.push({
+            'date':new Date(e.date),
+            'value':e.totalassets,
+            'zero-value':e.totalassets==0
+        });
+    })   
+    data[0].values.sort(function(a,b){
+        return new Date(a.date).valueOf() - new Date(b.date).valueOf();
+    });
+
+    return data;
+  }
+
+  private getAssetsCountData(queryObj) {
+    if(!this.assetGroupName){
+      return;
+    }
+    const queryParams = {
+      ag: this.assetGroupName,
+      ...queryObj
+    };
+
+    this.totalAssetsCountDataError = '';
+    this.totalAssetsCountData = [];
+
+    try {
+        this.multilineChartService.getAssetTrendData(queryParams).subscribe(response => {
+            this.totalAssetsCountData = this.massageAssetTrendGraphData(response[0]);
+            if(this.totalAssetsCountData.length==0){
+                this.totalAssetsCountDataError = 'noDataAvailable';
+            }
+        });
+    } catch (error) {
+        this.totalAssetsCountDataError = "apiResponseError";
+        this.logger.log("error", error);
+    }
+  }
+
   getAssetGroup() {
     this.assetGroupObservableService.getAssetGroup().subscribe(
       assetGroupName => {
           this.backButtonRequired = this.workflowService.checkIfFlowExistsCurrently(this.pageLevel);
+          this.assetGroupName = assetGroupName;
+          this.getAssetsCountData({});
     });
   }
 
