55c7a178fdd031143154da732ba6861cba8c7531
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2022-09-28 16:00:44 +0530

    feat: Service account rule

--- jobs/gcp-discovery/src/main/java/com/tmobile/pacbot/gcp/inventory/collector/ServiceAccountInventoryCollector.java
+++ jobs/gcp-discovery/src/main/java/com/tmobile/pacbot/gcp/inventory/collector/ServiceAccountInventoryCollector.java
@@ -3,8 +3,6 @@ package com.tmobile.pacbot.gcp.inventory.collector;
 import com.google.api.services.cloudresourcemanager.CloudResourceManager;
 import com.google.api.services.cloudresourcemanager.model.GetIamPolicyRequest;
 import com.google.api.services.iam.v1.Iam;
-import com.google.api.services.iam.v1.model.Binding;
-import com.google.api.services.iam.v1.model.Policy;
 import com.google.api.services.iam.v1.model.ServiceAccount;
 import com.google.api.services.iam.v1.model.ServiceAccountKey;
 import com.tmobile.pacbot.gcp.inventory.auth.GCPCredentialsProvider;
@@ -44,55 +42,37 @@ public class ServiceAccountInventoryCollector {
            serviceAccountVH.setDescription(serviceAccount.getDescription());
            logger.info("Serice account inventory collector-> {}",serviceAccount.getName());
            getServiceAccountKeys(serviceAccount.getUniqueId(),serviceAccountVH,project);
-           getServiceAccountMemberRoles(serviceAccount,serviceAccountVH);
            serviceAccountMap.put(serviceAccount.getName(), serviceAccountVH);
 
            //Method to fetch the role and membership of accounts of project
-           fetchMembership(serviceAccountMap,project.getProjectId());
+           fetchMembership(serviceAccountVH,project.getProjectId());
 
            serviceAccountVHList.add(serviceAccountVH);
 
        }
        return serviceAccountVHList;
     }
-    private void fetchMembership(Map<String, ServiceAccountVH> serviceAccount, String projectId){
+    private void fetchMembership(ServiceAccountVH serviceAccountVH, String projectId){
         CloudResourceManager.Projects.GetIamPolicy iamPolicy = null;
         try {
+            Map<String,List<String>> rolesMembers=new HashMap<>();
             iamPolicy = gcpCredentialsProvider.getCloudResourceManager().projects().getIamPolicy(projectId, new GetIamPolicyRequest());
             List<com.google.api.services.cloudresourcemanager.model.Binding> binds = iamPolicy.execute().getBindings();
             if(binds!=null) {
                 for (com.google.api.services.cloudresourcemanager.model.Binding binding : binds) {
-                    //TODO: process these bindings
                     logger.info("Binding role: {}",binding.getRole());
                     logger.info("Binding members: {}",binding.getMembers());
+                    rolesMembers.put(binding.getRole(),binding.getMembers());
                 }
             }
+            serviceAccountVH.setRolesMembers(rolesMembers);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (GeneralSecurityException e) {
             throw new RuntimeException(e);
         }
     }
-    private void getServiceAccountMemberRoles(ServiceAccount serviceAccount, ServiceAccountVH serviceAccountVH)  {
-        Iam.Projects.ServiceAccounts.GetIamPolicy requestPolicy =
-                null;
-        try {
-            requestPolicy = gcpCredentialsProvider.getIamService().projects().serviceAccounts().getIamPolicy(serviceAccount.getName());
-            Policy responsePolicy = requestPolicy.execute();
-            Map<String,List<String>> roleMembers=new HashMap() ;
-            List<Binding> bindings=responsePolicy.getBindings();
-            if(bindings!=null) {
-                for (Binding binding : bindings) {
-                    roleMembers.put(binding.getRole(), binding.getMembers());
-                }
-            }
-            serviceAccountVH.setRolesMembers(roleMembers);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }
-
     public void  getServiceAccountKeys(String serviceAccountName,ServiceAccountVH serviceAccountVH,ProjectVH projectVH) throws GeneralSecurityException, IOException {
 
         String apiUrlTemplate = "projects/"+projectVH.getProjectId()+"/serviceAccounts/"+serviceAccountName;
