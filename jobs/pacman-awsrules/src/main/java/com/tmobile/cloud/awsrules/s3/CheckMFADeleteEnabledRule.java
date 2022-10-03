9f0cc4521b28f9516bf02db24b35c1a8648c0116
amishavijayakumar08 <104547680+amishavijayakumar08@users.noreply.github.com>
2022-10-03 16:05:37 +0530

    feat:Policy to check enable mfa delete on s3 bucket (#508)
    feat:Updated aws rule to check mfa delete enabled on s3

--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/awsrules/s3/CheckMFADeleteEnabledRule.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/awsrules/s3/CheckMFADeleteEnabledRule.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright 2018 T Mobile, Inc. or its affiliates. All Rights Reserved.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
- * of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations under
- * the License.
- ******************************************************************************/
 package com.tmobile.cloud.awsrules.s3;
 
 import java.util.ArrayList;
@@ -54,7 +39,7 @@ public class CheckMFADeleteEnabledRule extends BaseRule {
 	 * 
 	 * ruleCategory : Enter the value of category <br><br>
 	 * 
-	 * roleIdentifyingString : Configure it as role/pac_ro <br><br>
+	 * roleIdentifyingString : Configure it as role/paladincloud_ro <br><br>
 	 * 
 	 * @param resourceAttributes this is a resource in context which needs to be scanned this is provided by execution engine
 	 *
--- jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/s3/CheckMFADeleteEnabledRuleTest.java
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/s3/CheckMFADeleteEnabledRuleTest.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright 2018 T Mobile, Inc. or its affiliates. All Rights Reserved.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
- * of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations under
- * the License.
- ******************************************************************************/
 package com.tmobile.cloud.awsrules.s3;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -83,7 +68,10 @@ public class CheckMFADeleteEnabledRuleTest {
         when(awsS3Client.getBucketVersioningConfiguration(anyString())).thenReturn(configuration);
         spy.execute(CommonTestUtils.getMapString("r_123 "),CommonTestUtils.getMapString("r_123 "));
         
-        assertThat(checkMFADeleteEnabledRule.execute(CommonTestUtils.getMapString("r_123 "),CommonTestUtils.getEmptyMapString()), is(notNullValue()));
+        BucketVersioningConfiguration configurationEnabled = new BucketVersioningConfiguration();
+        configurationEnabled.setMfaDeleteEnabled(true);
+        when(awsS3Client.getBucketVersioningConfiguration(anyString())).thenReturn(configurationEnabled);
+        spy.execute(CommonTestUtils.getMapString("r_123 "),CommonTestUtils.getMapString("r_123 "));
         
         when(awsS3Client.getBucketVersioningConfiguration(anyString())).thenThrow(new RuleExecutionFailedExeption());
         assertThatThrownBy( 
