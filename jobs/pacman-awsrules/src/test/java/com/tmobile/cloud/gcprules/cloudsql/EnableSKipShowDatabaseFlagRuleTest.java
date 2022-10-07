064880548be4f4c6c6a9059163c5e975c74414d5
kushZemoso <kushagra.jain@zemosolabs.com>
2022-10-07 10:46:21 +0530

    feat:This rule checks if skip_show_database flag for MYSQL db is enabled.

---
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/gcprules/cloudsql/EnableSKipShowDatabaseFlagRuleTest.java
@@ -0,0 +1,104 @@
+package com.tmobile.cloud.gcprules.cloudsql;
+
+import com.tmobile.cloud.awsrules.utils.CommonTestUtils;
+import com.tmobile.cloud.awsrules.utils.PacmanUtils;
+import com.tmobile.cloud.gcprules.utils.GCPUtils;
+import com.tmobile.pacman.commons.PacmanSdkConstants;
+import com.tmobile.pacman.commons.exception.InvalidInputException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.tmobile.cloud.gcprules.utils.TestUtils.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({PacmanUtils.class, GCPUtils.class})
+@PowerMockIgnore({"javax.net.ssl.*", "javax.management.*","jdk.internal.reflect.*"})
+public class EnableSKipShowDatabaseFlagRuleTest {
+
+    @InjectMocks
+    DisableOrEnableDBFlagsRule disableOrEnableDBFlagsRule;
+    @Before
+    public void setUp() {
+        mockStatic(PacmanUtils.class);
+        mockStatic(GCPUtils.class);
+    }
+
+    @Test
+    public void executeSuccessTest() throws Exception {
+
+
+        when(PacmanUtils.getPacmanHost(anyString())).thenReturn("host");
+        when(GCPUtils.getHitsArrayFromEs(anyObject(), anyObject())).thenReturn(getHitjsonArrayForMySqlDatabase());
+
+        when(PacmanUtils.createAnnotation(anyString(), anyObject(), anyString(), anyString(), anyString())).thenReturn(CommonTestUtils.getAnnotation("123"));
+        when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString())).thenReturn(
+                true);
+        assertThat(disableOrEnableDBFlagsRule.execute(getMapString("r_123 "), getMapString("r_123 ")).getStatus(), is(PacmanSdkConstants.STATUS_SUCCESS));
+
+    }
+
+    @Test
+    public void executeFailureTest() throws Exception {
+
+
+        when(PacmanUtils.getPacmanHost(anyString())).thenReturn("host");
+        when(GCPUtils.getHitsArrayFromEs(anyObject(), anyObject())).thenReturn(getFailurejsonArrayForMysSqlDatabase());
+
+        when(PacmanUtils.createAnnotation(anyString(), anyObject(), anyString(), anyString(), anyString())).thenReturn(CommonTestUtils.getAnnotation("123"));
+        when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString())).thenReturn(
+                true);
+        assertThat(disableOrEnableDBFlagsRule.execute(getMapString("r_123 "), getMapString("r_123 ")).getStatus(), is(PacmanSdkConstants.STATUS_FAILURE));
+    }
+
+    @Test
+    public void executeFailureTestWithInvalidInputException() throws Exception {
+
+        when(PacmanUtils.getPacmanHost(anyString())).thenReturn("host");
+        when(GCPUtils.getHitsArrayFromEs(anyObject(), anyObject())).thenReturn(getFailurejsonArrayForMysSqlDatabase());
+
+        when(PacmanUtils.createAnnotation(anyString(), anyObject(), anyString(), anyString(), anyString())).thenReturn(CommonTestUtils.getAnnotation("123"));
+        when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString())).thenReturn(
+                false);
+        assertThatThrownBy(() -> disableOrEnableDBFlagsRule.execute(getMapString("r_123 "), getMapString("r_123 "))).isInstanceOf(InvalidInputException.class);
+    }
+
+    public static Map<String, String> getMapString(String passRuleResourceId) {
+        Map<String, String> commonMap = new HashMap<>();
+        commonMap.put("executionId", "1234");
+        commonMap.put("_resourceid", passRuleResourceId);
+        commonMap.put("severity", "medium");
+        commonMap.put("ruleCategory", "security");
+        commonMap.put("accountid", "12345");
+        commonMap.put("ruleId", "Enable_skip_show_database_for_MySQL_Server_DB_Instances");
+        commonMap.put("policyId", "Enable_skip_show_database_for_MySQL_Server_DB_Instances");
+        commonMap.put("policyVersion", "version-1");
+        commonMap.put("dataBaseType", "MYSQLSERVER");
+        commonMap.put("dbFlagName", "skip_show_database");
+        commonMap.put("dbFlagValue", "on");
+        commonMap.put("description", "Ensure that the skip_show_database database flag is enabled for your Google Cloud MySQL database instances in order to prevent users from using the SHOW DATABASES statement if they don't have this privilege");
+        commonMap.put("violationReason", "skip_show _database flag is not enabled for mysql database instance");
+
+        return commonMap;
+    }
+
+    @Test
+    public void getHelpTextTest() {
+        assertThat(disableOrEnableDBFlagsRule.getHelpText(), is(notNullValue()));
+    }
+}
