f371ce2d3d052d09822cbc141f1fef0cd3754486
Anjali Madhavi Nakirikanti <anjali-madhavi.nakirikanti@paladincloud.io>
2022-10-07 14:04:26 +0530

    feat: Enable IP Forwarding for Compute Engine Instance

--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/constants/PacmanRuleConstants.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/constants/PacmanRuleConstants.java
@@ -580,4 +580,6 @@ public class PacmanRuleConstants {
     public static final String EMAIL="email";
     public static final String EMAIL_PATTERN="-compute@developer.gserviceaccount.com";
     public static final String CONFIDENTIAL_COMPUTING="confidentialComputing";
+    public  static  final String CAN_IP_FORWARD="canIPForward";
+    public static final String GKE_CLUSTER_NAME="goog-gke-node";
 }
--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/gcprules/vminstance/IPFowardingRule.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/gcprules/vminstance/IPFowardingRule.java
@@ -1,7 +1,8 @@
-package com.tmobile.cloud.gcprules.projectRules;
+package com.tmobile.cloud.gcprules.vminstance;
 
 import com.amazonaws.util.StringUtils;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.tmobile.cloud.awsrules.utils.PacmanUtils;
 import com.tmobile.cloud.constants.PacmanRuleConstants;
@@ -20,13 +21,13 @@ import org.slf4j.MDC;
 
 import java.util.*;
 
-@PacmanRule(key = "enable-cloud-asset-api",desc = "Ensure cloud asset api is enabled",severity = PacmanRuleConstants.MEDIUM,category = PacmanSdkConstants.SECURITY)
-public class EnableCloudAssetApiRule extends BaseRule {
-    private static final Logger logger = LoggerFactory.getLogger(EnableCloudAssetApiRule.class);
-
+@PacmanRule(key = "ip-forward-rule", desc = "disable ip forwarding for GCE instance and enable for GKE instance", severity = PacmanSdkConstants.SEV_MEDIUM, category = PacmanSdkConstants.SECURITY)
+public class IPFowardingRule  extends BaseRule {
+    private static final Logger logger = LoggerFactory.getLogger(IPFowardingRule.class);
+    public String violationReason="";
     @Override
     public RuleResult execute(Map<String, String> ruleParam, Map<String, String> resourceAttributes) {
-        logger.debug("========Cloud asset inventory level rule started=========");
+        logger.debug("======== IPFowarding Rule started=========");
         Annotation annotation = null;
 
         String resourceId = ruleParam.get(PacmanRuleConstants.RESOURCE_ID);
@@ -34,17 +35,17 @@ public class EnableCloudAssetApiRule extends BaseRule {
         String category = ruleParam.get(PacmanRuleConstants.CATEGORY);
 
         String vmEsURL = CommonUtils.getEnvVariableValue(PacmanSdkConstants.ES_URI_ENV_VAR_NAME);
+
         if (Boolean.FALSE.equals(PacmanUtils.doesAllHaveValue(severity, category, vmEsURL))) {
             logger.info(PacmanRuleConstants.MISSING_CONFIGURATION);
             throw new InvalidInputException(PacmanRuleConstants.MISSING_CONFIGURATION);
         }
 
         if (!StringUtils.isNullOrEmpty(vmEsURL)) {
-            vmEsURL = vmEsURL + "/gcp_project/project/_search";
+            vmEsURL = vmEsURL + "/gcp_vminstance/_search";
         }
         logger.debug("========vmEsURL URL after concatenation param {}  =========", vmEsURL);
-
-        boolean isCloudAssetApiEnabled = false;
+        boolean isIPfowardRulePassed = false;
 
         MDC.put("executionId", ruleParam.get("executionId"));
         MDC.put("ruleId", ruleParam.get(PacmanSdkConstants.RULE_ID));
@@ -52,22 +53,24 @@ public class EnableCloudAssetApiRule extends BaseRule {
         if (!StringUtils.isNullOrEmpty(resourceId)) {
 
             Map<String, Object> mustFilter = new HashMap<>();
+            mustFilter.put(PacmanUtils.convertAttributetoKeyword(PacmanRuleConstants.RESOURCE_ID), resourceId);
+            mustFilter.put(PacmanRuleConstants.LATEST, true);
 
             try {
-                isCloudAssetApiEnabled = checkCloudAssetApiEnabled(vmEsURL, mustFilter);
-                if (!isCloudAssetApiEnabled) {
+                isIPfowardRulePassed = checkIPForwardRule(vmEsURL, mustFilter);
+                if (!isIPfowardRulePassed) {
                     List<LinkedHashMap<String, Object>> issueList = new ArrayList<>();
                     LinkedHashMap<String, Object> issue = new LinkedHashMap<>();
 
                     annotation = Annotation.buildAnnotation(ruleParam, Annotation.Type.ISSUE);
-                    annotation.put(PacmanSdkConstants.DESCRIPTION, "Ensure Cloud Asset Inventory  enabled for a project");
+                    annotation.put(PacmanSdkConstants.DESCRIPTION, "Ensure that IP Forwarding feature is not enabled at the Google Compute Engine instance level for security and compliance reasons and Ensure IP Forwarding feature Disabled for GKE instance. ");
                     annotation.put(PacmanRuleConstants.SEVERITY, severity);
                     annotation.put(PacmanRuleConstants.CATEGORY, category);
 
-                    issue.put(PacmanRuleConstants.VIOLATION_REASON, "Cloud Asset inventory Disabled for  a project ");
+                    issue.put(PacmanRuleConstants.VIOLATION_REASON, violationReason);
                     issueList.add(issue);
                     annotation.put("issueDetails", issueList.toString());
-                    logger.debug("========Cloud Asset inventory Disabled for  a project {} : =========", annotation);
+                    logger.debug("========violation Reason {} : =========", annotation);
                     return new RuleResult(PacmanSdkConstants.STATUS_FAILURE, PacmanRuleConstants.FAILURE_MESSAGE, annotation);
                 }
 
@@ -76,37 +79,48 @@ public class EnableCloudAssetApiRule extends BaseRule {
             }
         }
 
-        logger.debug("========OCloud Asset inventory enabled for  a project=========");
+        logger.debug("========Rule  ended with sucess ms=========");
         return new RuleResult(PacmanSdkConstants.STATUS_SUCCESS, PacmanRuleConstants.SUCCESS_MESSAGE);
     }
-    private boolean checkCloudAssetApiEnabled(String vmEsURL, Map<String, Object> mustFilter) throws Exception {
-        JsonArray hitsJsonArray = GCPUtils.getHitsArrayFromEs(vmEsURL, mustFilter);
-        logger.debug("========verify CloudAssetApiEnabled rule started========= {}",hitsJsonArray.size());
 
+    private boolean checkIPForwardRule(String vmEsURL, Map<String, Object> mustFilter) throws Exception {
+        logger.debug("========DefaultServiceAccountUsageRule started=========");
+        JsonArray hitsJsonArray = GCPUtils.getHitsArrayFromEs(vmEsURL, mustFilter);
         boolean validationResult = false;
         if (hitsJsonArray.size() > 0) {
-            JsonObject ProjectdataJson = (JsonObject) ((JsonObject) hitsJsonArray.get(0))
+            JsonObject vmInstanceObject = (JsonObject) ((JsonObject) hitsJsonArray.get(0))
                     .get(PacmanRuleConstants.SOURCE);
 
-            logger.debug("Validating the data item: {}", ProjectdataJson.toString());
+            logger.debug("Validating the data item: {}", vmInstanceObject.toString());
 
-            JsonObject cloudAssetJSON = null;
+            boolean canIPForward=true;
+            boolean isGKECluster=false;
+            if(vmInstanceObject.get(PacmanRuleConstants.CAN_IP_FORWARD) !=null){
+                 canIPForward=vmInstanceObject.get(PacmanRuleConstants.CAN_IP_FORWARD).getAsBoolean();
+                violationReason="IP Forward Enabled for  GCE  Instance";
 
-            if(ProjectdataJson.get(PacmanRuleConstants.CLOUD_ASSET)!=null){
-                cloudAssetJSON=ProjectdataJson.get(PacmanRuleConstants.CLOUD_ASSET).getAsJsonObject();
+
             }
-
-
-            if (cloudAssetJSON != null) {
-               String isCloudAssetApiEnabled= cloudAssetJSON.get(PacmanRuleConstants.STATE).getAsString();
-                if(isCloudAssetApiEnabled.equalsIgnoreCase(PacmanRuleConstants.ENABLED)){
-                    validationResult=true;
-                }
-
-
-            } else {
+            else {
                 logger.info(PacmanRuleConstants.RESOURCE_DATA_NOT_FOUND);
             }
+            if(vmInstanceObject.get(PacmanRuleConstants.TAGS).getAsJsonObject()!=null){
+                JsonObject tags=vmInstanceObject.get(PacmanRuleConstants.TAGS).getAsJsonObject();
+                if(tags.get(PacmanRuleConstants.GKE_CLUSTER_NAME)!=null && tags.get(PacmanRuleConstants.GKE_CLUSTER_NAME).getAsString()==""){
+                    violationReason="IP Forward Disabled for GKE Cluster Instance";
+                    isGKECluster=true;
+
+
+                }
+
+            }
+            if(canIPForward==false && isGKECluster==false){
+                validationResult=true;
+            } else if (canIPForward==true && isGKECluster==true) {
+
+                validationResult=true;
+            }
+
 
         } else {
             logger.info(PacmanRuleConstants.RESOURCE_DATA_NOT_FOUND);
@@ -117,6 +131,6 @@ public class EnableCloudAssetApiRule extends BaseRule {
 
     @Override
     public String getHelpText() {
-        return "Cloud asset api rule";
+        return "disable ip forwarding for GCE instance and enable for GKE instance";
     }
 }
