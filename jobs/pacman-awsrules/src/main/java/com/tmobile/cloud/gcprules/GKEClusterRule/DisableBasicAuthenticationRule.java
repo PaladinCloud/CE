d3ae79b38067f485f983ae046b7256bceaf2bc66
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2022-11-09 12:30:23 +0530

    fix: changed logic for version comparison

--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/gcprules/GKEClusterRule/DisableBasicAuthenticationRule.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/gcprules/GKEClusterRule/DisableBasicAuthenticationRule.java
@@ -98,7 +98,8 @@ public class DisableBasicAuthenticationRule extends BaseRule {
             String username = gkeCluster.getAsJsonPrimitive("username").getAsString();
             String password = gkeCluster.getAsJsonPrimitive("password").getAsString();
             String version=gkeCluster.getAsJsonPrimitive("version").getAsString().substring(0,4);
-            if(versionCompare(version,"1.19")<0 &&!username.isEmpty() && !password.isEmpty())
+            float versionFloat=Float.parseFloat(version);
+            if(versionFloat<1.19 &&!username.isEmpty() && !password.isEmpty())
             {
                 validationResult=false;
             }
@@ -106,45 +107,6 @@ public class DisableBasicAuthenticationRule extends BaseRule {
         }
 
         return validationResult;
-    }
-    // Method to compare two versions. Returns 1 if v2 is smaller, -1 if v1 is smaller, 0 if equal
-    static int versionCompare(String v1, String v2)
-    {
-        // vnum stores each numeric part of version
-        int vnum1 = 0;
-        int vnum2 = 0;
-
-        // loop until both String are processed
-        for (int i = 0, j = 0; (i < v1.length()
-                || j < v2.length());) {
-            // Storing numeric part of
-            // version 1 in vnum1
-            while (i < v1.length()
-                    && v1.charAt(i) != '.') {
-                vnum1 = vnum1 * 10 + (v1.charAt(i) - '0');
-                i++;
-            }
-
-            // storing numeric part
-            // of version 2 in vnum2
-            while (j < v2.length()
-                    && v2.charAt(j) != '.') {
-                vnum2 = vnum2 * 10 + (v2.charAt(j) - '0');
-                j++;
-            }
-
-            if (vnum1 > vnum2)
-                return 1;
-            if (vnum2 > vnum1)
-                return -1;
-
-            // if equal, reset variables and
-            // go for next numeric part
-            vnum1 = vnum2 = 0;
-            i++;
-            j++;
-        }
-        return 0;
     }
 
     @Override
