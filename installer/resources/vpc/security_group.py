efb99ad57b43b2c2227acadce3a0f9ae0a8e5d82
Nithin <nithin@zemosolabs.com>
2022-11-28 12:37:11 +0530

    inbound changes in  sg to restrict unwanted traffic

--- installer/resources/vpc/security_group.py
+++ installer/resources/vpc/security_group.py
@@ -6,19 +6,109 @@ class InfraSecurityGroupResource(SecurityGroupResource):
     name = ""
     vpc_id = Settings.get('VPC')['ID']
 
-    ingress = [
-        {
-            'from_port': 0,
-            'to_port': 0,
-            'protocol': "-1",
+    ingress = [                           #https and internal alb 
+            {
+            'from_port': 443,
+            'to_port': 443,
+            'protocol': "tcp",
             'cidr_blocks': Settings.get('VPC')['CIDR_BLOCKS'],
             'ipv6_cidr_blocks': [],
             'prefix_list_ids': [],
             'description': "",
             'self': False,
-            'security_groups': []
-        }
-    ]
+            'security_groups': []   
+            }
+        ,
+            {
+            'from_port': 80,
+            'to_port': 80,
+            'protocol': "tcp",
+            'cidr_blocks': Settings.get('VPC')['CIDR_BLOCKS'],
+            'ipv6_cidr_blocks': [],
+            'prefix_list_ids': [],
+            'description': "",
+            'self': False,
+            'security_groups': []   
+            }
+        ,
+            {
+            'from_port': 3306,
+            'to_port': 3306,
+            'protocol': "tcp",
+            'cidr_blocks': Settings.get('VPC')['CIDR_BLOCKS'],
+            'ipv6_cidr_blocks': [],
+            'prefix_list_ids': [],
+            'description': "",
+            'self': False,
+            'security_groups': []   
+            }
+        ]
+    if Settings.get('ALB_PROTOCOL') == "HTTP" and Settings.MAKE_ALB_INTERNAL == True:
+         ingress = [
+            {
+            'from_port': 80,
+            'to_port': 80,
+            'protocol': "tcp",
+            'cidr_blocks':  Settings.get('VPC')['CIDR_BLOCKS'],
+            'ipv6_cidr_blocks': [],
+            'prefix_list_ids': [],
+            'description': "",
+            'self': False,
+            'security_groups': []   
+            }
+        ,
+            {
+            'from_port': 3306,
+            'to_port': 3306,
+            'protocol': "tcp",
+            'cidr_blocks': Settings.get('VPC')['CIDR_BLOCKS'],
+            'ipv6_cidr_blocks': [],
+            'prefix_list_ids': [],
+            'description': "",
+            'self': False,
+            'security_groups': []   
+            }
+        ]
+
+    if Settings.get('ALB_PROTOCOL') == "HTTPS" and Settings.MAKE_ALB_INTERNAL == False:
+        ingress = [
+            {
+            'from_port': 443,
+            'to_port': 443,
+            'protocol': "tcp",
+            'cidr_blocks': ["0.0.0.0/0"],
+            'ipv6_cidr_blocks': [],
+            'prefix_list_ids': [],
+            'description': "",
+            'self': False,
+            'security_groups': []   
+            }
+        ,
+            {
+            'from_port': 80,
+            'to_port': 80,
+            'protocol': "tcp",
+            'cidr_blocks': ["0.0.0.0/0"],
+            'ipv6_cidr_blocks': [],
+            'prefix_list_ids': [],
+            'description': "",
+            'self': False,
+            'security_groups': []   
+            }
+        ,
+            {
+            'from_port': 3306,
+            'to_port': 3306,
+            'protocol': "tcp",
+            'cidr_blocks': Settings.get('VPC')['CIDR_BLOCKS'],
+            'ipv6_cidr_blocks': [],
+            'prefix_list_ids': [],
+            'description': "",
+            'self': False,
+            'security_groups': []   
+            }
+        ]
+
 
     egress = [
         {
