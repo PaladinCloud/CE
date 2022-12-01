55211abd9e8cb5153595b1c5cd7008752d418755
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2022-12-01 12:26:13 +0530

    fix : missing rule class

---
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/azurerules/Kubernetes/EnableClusterMonitoring.java
@@ -0,0 +1,115 @@
+package com.tmobile.cloud.azurerules.Kubernetes;
+
+import com.amazonaws.util.StringUtils;
+import com.google.common.collect.HashMultimap;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.tmobile.cloud.awsrules.utils.PacmanUtils;
+import com.tmobile.cloud.awsrules.utils.RulesElasticSearchRepositoryUtil;
+import com.tmobile.cloud.constants.PacmanRuleConstants;
+import com.tmobile.pacman.commons.PacmanSdkConstants;
+import com.tmobile.pacman.commons.exception.InvalidInputException;
+import com.tmobile.pacman.commons.exception.RuleExecutionFailedExeption;
+import com.tmobile.pacman.commons.rule.Annotation;
+import com.tmobile.pacman.commons.rule.BaseRule;
+import com.tmobile.pacman.commons.rule.PacmanRule;
+import com.tmobile.pacman.commons.rule.RuleResult;
+import com.tmobile.pacman.commons.utils.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+@PacmanRule(key = "enable-cluster-monitoring-for-kubernetes", desc = "Enable azure AKS cluster monitoring", severity = PacmanSdkConstants.SEV_MEDIUM, category = PacmanSdkConstants.SECURITY)
+public class EnableClusterMonitoring extends BaseRule {
+    private static final Logger logger = LoggerFactory.getLogger(EnableClusterMonitoring.class);
+    @Override
+    public RuleResult execute(Map<String, String> ruleParam, Map<String, String> resourceAttributes) {
+        logger.info("Executing EnableClusterMonitoring..");
+        String severity = ruleParam.get(PacmanRuleConstants.SEVERITY);
+        String category = ruleParam.get(PacmanRuleConstants.CATEGORY);
+        String successMSG = ruleParam.get(PacmanRuleConstants.SUCCESS);
+        String failureMsg = ruleParam.get(PacmanRuleConstants.VIOLATION_REASON);
+        if (!PacmanUtils.doesAllHaveValue(severity, category)) {
+            logger.info(PacmanRuleConstants.MISSING_CONFIGURATION);
+            throw new InvalidInputException(PacmanRuleConstants.MISSING_CONFIGURATION);
+        }
+        String esUrl = CommonUtils.getEnvVariableValue(PacmanSdkConstants.ES_URI_ENV_VAR_NAME);
+        String url = CommonUtils.getEnvVariableValue(PacmanSdkConstants.ES_URI_ENV_VAR_NAME);
+        if (!StringUtils.isNullOrEmpty(url)) {
+            esUrl = url + "/azure_kubernetes/_search";
+        }
+        boolean isValid = false;
+        String resourceId = ruleParam.get(PacmanRuleConstants.RESOURCE_ID);
+        if (!StringUtils.isNullOrEmpty(resourceId)) {
+            Map<String, Object> mustFilter = new HashMap<>();
+            mustFilter.put(PacmanUtils.convertAttributetoKeyword(PacmanRuleConstants.RESOURCE_ID), resourceId);
+            mustFilter.put(PacmanRuleConstants.LATEST, true);
+            try {
+                isValid = checkifClusterMonitoringEnabledForKubernetes(esUrl, mustFilter);
+                if (!isValid) {
+                    List<LinkedHashMap<String, Object>> issueList = new ArrayList<>();
+                    LinkedHashMap<String, Object> issue = new LinkedHashMap<>();
+                    Annotation annotation = null;
+                    annotation = Annotation.buildAnnotation(ruleParam, Annotation.Type.ISSUE);
+                    annotation.put(PacmanSdkConstants.DESCRIPTION,
+                            failureMsg);
+                    annotation.put(PacmanRuleConstants.SEVERITY, severity);
+                    annotation.put(PacmanRuleConstants.CATEGORY, category);
+                    issue.put(PacmanRuleConstants.VIOLATION_REASON,
+                            ruleParam.get(PacmanRuleConstants.RULE_ID) + failureMsg + " Violation Found!");
+                    issueList.add(issue);
+                    annotation.put(PacmanRuleConstants.ISSUE_DETAILS, issueList.toString());
+                    logger.debug(
+                            failureMsg);
+                    return new RuleResult(PacmanSdkConstants.STATUS_FAILURE, PacmanRuleConstants.FAILURE_MESSAGE,
+                            annotation);
+                }
+            } catch (Exception e) {
+                throw new RuleExecutionFailedExeption("unable to determine" + e);
+            }
+        }
+        logger.debug(successMSG);
+        return new RuleResult(PacmanSdkConstants.STATUS_SUCCESS, PacmanRuleConstants.SUCCESS_MESSAGE);
+
+    }
+
+    private boolean checkifClusterMonitoringEnabledForKubernetes(String esUrl, Map<String, Object> mustFilter) throws Exception {
+        logger.info("Validating the resource data from elastic search. ES URL:{}, FilterMap : {}", esUrl, mustFilter);
+
+        boolean validationResult = false;
+        JsonParser parser = new JsonParser();
+        JsonObject resultJson = RulesElasticSearchRepositoryUtil.getQueryDetailsFromES(esUrl, mustFilter,
+                new HashMap<>(), HashMultimap.create(), null, 0, new HashMap<>(), null, null);
+        logger.debug("Data fetched from elastic search. Response JSON: {}", resultJson);
+
+        if (resultJson != null && resultJson.has(PacmanRuleConstants.HITS)) {
+            String hitsString = resultJson.get(PacmanRuleConstants.HITS).toString();
+            logger.debug("hit content in result json: {}", hitsString);
+            JsonObject hitsJson = JsonParser.parseString(hitsString).getAsJsonObject();
+            JsonArray hitsJsonArray = hitsJson.getAsJsonObject().get(PacmanRuleConstants.HITS).getAsJsonArray();
+            if (hitsJsonArray.size() > 0) {
+                JsonObject jsonDataItem = (JsonObject) ((JsonObject) hitsJsonArray.get(0))
+                        .get(PacmanRuleConstants.SOURCE);
+                JsonObject properties = jsonDataItem.getAsJsonObject("properties");
+                JsonObject addonProfiles=properties.getAsJsonObject("addonProfiles");
+                if (addonProfiles.has("omsAgent")) {
+                    JsonObject omsAgent = addonProfiles.get("omsAgent").getAsJsonObject();
+                    if (omsAgent.has("config")) {
+                        JsonObject config = omsAgent.getAsJsonObject("config");
+                        boolean enabled=omsAgent.get("enabled").getAsBoolean();
+                        if (config.has("logAnalyticsWorkspaceResourceID") && enabled) {
+                            validationResult = true;
+                        }
+                    }
+                }
+            }
+        }
+        return validationResult;
+    }
+    @Override
+    public String getHelpText() {
+        return "Enable azure AKS cluster monitoring";
+    }
+}
