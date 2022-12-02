2d4a4e2f42d10eec30d40e530bf0bcd642e3d639
kushZemoso <kushagra.jain@zemosolabs.com>
2022-12-02 11:41:13 +0530

    feat: Avoid Assigning Service Roles to IAM Users at project level.

---
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/gcprules/iam/AvoidAssigningServiceRolesRule.java
@@ -0,0 +1,103 @@
+package com.tmobile.cloud.gcprules.iam;
+
+import com.amazonaws.util.StringUtils;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.tmobile.cloud.awsrules.utils.PacmanUtils;
+import com.tmobile.cloud.constants.PacmanRuleConstants;
+import com.tmobile.cloud.gcprules.utils.GCPUtils;
+import com.tmobile.pacman.commons.PacmanSdkConstants;
+import com.tmobile.pacman.commons.exception.InvalidInputException;
+import com.tmobile.pacman.commons.exception.RuleExecutionFailedExeption;
+import com.tmobile.pacman.commons.rule.Annotation;
+import com.tmobile.pacman.commons.rule.BaseRule;
+import com.tmobile.pacman.commons.rule.PacmanRule;
+import com.tmobile.pacman.commons.rule.RuleResult;
+import com.tmobile.pacman.commons.utils.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.*;
+
+@PacmanRule(key = "avoid-assigning-service-roles-to-iam-user-at-project-level", desc = "Ensure that Service Account User (iam.serviceAccountUser) and Service Account Token Creator (iam.serviceAccountTokenCreator) roles to a user for a specific service account rather than assigning the role to a user at project level.", severity = PacmanSdkConstants.SEV_MEDIUM, category = PacmanSdkConstants.SECURITY)
+public class AvoidAssigningServiceRolesRule extends BaseRule {
+
+    private static final Logger logger = LoggerFactory.getLogger(AvoidAssigningServiceRolesRule.class);
+    @Override
+    public RuleResult execute(Map<String, String> ruleParam, Map<String, String> resourceAttributes) {
+        logger.debug("executing AvoidAssigningServiceRolesRule....");
+        Annotation annotation = null;
+
+        String resourceId = ruleParam.get(PacmanRuleConstants.RESOURCE_ID);
+        String severity = ruleParam.get(PacmanRuleConstants.SEVERITY);
+        String category = ruleParam.get(PacmanRuleConstants.CATEGORY);
+        String vmEsURL = CommonUtils.getEnvVariableValue(PacmanSdkConstants.ES_URI_ENV_VAR_NAME);
+        if (Boolean.FALSE.equals(PacmanUtils.doesAllHaveValue(severity, category, vmEsURL))) {
+            logger.info(PacmanRuleConstants.MISSING_CONFIGURATION);
+            throw new InvalidInputException(PacmanRuleConstants.MISSING_CONFIGURATION);
+        }
+
+        if (!StringUtils.isNullOrEmpty(vmEsURL)) {
+            vmEsURL = vmEsURL + "/gcp_iamusers/_search";
+        }
+        logger.debug("========gcp_iamusers URL after concatenation param {}  =========", vmEsURL);
+
+        boolean isRolesAssigned = false;
+
+        MDC.put("executionId", ruleParam.get("executionId"));
+        MDC.put("ruleId", ruleParam.get(PacmanSdkConstants.RULE_ID));
+
+        if (!StringUtils.isNullOrEmpty(resourceId)) {
+            Map<String, Object> mustFilter = new HashMap<>();
+            mustFilter.put(PacmanUtils.convertAttributetoKeyword(PacmanRuleConstants.RESOURCE_ID), resourceId);
+            mustFilter.put(PacmanRuleConstants.LATEST, true);
+            try {
+                isRolesAssigned= checkIfServiceRolesAreAssignedToIAMUsers(vmEsURL, mustFilter);
+                if (isRolesAssigned) {
+                    List<LinkedHashMap<String, Object>> issueList = new ArrayList<>();
+                    LinkedHashMap<String, Object> issue = new LinkedHashMap<>();
+
+                    annotation = Annotation.buildAnnotation(ruleParam, Annotation.Type.ISSUE);
+                    annotation.put(PacmanSdkConstants.DESCRIPTION, ruleParam.get(PacmanRuleConstants.DESCRIPTION));
+                    annotation.put(PacmanRuleConstants.SEVERITY, severity);
+                    annotation.put(PacmanRuleConstants.CATEGORY, category);
+                    issue.put(PacmanRuleConstants.VIOLATION_REASON, ruleParam.get(PacmanRuleConstants.VIOLATION_REASON));
+                    issueList.add(issue);
+                    annotation.put("issueDetails", issueList.toString());
+                    logger.debug("========AvoidAssigningServiceRolesRule ended with an annotation {} : =========", annotation);
+                    return new RuleResult(PacmanSdkConstants.STATUS_FAILURE, PacmanRuleConstants.FAILURE_MESSAGE,
+                            annotation);
+                }
+
+            } catch (Exception exception) {
+                throw new RuleExecutionFailedExeption(exception.getMessage());
+            }
+        }
+        logger.debug("success::AvoidAssigningServiceRolesRule");
+        return new RuleResult(PacmanSdkConstants.STATUS_SUCCESS, PacmanRuleConstants.SUCCESS_MESSAGE);
+    }
+
+    private boolean checkIfServiceRolesAreAssignedToIAMUsers(String vmEsURL, Map<String, Object> mustFilter) throws Exception {
+        logger.debug("inside checkIfServiceRolesAreAssigned of AvoidAssigningServiceRolesRule");
+        boolean validationResult=false;
+        JsonArray hitsJsonArray = GCPUtils.getHitsArrayFromEs(vmEsURL, mustFilter);
+        if (hitsJsonArray.size() > 0) {
+            JsonObject iamUser = (JsonObject) ((JsonObject) hitsJsonArray.get(0))
+                    .get(PacmanRuleConstants.SOURCE);
+            JsonArray roles = iamUser.getAsJsonObject().get(PacmanRuleConstants.ROLES).getAsJsonArray();
+            for (JsonElement role : roles) {
+                if (role.getAsString().equalsIgnoreCase("roles/iam.serviceAccountUser")  || role.getAsString().equalsIgnoreCase("roles/iam.serviceAccountTokenCreator") ) {
+                    validationResult = true;
+                }
+            }
+        }
+        return validationResult;
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Ensure that Service Account User (iam.serviceAccountUser) and Service Account Token Creator (iam.serviceAccountTokenCreator) roles to a user for a specific service account rather than assigning the role to a user at project level.";
+    }
+}
---
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/gcprules/iam/AvoidAssigningServiceRolesRuleTest.java
@@ -0,0 +1,155 @@
+package com.tmobile.cloud.gcprules.iam;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.tmobile.cloud.awsrules.utils.CommonTestUtils;
+import com.tmobile.cloud.awsrules.utils.PacmanUtils;
+import com.tmobile.cloud.gcprules.utils.GCPUtils;
+import com.tmobile.pacman.commons.PacmanSdkConstants;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({PacmanUtils.class, GCPUtils.class})
+public class AvoidAssigningServiceRolesRuleTest {
+
+    @InjectMocks
+    AvoidAssigningServiceRolesRule avoidAssigningServiceRolesRule;
+
+    @Before
+    public void setUp() {
+        mockStatic(PacmanUtils.class);
+        mockStatic(GCPUtils.class);
+    }
+
+    @Test
+    public void executeSuccessTest() throws Exception {
+
+        when(PacmanUtils.getPacmanHost(anyString())).thenReturn("host");
+        when(GCPUtils.getHitsArrayFromEs(anyObject(), anyObject())).thenReturn(getHitsJsonForAvoidAssigningServiceRoles());
+
+        when(PacmanUtils.createAnnotation(anyString(), anyObject(), anyString(), anyString(), anyString()))
+                .thenReturn(CommonTestUtils.getAnnotation("123"));
+        when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString())).thenReturn(
+                true);
+        assertThat(avoidAssigningServiceRolesRule.execute(getMapString("r_123 "), getMapString("r_123 ")).getStatus(),
+                is(PacmanSdkConstants.STATUS_SUCCESS));
+
+    }
+
+    private JsonArray getHitsJsonForAvoidAssigningServiceRoles(){
+        Gson gson = new Gson();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("_source", gson.fromJson(
+                "{\n" +
+                        "          \"discoveryDate\": \"2022-11-29 18:00:00+0530\",\n" +
+                        "          \"_cloudType\": \"GCP\",\n" +
+                        "          \"region\": null,\n" +
+                        "          \"id\": \"central-run-349616_anjali-madhavi.nakirikanti@paladincloud.io\",\n" +
+                        "          \"projectName\": \"Paladin Cloud\",\n" +
+                        "          \"projectId\": \"central-run-349616\",\n" +
+                        "          \"userId\": \"user:anjali-madhavi.nakirikanti@paladincloud.io\",\n" +
+                        "          \"email\": \"anjali-madhavi.nakirikanti@paladincloud.io\",\n" +
+                        "          \"roles\": [\n" +
+                        "            \"roles/cloudkms.cryptoKeyEncrypterDecrypter\",\n" +
+                        "            \"roles/resourcemanager.projectIamAdmin\",\n" +
+                        "            \"roles/cloudkms.cryptoKeyDecrypter\",\n" +
+                        "            \"roles/resourcemanager.organizationAdmin\",\n" +
+                        "            \"roles/owner\"\n" +
+                        "          ],\n" +
+                        "          \"discoverydate\": \"2022-11-29 18:00:00+0530\",\n" +
+                        "          \"_resourceid\": \"central-run-349616_anjali-madhavi.nakirikanti@paladincloud.io\",\n" +
+                        "          \"_docid\": \"central-run-349616_anjali-madhavi.nakirikanti@paladincloud.io\",\n" +
+                        "          \"_entity\": \"true\",\n" +
+                        "          \"_entitytype\": \"iamusers\",\n" +
+                        "          \"firstdiscoveredon\": \"2022-11-29 18:00:00+0530\",\n" +
+                        "          \"latest\": true,\n" +
+                        "          \"_loaddate\": \"2022-11-29 12:49:00+0000\"\n" +
+                        "        }",
+                JsonElement.class));
+        JsonArray array = new JsonArray();
+        array.add(jsonObject);
+        return array;
+    }
+
+    @Test
+    public void executeFailureTest() throws Exception {
+
+        when(PacmanUtils.getPacmanHost(anyString())).thenReturn("host");
+        when(GCPUtils.getHitsArrayFromEs(anyObject(), anyObject()))
+                .thenReturn(getHitsJsonForAvoidAssigningServiceRolesFailure());
+
+        when(PacmanUtils.createAnnotation(anyString(), anyObject(), anyString(), anyString(), anyString()))
+                .thenReturn(CommonTestUtils.getAnnotation("123"));
+        when(PacmanUtils.doesAllHaveValue(anyString(), anyString(), anyString())).thenReturn(
+                true);
+        assertThat(avoidAssigningServiceRolesRule.execute(getMapString("r_123 "), getMapString("r_123 ")).getStatus(),
+                is(PacmanSdkConstants.STATUS_FAILURE));
+    }
+
+    private JsonArray getHitsJsonForAvoidAssigningServiceRolesFailure(){
+        Gson gson = new Gson();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("_source", gson.fromJson(
+                "{\n" +
+                        "          \"discoveryDate\": \"2022-11-29 18:00:00+0530\",\n" +
+                        "          \"_cloudType\": \"GCP\",\n" +
+                        "          \"region\": null,\n" +
+                        "          \"id\": \"central-run-349616_aishwarya.kulkarni@paladincloud.io\",\n" +
+                        "          \"projectName\": \"Paladin Cloud\",\n" +
+                        "          \"projectId\": \"central-run-349616\",\n" +
+                        "          \"userId\": \"user:aishwarya.kulkarni@paladincloud.io\",\n" +
+                        "          \"email\": \"aishwarya.kulkarni@paladincloud.io\",\n" +
+                        "          \"roles\": [\n" +
+                        "            \"roles/owner\",\n" +
+                        "            \"roles/iam.serviceAccountTokenCreator\",\n" +
+                        "            \"roles/iam.serviceAccountUser\"\n" +
+                        "          ],\n" +
+                        "          \"discoverydate\": \"2022-11-29 18:00:00+0530\",\n" +
+                        "          \"_resourceid\": \"central-run-349616_aishwarya.kulkarni@paladincloud.io\",\n" +
+                        "          \"_docid\": \"central-run-349616_aishwarya.kulkarni@paladincloud.io\",\n" +
+                        "          \"_entity\": \"true\",\n" +
+                        "          \"_entitytype\": \"iamusers\",\n" +
+                        "          \"firstdiscoveredon\": \"2022-11-29 18:00:00+0530\",\n" +
+                        "          \"latest\": true,\n" +
+                        "          \"_loaddate\": \"2022-11-29 12:49:00+0000\"\n" +
+                        "        }",
+                JsonElement.class));
+        JsonArray array = new JsonArray();
+        array.add(jsonObject);
+        return array;
+    }
+
+    public static Map<String, String> getMapString(String passRuleResourceId) {
+        Map<String, String> commonMap = new HashMap<>();
+        commonMap.put("executionId", "1234");
+        commonMap.put("_resourceid", passRuleResourceId);
+        commonMap.put("severity", "medium");
+        commonMap.put("ruleCategory", "security");
+        commonMap.put("accountid", "12345");
+        commonMap.put("violationReason", "Assigned Service roles to IAM users at project level.");
+        return commonMap;
+    }
+
+    @Test
+    public void getHelpTextTest() {
+        assertThat(avoidAssigningServiceRolesRule.getHelpText(), is(notNullValue()));
+    }
+}
