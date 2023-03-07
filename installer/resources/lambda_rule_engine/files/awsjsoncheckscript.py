f72c9fd4cf4f85ea99b6800fc1e2becebf1a6935
Nithin <nithin.r@paladincloud.io>
2023-03-07 13:06:00 +0530

    chore/json-check

---
+++ installer/resources/lambda_rule_engine/files/awsjsoncheckscript.py
@@ -0,0 +1,15 @@
+
+import json
+
+# Opening JSON file
+f = open('rule_engine_cloudwatch_aws_rules.json')
+
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+
+for i in data:
+    if len(i) >= 64:
+        raise Exception(i, "cannot be longer than 64 characters")
+        
+print("json file valid")
