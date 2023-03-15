813ece808860105fe7a25c918881cc43e4b89047
Arifa Shaik <shaik.arifa@zemosolabs.com>
2023-03-15 18:42:05 +0530

    fix: worked on comments

--- webapp/src/app/pacman-features/secondary-components/policy-violation-desc/policy-violation-desc.component.ts
+++ webapp/src/app/pacman-features/secondary-components/policy-violation-desc/policy-violation-desc.component.ts
@@ -38,7 +38,7 @@ export class PolicyViolationDescComponent implements OnInit {
   selectedDomain: string;
   accordionData: any;
   labelData: any;
-  Params: any;
+  violationId: string;
   showAccordion = true;
   testData;
   public agAndDomain = {};
@@ -68,7 +68,7 @@ export class PolicyViolationDescComponent implements OnInit {
 
   ngOnInit() {
     this.urlToRedirect = this.router.routerState.snapshot.url;
-    this.Params = this.activatedRoute.snapshot.params;
+    this.violationId = this.activatedRoute.snapshot.params.issueId;
     this.processDataForAccordion(this.violationData);
   }
   /**
