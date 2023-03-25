fee830f155eef7442cba0d4f47b5c4eff6ff4062
Maxim Cheremkhin <astralfestival@gmail.com>
2023-03-25 20:29:34 +0300

    feat: using mat-calendar instead of dropdown

--- webapp/src/app/shared/date-selection/date-selection.component.ts
+++ webapp/src/app/shared/date-selection/date-selection.component.ts
@@ -1,97 +1,51 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+    DateRange,
+    DefaultMatCalendarRangeStrategy,
+    MAT_DATE_RANGE_SELECTION_STRATEGY,
+} from '@angular/material/datepicker';
 
 @Component({
-  selector: "app-date-selection",
-  templateUrl: "./date-selection.component.html",
-  styleUrls: ["./date-selection.component.css"],
+    selector: 'app-date-selection',
+    templateUrl: './date-selection.component.html',
+    styleUrls: ['./date-selection.component.css'],
+    providers: [
+        {
+            provide: MAT_DATE_RANGE_SELECTION_STRATEGY,
+            useClass: DefaultMatCalendarRangeStrategy,
+        },
+    ],
 })
-export class DateSelection implements OnInit {
-    years = [];
-    allMonths = [
-        { text: 'January', id: 0 },
-        { text: 'February', id: 1 },
-        { text: 'March', id: 2 },
-        { text: 'April', id: 3 },
-        { text: 'May', id: 4 },
-        { text: 'June', id: 5 },
-        { text: 'July', id: 6 },
-        { text: 'August', id: 7 },
-        { text: 'September', id: 8 },
-        { text: 'October', id: 9 },
-        { text: 'November', id: 10 },
-        { text: 'December', id: 11 }
-    ];
-  fromDays = [];
-  toDays = [];
-  today: Date = new Date();
-  fromDate: Date = new Date();
-  toDate: Date = new Date();
+export class DateSelectionComponent implements OnInit {
+    today = new Date();
 
-  @Output() datesSelected = new EventEmitter<any>();
+    fromDate: Date = new Date();
+    toDate: Date = new Date();
 
-  ngOnInit() {
-    for (let i = 2022; i <= new Date().getFullYear(); i++) {
-      this.years.push(i);
+    selectedRange?: DateRange<Date>;
+
+    @Output() datesSelected = new EventEmitter<{ from: Date; to: Date }>();
+
+    ngOnInit() {}
+
+    customDateSelected() {
+        this.datesSelected.emit({
+            from: this.selectedRange.start,
+            to: this.selectedRange.end,
+        });
     }
-  }
 
-  private getNumberOfDays = function (year, monthId: any) {
-    const isLeap = ((year % 4) === 0 && ((year % 100) !== 0 || (year % 400) === 0));
-    return [31, (isLeap ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][monthId];
-  };
-
-  onSelectYear(date: Date, selectedYear){
-    date.setFullYear(selectedYear);
-  }
-
-  getMonthId(selectedMonth){
-    let monthId = 0;
-    for (let id = 0; id < this.allMonths.length; id++) {
-      if (this.allMonths[id].text == selectedMonth) {
-        monthId = id;
-      }
+    selectedDateChange(event: Date) {
+        if (!this.selectedRange?.start || this.selectedRange?.end) {
+            this.selectedRange = new DateRange<Date>(event, null);
+        } else {
+            const start = this.selectedRange.start;
+            const end = event;
+            if (end < start) {
+                this.selectedRange = new DateRange<Date>(end, start);
+            } else {
+                this.selectedRange = new DateRange<Date>(start, end);
+            }
+        }
     }
-    return monthId;
-  }
-
-  getMonth(date: Date){
-    let selectedMonth = "";
-    for (let id = 0; id < this.allMonths.length; id++) {
-      if (this.allMonths[id].id == date.getMonth()) {
-        selectedMonth = this.allMonths[id].text;
-      }
-    }
-    return selectedMonth;
-  }
-
-  onSelectMonth(date: Date, selectedMonth: any, allMonthDays) {
-    const monthDays: any = [];
-    let monthId = this.getMonthId(selectedMonth);
-    
-    const daysCount = this.getNumberOfDays(date.getFullYear(), monthId);
-    for (let dayNo = 1; dayNo <= daysCount; dayNo++) {
-      monthDays.push({ id: dayNo, text: dayNo.toString() });
-    }
-    if(allMonthDays=="from"){
-      this.fromDays = monthDays;
-    }else{ 
-      this.toDays = monthDays;
-    }
-    date.setMonth(monthId);
-  }
-
-  onSelectDay(date: Date, selectedDay: any) {
-    date.setDate(selectedDay);    
-  }
-
-  customDateSelected(from: Date, to: Date){
-    if(to<=from || to>this.today){
-      return;
-    }
-    let event = {
-      from: from,
-      to: to
-    }
-    this.datesSelected.emit(event);
-  }
 }
