3a1d16250ae54b6c25e5ec50a5465c13e8ed2832
Maxim Cheremkhin <astralfestival@gmail.com>
2023-03-25 17:09:25 +0300

    fix: violations by severity tile legend order

--- webapp/src/app/pacman-features/services/issues-history.service.ts
+++ webapp/src/app/pacman-features/services/issues-history.service.ts
@@ -3,9 +3,9 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); You may not use
  * this file except in compliance with the License. A copy of the License is located at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * or in the "license" file accompanying this file. This file is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or
  * implied. See the License for the specific language governing permissions and
@@ -24,25 +24,35 @@ import { LoggerService } from '../../shared/services/logger.service';
 import { ErrorHandlingService } from '../../shared/services/error-handling.service';
 import { map } from 'rxjs/operators';
 
+export interface IssueHistoryItem {
+    key: string;
+    values: IssueHistoryItemValue[];
+}
+interface IssueHistoryItemValue {
+    date: Date;
+    value: number;
+    'zero-value': boolean;
+}
+
 @Injectable()
 export class IssuesHistoryService {
+    constructor(
+        @Inject(HttpService) private httpService: HttpService,
+        private logger: LoggerService,
+        private errorHandling: ErrorHandlingService,
+    ) {}
 
-    constructor (
-                 @Inject(HttpService) private httpService: HttpService,
-                 private logger: LoggerService,
-                 private errorHandling: ErrorHandlingService) { }
-
-    private combinedData: any = [];
-    getData(url, method, payload, queryParameters): Observable<any> {
+    getData(url, method, payload, queryParameters) {
         try {
-            return this.httpService.getHttpResponse(url, method, payload, queryParameters)
-                .pipe(map(response => {
-                    if (method === 'POST') {
-                        return this.massagePostResponse(response);
-                    } else {
-                        return this.massageGetResponse(response);
-                    }
-                }));
+            return this.httpService
+                .getHttpResponse(url, method, payload, queryParameters)
+                .pipe(
+                    map((response) =>
+                        method === 'POST'
+                            ? this.massagePostResponse(response)
+                            : this.massageGetResponse(response),
+                    ),
+                );
         } catch (error) {
             this.logger.log('error', error);
             this.errorHandling.handleJavascriptError(error);
@@ -53,10 +63,7 @@ export class IssuesHistoryService {
         const url = environment.severity.url;
         const method = environment.severity.method;
         try {
-            return this.httpService.getHttpResponse(url, method, {}, queryParameters)
-                    .pipe(map(response => {
-                        return response;
-                    }));
+            return this.httpService.getHttpResponse(url, method, {}, queryParameters);
         } catch (error) {
             this.errorHandling.handleJavascriptError(error);
         }
@@ -64,31 +71,30 @@ export class IssuesHistoryService {
 
     massageGetResponse(data) {
         const apiResponse = Object.assign({}, data);
-        const values = [];
-        let formattedObject = {};
+        const values: IssueHistoryItemValue[] = [];
         const allDates = Object.keys(apiResponse).sort();
         for (let i = 0; i < allDates.length; i++) {
             // Additional property 'zero-value' being added to keep track of zero values, as the zero values are replaced
             // with 1 during plotting graph with a log axis (as [log 0]  is infinity)
             const obj = {
-                'date' : new Date(allDates[i]),
-                'value' : apiResponse[allDates[i]],
-                'zero-value' : apiResponse[allDates[i]] === 0 ? true : false
+                date: new Date(allDates[i]),
+                value: apiResponse[allDates[i]],
+                'zero-value': apiResponse[allDates[i]] === 0,
             };
             values.push(obj);
         }
-        formattedObject = {
-            'key'    : 'total',
-            'values' : values
+        const formattedObject: IssueHistoryItem = {
+            key: 'total',
+            values,
         };
         return formattedObject;
     }
 
     massagePostResponse(data) {
-        const finalData = [];
+        const finalData: IssueHistoryItem[] = [];
         let apiResponse = data.data.response['issues_info'];
         if (data.data.response['issues_info'] && apiResponse.constructor.name === 'Array') {
-                apiResponse =  apiResponse.sort(function(a, b){
+            apiResponse = apiResponse.sort((a, b) => {
                 a = new Date(a.date);
                 b = new Date(b.date);
                 const x = a < b ? -1 : 1;
@@ -99,18 +105,17 @@ export class IssuesHistoryService {
             types.splice(types.indexOf('date'), 1);
             types.forEach(type => {
                 const values = [];
-                let formattedObject = {};
                 apiResponse.forEach(details => {
                     const obj = {
-                        'date' : new Date(details['date']),
-                        'value': details[type],
-                        'zero-value': details[type] === 0 ? true : false
+                        date: new Date(details['date']),
+                        value: details[type],
+                        'zero-value': details[type] === 0,
                     };
                     values.push(obj);
                 });
-                formattedObject = {
-                    'key'    : type,
-                    'values' : values
+                const formattedObject: IssueHistoryItem = {
+                    key: type,
+                    values,
                 };
                 if (type.toLowerCase() !== 'overall' && type.toLowerCase() !== 'total') {
                     if (type.toLowerCase() !== 'security' && type.toLowerCase() !== 'governance') {
@@ -123,6 +128,4 @@ export class IssuesHistoryService {
         }
         return finalData;
     }
-
 }
-
