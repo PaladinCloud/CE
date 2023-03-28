ac3e155273d7acdce22d8a2fd521ac222c7835e8
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2023-03-28 16:49:52 +0530

    fix: JUnits and Rule logic for elastic search upgrade

--- jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/elasticsearch/ESNodetoNodeEncryptionRuleTest.java
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/elasticsearch/ESNodetoNodeEncryptionRuleTest.java
@@ -4,10 +4,14 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyObject;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import com.tmobile.pacman.commons.policy.Annotation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -22,7 +26,7 @@ import com.tmobile.pacman.commons.policy.PolicyResult;
 
 @PowerMockIgnore("jdk.internal.reflect.*")
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ PacmanUtils.class })
+@PrepareForTest({ PacmanUtils.class , Annotation.class})
 public class ESNodetoNodeEncryptionRuleTest {
 
 	@InjectMocks
@@ -47,7 +51,8 @@ public class ESNodetoNodeEncryptionRuleTest {
 		ruleParam.put(PacmanSdkConstants.POLICY_ID, "esEncryptionAtRestRule");
 		ruleParam.put(PacmanRuleConstants.CATEGORY, PacmanSdkConstants.SECURITY);
 		ruleParam.put(PacmanRuleConstants.SEVERITY, PacmanSdkConstants.SEV_HIGH);
-
+		mockStatic(Annotation.class);
+		when(Annotation.buildAnnotation(anyObject(),anyObject())).thenReturn(getMockAnnotation());
 		Map<String, String> resourceAttribute = getResourceForWithOutEncryption("ES1234");
 		PolicyResult ruleResult = esNodetoNodeEncryptionRule.execute(ruleParam, resourceAttribute);
 		assertEquals(PacmanSdkConstants.STATUS_FAILURE, ruleResult.getStatus());
@@ -63,7 +68,7 @@ public class ESNodetoNodeEncryptionRuleTest {
 
 		Map<String, String> resourceAttribute = getResourceWithLowerVersion("ES1234");
 		PolicyResult ruleResult = esNodetoNodeEncryptionRule.execute(ruleParam, resourceAttribute);
-		assertEquals(PacmanSdkConstants.STATUS_FAILURE, ruleResult.getStatus());
+		assertEquals(PacmanSdkConstants.STATUS_SUCCESS, ruleResult.getStatus());
 	}
 
 	@Test
@@ -102,6 +107,15 @@ public class ESNodetoNodeEncryptionRuleTest {
 		clusterObj.put("elasticsearchversion", "5.5");
 
 		return clusterObj;
+	}
+	private Annotation getMockAnnotation() {
+		Annotation annotation=new Annotation();
+		annotation.put(PacmanSdkConstants.POLICY_NAME,"Mock policy name");
+		annotation.put(PacmanSdkConstants.POLICY_ID, "Mock policy id");
+		annotation.put(PacmanSdkConstants.POLICY_VERSION, "Mock policy version");
+		annotation.put(PacmanSdkConstants.RESOURCE_ID, "Mock resource id");
+		annotation.put(PacmanSdkConstants.TYPE, "Mock type");
+		return annotation;
 	}
 
 }
