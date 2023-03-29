626aa583e067f3738d063406e5fcec24f3a7f63a
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2023-03-29 12:02:57 +0530

    fix: JUnits fixed for elb

--- jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/elb/UnrestrctedClassicElbSecurityGroupTest.java
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/elb/UnrestrctedClassicElbSecurityGroupTest.java
@@ -19,9 +19,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.anyListOf;
-import static org.mockito.Matchers.anySetOf;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.*;
+import static org.mockito.Matchers.anyObject;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
@@ -31,6 +30,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.tmobile.pacman.commons.PacmanSdkConstants;
+import com.tmobile.pacman.commons.policy.Annotation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -46,7 +47,7 @@ import com.tmobile.pacman.commons.exception.InvalidInputException;
 import com.tmobile.pacman.commons.exception.RuleExecutionFailedExeption;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ PacmanUtils.class,PacmanEc2Utils.class})
+@PrepareForTest({ PacmanUtils.class,PacmanEc2Utils.class, Annotation.class})
 public class UnrestrctedClassicElbSecurityGroupTest {
 
     @InjectMocks
@@ -55,10 +56,11 @@ public class UnrestrctedClassicElbSecurityGroupTest {
     @Test
     public void executeTest() throws Exception {
         mockStatic(PacmanUtils.class);
+		mockStatic(Annotation.class);
         when(PacmanUtils.doesAllHaveValue(anyString(),anyString(),anyString(),anyString(),anyString())).thenReturn(true);
         
         when(PacmanUtils.getPacmanHost(anyString())).thenReturn("host");
-        
+		when(Annotation.buildAnnotation(anyObject(),anyObject())).thenReturn(getMockAnnotation());
         when(PacmanUtils.getSecurityBroupIdByElb(anyString(),anyString(),anyString(),anyString())).thenReturn(CommonTestUtils.getListSecurityGroupId());
         when(PacmanUtils.getClassicLBListenerPortsByName(anyString(),anyString(),anyString(),anyString())).thenReturn(getListenerPorts("123"));
         when(PacmanUtils.checkUnrestrictedSgAccessForClassicLB(anySetOf(GroupIdentifier.class),anyListOf(Listener.class),anyString())).thenReturn(getSecurityGroups("r_123"));
@@ -79,8 +81,16 @@ public class UnrestrctedClassicElbSecurityGroupTest {
         assertThatThrownBy(
                 () -> unrestrctedClassicElbSecurityGroup.execute(getMapString("r_123 "),getMapString("r_123 "))).isInstanceOf(InvalidInputException.class);
     }
-  
 
+	private Annotation getMockAnnotation() {
+		Annotation annotation=new Annotation();
+		annotation.put(PacmanSdkConstants.POLICY_NAME,"Mock policy name");
+		annotation.put(PacmanSdkConstants.POLICY_ID, "Mock policy id");
+		annotation.put(PacmanSdkConstants.POLICY_VERSION, "Mock policy version");
+		annotation.put(PacmanSdkConstants.RESOURCE_ID, "Mock resource id");
+		annotation.put(PacmanSdkConstants.TYPE, "Mock type");
+		return annotation;
+	}
 	@Test
     public void getHelpTextTest(){
         assertThat(unrestrctedClassicElbSecurityGroup.getHelpText(), is(notNullValue()));
