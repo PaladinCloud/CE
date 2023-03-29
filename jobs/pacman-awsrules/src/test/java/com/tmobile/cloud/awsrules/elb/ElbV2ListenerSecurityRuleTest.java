be8485c0da50685faedbe2da2b7ac79716bde35b
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2023-03-29 12:02:57 +0530

    fix: JUnits fixed for elb

--- jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/elb/ElbV2ListenerSecurityRuleTest.java
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/elb/ElbV2ListenerSecurityRuleTest.java
@@ -19,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
@@ -28,6 +29,8 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import com.tmobile.pacman.commons.PacmanSdkConstants;
+import com.tmobile.pacman.commons.policy.Annotation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -40,7 +43,7 @@ import com.tmobile.pacman.commons.exception.InvalidInputException;
 import com.tmobile.pacman.commons.exception.RuleExecutionFailedExeption;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ PacmanUtils.class,PacmanEc2Utils.class})
+@PrepareForTest({ PacmanUtils.class,PacmanEc2Utils.class, Annotation.class})
 public class ElbV2ListenerSecurityRuleTest {
 
     @InjectMocks
@@ -49,8 +52,9 @@ public class ElbV2ListenerSecurityRuleTest {
     @Test
     public void executeTest() throws Exception {
         mockStatic(PacmanUtils.class);
+        mockStatic(Annotation.class);
         when(PacmanUtils.doesAllHaveValue(anyString(),anyString())).thenReturn(true);
-        
+        when(Annotation.buildAnnotation(anyObject(),anyObject())).thenReturn(getMockAnnotation());
         when(PacmanUtils.getPacmanHost(anyString())).thenReturn("host");
         when(PacmanUtils.checkElbUsingSecuredProtocol(anyString(),anyString())).thenReturn(geValidSetString("r_123 "));
         assertThat(elbV2ListenerSecurityRule.execute(getMapString("r_123 "),getMapString("r_123 ")), is(notNullValue()));
@@ -67,7 +71,15 @@ public class ElbV2ListenerSecurityRuleTest {
         assertThatThrownBy(
                 () -> elbV2ListenerSecurityRule.execute(getMapString("r_123 "),getMapString("r_123 "))).isInstanceOf(InvalidInputException.class);
     }
-    
+    private Annotation getMockAnnotation() {
+        Annotation annotation=new Annotation();
+        annotation.put(PacmanSdkConstants.POLICY_NAME,"Mock policy name");
+        annotation.put(PacmanSdkConstants.POLICY_ID, "Mock policy id");
+        annotation.put(PacmanSdkConstants.POLICY_VERSION, "Mock policy version");
+        annotation.put(PacmanSdkConstants.RESOURCE_ID, "Mock resource id");
+        annotation.put(PacmanSdkConstants.TYPE, "Mock type");
+        return annotation;
+    }
     @Test
     public void getHelpTextTest(){
         assertThat(elbV2ListenerSecurityRule.getHelpText(), is(notNullValue()));
