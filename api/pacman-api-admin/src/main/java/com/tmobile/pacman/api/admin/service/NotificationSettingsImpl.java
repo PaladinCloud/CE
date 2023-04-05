94984965d36c6ed63a1e7030c70b8a867fa641ea
Raviteja Mandala <raviteja.mandala@zemosolabs.com>
2023-04-05 20:03:04 +0530

    feat: new api for notification settings and configurations

--- api/pacman-api-admin/src/main/java/com/tmobile/pacman/api/admin/service/NotificationSettingsImpl.java
+++ api/pacman-api-admin/src/main/java/com/tmobile/pacman/api/admin/service/NotificationSettingsImpl.java
@@ -5,6 +5,7 @@ import com.tmobile.pacman.api.commons.repo.PacmanRdsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -14,6 +15,10 @@ public class NotificationSettingsImpl implements NotificationSettings{
 
     @Autowired
     PacmanRdsRepository pacmanRdsRepository;
+
+    @Value("${notification.to.emailid:}")
+    String toEmailId;
+
     private static final Logger log = LoggerFactory.getLogger(NotificationSettingsImpl.class);
     @Override
     public Map<String,Object> getNotificationSettings() {
@@ -90,4 +95,28 @@ public class NotificationSettingsImpl implements NotificationSettings{
             pacmanRdsRepository.update(deleteNotifMappingsQuery.toString());
         }
     }
+
+
+    @Override
+    public Map<String,Object> getNotificationSettingsAndConfigs() {
+        Map<String,Object> notfSettingsAndConfigMap =new HashMap<>();
+        Map<String,Object> notificationTypesMap = getNotificationSettings();
+        for(String notificationType : notificationTypesMap.keySet()){
+            Map<String,Object> channelsMap = (Map<String,Object>) notificationTypesMap.get(notificationType);
+            for(String channel : channelsMap.keySet()){
+                Map<String,Object> channelConfigMap = new HashMap<>();
+                channelConfigMap.put("isOn",Integer.parseInt(channelsMap.get(channel).toString()));
+                if("email".equalsIgnoreCase(channel)){
+                    channelConfigMap.put("toAddress",Arrays.asList(toEmailId));
+                }
+                else{
+                    channelConfigMap.put("toAddress",Collections.emptyList());
+                }
+                channelsMap.put(channel,channelConfigMap);
+            }
+        }
+        notfSettingsAndConfigMap.put("settings",notificationTypesMap);
+        return notfSettingsAndConfigMap;
+    }
+
 }
