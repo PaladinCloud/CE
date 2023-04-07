4683c9e2d44243aa4d5ff7c422a96efff5b2bf0a
rnithinpaladin <nithin.r@paladincloud.io>
2023-04-07 11:07:18 +0530

    chore/lambda_deployment_es_log_lambda

--- installer/resources/iam/base_role_policy.py
+++ installer/resources/iam/base_role_policy.py
@@ -2,14 +2,13 @@ from core.terraform.resources.aws import iam
 from resources.iam.base_role import BaseRole
 from resources.iam.ecs_role import ECSRole
 from resources.iam.batch_role import BatchRole
-from core.config import Settings
 
 
 class BaseRolePolicyDocument(iam.IAMPolicyDocumentData):
     statement = [
         {
             'actions': ["sts:AssumeRole"],
-            'resources': "arn:aws:iam::" + Settings.ACCOUNT_ID + ":role/" + Settings.RESOURCE_NAME_PREFIX
+            'resources': [BaseRole.get_output_attr('arn')]
         }
     ]
 
@@ -27,4 +26,8 @@ class BatchBaseRolePolicyAttach(iam.IAMRolePolicyAttachmentResource):
 
 class EcsBaseRolePolicyAttach(iam.IAMRolePolicyAttachmentResource):
     role = ECSRole.get_output_attr('name')
+    policy_arn = BaseRolePolicy.get_output_attr('arn')
+
+class BaseRoleBasePolicyAttach(iam.IAMRolePolicyAttachmentResource):
+    role = BaseRole.get_output_attr('name')
     policy_arn = BaseRolePolicy.get_output_attr('arn')
