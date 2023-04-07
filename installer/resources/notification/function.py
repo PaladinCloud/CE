5e68e7bca2e1369a0980aad32f51edae2f14a66d
rnithinpaladin <nithin.r@paladincloud.io>
2023-04-07 11:07:18 +0530

    chore/lambda_deployment_es_log_lambda

--- installer/resources/notification/function.py
+++ installer/resources/notification/function.py
@@ -1,10 +1,11 @@
 from core.terraform.resources.aws.aws_lambda import LambdaFunctionResource, LambdaPermission
+from resources.datastore.es import ESDomain
 from resources.iam.lambda_role import LambdaRole
 from core.config import Settings
 from resources.s3.bucket import BucketStorage
 from resources.pacbot_app.alb import ApplicationLoadBalancer
 from resources.notification.appsync import AppSyncNotification, ApiSyncIdKey
-from resources.notification.s3_upload import FetchNotificationFunctionJarFile, FetchNotificationFunctionJarFile, SendNotificationFunctionJarFile, InvokeNotificationFunctionJarFile, SEND_NOTIFICATION, TEMPLATE_NOTIFICATION, INAPP_NOTIFICATION_FILE_NAME, INVOKE_NOTIFICATION
+from resources.notification.s3_upload import FetchNotificationFunctionJarFile, FetchNotificationFunctionJarFile, SendNotificationFunctionJarFile, InvokeNotificationFunctionJarFile, SEND_NOTIFICATION, TEMPLATE_NOTIFICATION, INAPP_NOTIFICATION_FILE_NAME, INVOKE_NOTIFICATION, NOTIFICATION_LOG_TO_ES, GET_STAKEHOLDER_RESOURCES
 from core.terraform.resources.aws.sns import SNSResoures, SNSSubscription
 
 class NotificationSNS(SNSResoures):
@@ -45,7 +46,6 @@ class TemplateFormatterFunction(LambdaFunctionResource):
         }
     }
 
-
 class InvokeNotificationFunction(LambdaFunctionResource):
     function_name = TEMPLATE_NOTIFICATION
     role = LambdaRole.get_output_attr('arn')
@@ -75,14 +75,41 @@ class InAppNotificationFunction(LambdaFunctionResource):
     }
     DEPENDS_ON = [AppSyncNotification]
 
+class LogEsNotificationFunction(LambdaFunctionResource):
+    function_name = NOTIFICATION_LOG_TO_ES
+    role = LambdaRole.get_output_attr('arn')
+    handler =  "com.paladincloud.notification_log.LogNotificationToOpenSearch::handleRequest"
+    runtime = "java8"
+    s3_bucket = BucketStorage.get_output_attr('bucket')
+    s3_key = Settings.RESOURCE_NAME_PREFIX + "/v1/" + NOTIFICATION_LOG_TO_ES + ".jar"
+    environment = {
+        'variables': {
+            'AUTH_API_URL' :	"https://"+ Settings.COGNITO_DOMAIN + ".auth." + Settings.AWS_REGION + ".amazoncognito.com",
+            'CONFIG_SERVER_CREDENTIALS' : "dXNlcjpwYWNtYW4=",
+            'CONFIG_SERVICE_URL'	: ApplicationLoadBalancer.get_api_base_url() + "/api/config/rule/prd/latest",
+            'ES_URI' : ESDomain.get_http_url_with_port()
+        }
+    }
+    
 class NotificationSubscription(SNSSubscription):
     topic_arn = NotificationSNS.get_output_attr('arn')
     protocol = "lambda"
     endpoint =  TemplateFormatterFunction.get_output_attr('arn')
     # DEPENDS_ON = [NotificationSNS]
+class InAppSubscription(SNSSubscription):
+    topic_arn = NotificationSNS.get_output_attr('arn')
+    protocol = "lambda"
+    endpoint =  InAppNotificationFunction.get_output_attr('arn')
+    # DEPENDS_ON = [NotificationSNS]
 
 class EmailSubscription(SNSSubscription):
     topic_arn = EmailSNS.get_output_attr('arn')
     protocol = "lambda"
-    endpoint = SendNotificationFunction.get_output_attr('arn')
+    endpoint = InvokeNotificationFunction.get_output_attr('arn')
     # DEPENDS_ON = [EmailSNS]
+class EmailSubscription(SNSSubscription):
+    topic_arn = NotificationSNS.get_output_attr('arn')
+    protocol = "lambda"
+    endpoint = LogEsNotificationFunction.get_output_attr('arn')
+    # DEPENDS_ON = [EmailSNS]
+    
