35710d3b29890cc8844eab6b64fbed941f6c0f30
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2023-04-13 17:23:03 +0530

    feat: added columns for [PC2-197]

--- installer/resources/pacbot_app/files/DB.sql
+++ installer/resources/pacbot_app/files/DB.sql
@@ -454,6 +454,10 @@ CREATE TABLE IF NOT EXISTS  cf_Accounts(
 /* Insert one account */
 
 insert ignore into cf_Accounts values(concat(@ACCOUNT_NAME,''),concat(@ACCOUNT_ID,''),0,0,'configured',concat(@ACCOUNT_PLATFORM,''));
+/* Add columns createdby and createdTime */
+ALTER TABLE cf_Accounts
+ADD column  createdBy varchar(255) NULL DEFAULT 'userName' after platform,
+ADD column createdTime varchar(500) NULL DEFAULT 'dateTime' after createdBy;
 
 DELIMITER $$
 DROP PROCEDURE IF EXISTS alter_cf_target_table_add_display_name_if_not_exists $$
