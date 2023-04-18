41329d6a49119dc636f718e3a58605f4fdde8aaa
Maxim Cheremkhin <astralfestival@gmail.com>
2023-04-18 18:26:32 +0300

    feat(violation-notification): initial layout

--- webapp/src/app/pacman-features/modules/notifications/notification-details/violation-notification/violation-notification.component.ts
+++ webapp/src/app/pacman-features/modules/notifications/notification-details/violation-notification/violation-notification.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-violation-notification',
-  templateUrl: './violation-notification.component.html',
-  styleUrls: ['./violation-notification.component.css']
+    selector: 'app-violation-notification',
+    templateUrl: './violation-notification.component.html',
+    styleUrls: ['./violation-notification.component.css'],
 })
 export class ViolationNotificationComponent implements OnInit {
+    @Input() details: {
+        policyName: string;
+        policyNameLink: string;
+        issueId: string;
+        issueIdLink: string;
+        scanTime: Date;
+    };
 
-  constructor() { }
+    @Output() navigateTo = new EventEmitter<string>();
 
-  ngOnInit(): void {
-  }
+    constructor() {}
 
+    ngOnInit(): void {}
 }
