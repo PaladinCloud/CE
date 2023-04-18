b76b01c0f56afedc40b505b5ae29276c6d6fb2c5
rnithinpaladin <nithin.r@paladincloud.io>
2023-04-18 14:44:31 +0530

    CHORE/lambda_vpc_fix

--- installer/resources/notification/function.py
+++ installer/resources/notification/function.py
@@ -1,4 +1,5 @@
 from core.terraform.resources.aws.aws_lambda import LambdaFunctionResource, LambdaPermission, LambdaFunctionUrl
+from resources.vpc.security_group import InfraSecurityGroupResource
 from resources.pacbot_app.build_ui_and_api import BuildUiAndApis
 from resources.datastore.es import ESDomain
 from resources.iam.lambda_role import LambdaRole
@@ -13,8 +14,6 @@ INVOKE_NOTIFICATION = "notification-invoke-service"
 TEMPLATE_NOTIFICATION= "notification-template-formatter-service"
 NOTIFICATION_LOG_TO_ES = "notification-es-logging-service"
 SEND_NOTIFICATION = "notification-send-email-service"
-
-
 
 class NotificationSNS(SNSResources):
     name = "notification-event"
@@ -92,6 +91,8 @@ class LogEsNotificationFunction(LambdaFunctionResource):
     runtime = "java8"
     s3_bucket = BucketStorage.get_output_attr('bucket')
     s3_key = Settings.RESOURCE_NAME_PREFIX + NOTIFICATION_LOG_TO_ES + ".jar"
+    security_group_ids = Settings.get('VPC')['SUBNETS']
+    subnet_ids = [InfraSecurityGroupResource.get_output_attr('id')]
     environment = {
         'variables': {
             'AUTH_API_URL' :	"https://"+ Settings.COGNITO_DOMAIN + ".auth." + Settings.AWS_REGION + ".amazoncognito.com",
