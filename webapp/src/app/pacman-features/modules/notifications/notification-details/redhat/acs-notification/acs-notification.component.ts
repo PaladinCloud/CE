4816a2f1f205c200229668ba6670eeab6ec3d92b
Maxim Cheremkhin <astralfestival@gmail.com>
2023-04-19 17:53:03 +0300

    feat(notification-details): redhat acs notification layout

--- webapp/src/app/pacman-features/modules/notifications/notification-details/redhat/acs-notification/acs-notification.component.ts
+++ webapp/src/app/pacman-features/modules/notifications/notification-details/redhat/acs-notification/acs-notification.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-redhat-acs-notification',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./acs-notification.component.css'],
 })
 export class RedhatAcsNotificationComponent implements OnInit {
+    @Input() details: {
+        severity: string;
+        lastUpdated: Date;
+        goToViolation: string;
+        clusterName: string;
+        namespace: string;
+        description: string;
+        id: string;
+    };
+
     constructor() {}
 
     ngOnInit(): void {}
