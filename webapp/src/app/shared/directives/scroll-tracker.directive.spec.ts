fdbbbc0adbf4cb01c889aafcf4f6283bbc2909ed
Maxim Cheremkhin <astralfestival@gmail.com>
2023-04-21 17:09:43 +0300

    feat(scroll-state): initial scrolling state implementation

---
+++ webapp/src/app/shared/directives/scroll-tracker.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ScrollTrackerDirective } from './scroll-tracker.directive';
+
+describe('ScrollTrackerDirective', () => {
+    it('should create an instance', () => {
+        // const directive = new ScrollTrackerDirective();
+        // expect(directive).toBeTruthy();
+    });
+});
