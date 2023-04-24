40c43ee468cacb1cd4a2074f9a0148a5cd812270
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2023-04-24 19:20:46 +0530

    fix: [RSA2023-143]

--- jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/gcprules/loadbalancer/InsecureSSLCipherSuitesTest.java
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/gcprules/loadbalancer/InsecureSSLCipherSuitesTest.java
@@ -63,29 +63,51 @@ public class InsecureSSLCipherSuitesTest {
         Gson gson = new Gson();
         JsonObject jsonObject = new JsonObject();
         jsonObject.add("_source", gson.fromJson(
-                " {\n" +
-                        "          \"discoveryDate\": \"2022-12-08 17:00:00+0530\",\n" +
-                        "          \"_cloudType\": \"GCP\",\n" +
-                        "          \"region\": null,\n" +
-                        "          \"id\": \"4432585077905277180\",\n" +
-                        "          \"projectName\": null,\n" +
-                        "          \"projectId\": null,\n" +
-                        "          \"urlMap\": \"new-loadbalancer\",\n" +
-                        "          \"sslPolicyList\": [\n" +
-                        "            \"profile\":\"RESTRICTED\",\n" +
+                "{\n" +
+                        "          \"_cloudType\" : \"GCP\",\n" +
+                        "          \"region\" : \"\",\n" +
+                        "          \"id\" : \"8754306309307615551\",\n" +
+                        "          \"projectName\" : \"Paladin Cloud\",\n" +
+                        "          \"projectId\" : \"central-run-349616\",\n" +
+                        "          \"tags\" : null,\n" +
+                        "          \"urlMap\" : \"loadbalancer2\",\n" +
+                        "          \"targetHttpsProxy\" : [\n" +
+                        "            \"loadbalancer2-target-proxy-2\",\n" +
+                        "            \"loadbalancer2-target-proxy-4\"\n" +
                         "          ],\n" +
-                        "          \"targetHttpProxy\": [\n" +
-                        "            \"new-loadbalancer-target-proxy\"\n" +
+                        "          \"httpProxyDetailList\" : [\n" +
+                        "            {\n" +
+                        "              \"name\" : \"loadbalancer2-target-proxy-2\",\n" +
+                        "              \"hasCustomPolicy\" : true\n" +
+                        "            },\n" +
+                        "            {\n" +
+                        "              \"name\" : \"loadbalancer2-target-proxy-4\",\n" +
+                        "              \"hasCustomPolicy\" : false\n" +
+                        "            }\n" +
                         "          ],\n" +
-                        "          \"discoverydate\": \"2022-12-08 17:00:00+0530\",\n" +
-                        "          \"_resourceid\": \"4432585077905277180\",\n" +
-                        "          \"_docid\": \"4432585077905277180\",\n" +
-                        "          \"_entity\": \"true\",\n" +
-                        "          \"_entitytype\": \"gcploadbalancer\",\n" +
-                        "          \"firstdiscoveredon\": \"2022-12-07 17:00:00+0530\",\n" +
-                        "          \"latest\": true,\n" +
-                        "          \"_loaddate\": \"2022-12-08 11:48:00+0000\"\n" +
-                        "        }\n" ,
+                        "          \"logConfigEnabled\" : true,\n" +
+                        "          \"quicNegotiation\" : [\n" +
+                        "            true,\n" +
+                        "            false\n" +
+                        "          ],\n" +
+                        "          \"sslPolicyList\" : [\n" +
+                        "            {\n" +
+                        "              \"minTlsVersion\" : \"TLS_1_2\",\n" +
+                        "              \"profile\" : \"MODERN\",\n" +
+                        "              \"enabledFeatures\" : [ ]\n" +
+                        "            }\n" +
+                        "          ],\n" +
+                        "          \"discoverydate\" : \"2023-04-24 11:00:00+0000\",\n" +
+                        "          \"_resourceid\" : \"8754306309307615551\",\n" +
+                        "          \"_docid\" : \"8754306309307615551\",\n" +
+                        "          \"_entity\" : \"true\",\n" +
+                        "          \"_entitytype\" : \"gcploadbalancer\",\n" +
+                        "          \"docType\" : \"gcploadbalancer\",\n" +
+                        "          \"gcploadbalancer_relations\" : \"gcploadbalancer\",\n" +
+                        "          \"firstdiscoveredon\" : \"2023-04-24 11:00:00+0000\",\n" +
+                        "          \"latest\" : true,\n" +
+                        "          \"_loaddate\" : \"2023-04-24 11:14:00+0000\"\n" +
+                        "        }" ,
                 JsonElement.class));
         JsonArray array = new JsonArray();
         array.add(jsonObject);
@@ -111,29 +133,51 @@ public class InsecureSSLCipherSuitesTest {
         Gson gson = new Gson();
         JsonObject jsonObject = new JsonObject();
         jsonObject.add("_source", gson.fromJson(
-                " {\n"
-                        + "          \"discoveryDate\": \"2022-12-08 17:00:00+0530\",\n" +
-                        "          \"_cloudType\": \"GCP\",\n" +
-                        "          \"region\": null,\n" +
-                        "          \"id\": \"4432585077905277180\",\n" +
-                        "          \"projectName\": null,\n" +
-                        "          \"projectId\": null,\n" +
-                        "          \"urlMap\": \"loadbalancer2\",\n" +
-                        "          \"sslPolicyList\": [\n" +
-                        "            \"profile\":\"COMPATIBLE\",\n" +
+                "{\n" +
+                        "          \"_cloudType\" : \"GCP\",\n" +
+                        "          \"region\" : \"\",\n" +
+                        "          \"id\" : \"8754306309307615551\",\n" +
+                        "          \"projectName\" : \"Paladin Cloud\",\n" +
+                        "          \"projectId\" : \"central-run-349616\",\n" +
+                        "          \"tags\" : null,\n" +
+                        "          \"urlMap\" : \"loadbalancer2\",\n" +
+                        "          \"targetHttpsProxy\" : [\n" +
+                        "            \"loadbalancer2-target-proxy-2\",\n" +
+                        "            \"loadbalancer2-target-proxy-4\"\n" +
                         "          ],\n" +
-                        "          \"targetHttpProxy\": [\n" +
-                        "            \"new-loadbalancer-target-proxy\"\n" +
+                        "          \"httpProxyDetailList\" : [\n" +
+                        "            {\n" +
+                        "              \"name\" : \"loadbalancer2-target-proxy-2\",\n" +
+                        "              \"hasCustomPolicy\" : true\n" +
+                        "            },\n" +
+                        "            {\n" +
+                        "              \"name\" : \"loadbalancer2-target-proxy-4\",\n" +
+                        "              \"hasCustomPolicy\" : false\n" +
+                        "            }\n" +
                         "          ],\n" +
-                        "          \"discoverydate\": \"2022-12-08 17:00:00+0530\",\n" +
-                        "          \"_resourceid\": \"4432585077905277180\",\n" +
-                        "          \"_docid\": \"4432585077905277180\",\n" +
-                        "          \"_entity\": \"true\",\n" +
-                        "          \"_entitytype\": \"gcploadbalancer\",\n" +
-                        "          \"firstdiscoveredon\": \"2022-12-07 17:00:00+0530\",\n" +
-                        "          \"latest\": true,\n" +
-                        "          \"_loaddate\": \"2022-12-08 11:48:00+0000\"\n" +
-                        "      }",
+                        "          \"logConfigEnabled\" : true,\n" +
+                        "          \"quicNegotiation\" : [\n" +
+                        "            true,\n" +
+                        "            false\n" +
+                        "          ],\n" +
+                        "          \"sslPolicyList\" : [\n" +
+                        "            {\n" +
+                        "              \"minTlsVersion\" : \"TLS_1_2\",\n" +
+                        "              \"profile\" : \"CUSTOM\",\n" +
+                        "              \"enabledFeatures\" : [TLS_RSA_WITH_AES_128_GCM_SHA256]\n" +
+                        "            }\n" +
+                        "          ],\n" +
+                        "          \"discoverydate\" : \"2023-04-24 11:00:00+0000\",\n" +
+                        "          \"_resourceid\" : \"8754306309307615551\",\n" +
+                        "          \"_docid\" : \"8754306309307615551\",\n" +
+                        "          \"_entity\" : \"true\",\n" +
+                        "          \"_entitytype\" : \"gcploadbalancer\",\n" +
+                        "          \"docType\" : \"gcploadbalancer\",\n" +
+                        "          \"gcploadbalancer_relations\" : \"gcploadbalancer\",\n" +
+                        "          \"firstdiscoveredon\" : \"2023-04-24 11:00:00+0000\",\n" +
+                        "          \"latest\" : true,\n" +
+                        "          \"_loaddate\" : \"2023-04-24 11:14:00+0000\"\n" +
+                        "        }",
                 JsonElement.class));
         JsonArray array = new JsonArray();
         array.add(jsonObject);
