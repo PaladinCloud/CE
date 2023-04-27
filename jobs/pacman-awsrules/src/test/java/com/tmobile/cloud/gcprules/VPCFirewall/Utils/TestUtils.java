fa5c3d2fde4f21b515b8d43813bc735b932c2627
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2023-04-27 16:45:14 +0530

    fix: vpcfirewall  changes[MVP2023-170]

--- jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/gcprules/VPCFirewall/Utils/TestUtils.java
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/gcprules/VPCFirewall/Utils/TestUtils.java
@@ -10,38 +10,37 @@ public class TestUtils {
         Gson gson = new Gson();
         JsonObject jsonObject = new JsonObject();
         jsonObject.add("_source", gson.fromJson("{\n" +
-                "          \"_cloudType\": \"GCP\",\n" +
-                "          \"region\": \"us-west1-a\",\n" +
-                "          \"id\": \"8993151141438601059\",\n" +
-                "          \"projectName\": \"cool-bay-349411\",\n" +
-                "          \"name\": \"default-allow-http\",\n" +
-                "          \"direction\": \"INGRESS\",\n" +
-                "          \"sourceRanges\": [\n" +
+                "          \"_cloudType\" : \"GCP\",\n" +
+                "          \"region\" : null,\n" +
+                "          \"id\" : \"4715970846916771047\",\n" +
+                "          \"projectName\" : \"Paladin Cloud\",\n" +
+                "          \"projectId\" : \"central-run-349616\",\n" +
+                "          \"tags\" : null,\n" +
+                "          \"name\" : \"default-allow-http\",\n" +
+                "          \"direction\" : \"EGRESS\",\n" +
+                "          \"sourceRanges\" : [\n" +
+                "            \"0.0.0.0/0\"\n" +
+                "          ],\n" +
+                "          \"destinationRanges\" : [  \"0.0.0.0/0\" ],\n" +
+                "          \"allow\" : [\n" +
                 "            {\n" +
-                "              \".0.0.0/0\n" +
+                "              \"protocol\" : \"tcp\",\n" +
+                "              \"ports\" : [\n" +
+                "                \"80\"\n" +
+                "              ]\n" +
                 "            }\n" +
                 "          ],\n" +
-                "          \"allow\": [\n" +
-                "            {\n" +
-                "              \"protocol\": \"tcp\",\n" +
-                "  \"ports\": [\n" +
-                "            {\n" +
-                "              \".80\n" +
-                "            }\n" +
-                "          ],\n" + "            }\n" +
-                "          ],\n" +
-                "          \"disabled\": \"false\",\n" +
-                "              \"discoverydate\": null\n" +
-                "            }\n" +
-                "          ],\n" +
-                "          \"discoverydate\": \"2022-06-17 00:00:00+0000\",\n" +
-                "          \"_resourceid\": \"3078863054644366453\",\n" +
-                "          \"_docid\": \"8993151141438601059\",\n" +
-                "          \"_entity\": \"true\",\n" +
-                "          \"_entitytype\": \"vpcfirewall\",\n" +
-                "          \"firstdiscoveredon\": \"2022-06-16 13:00:00+0000\",\n" +
-                "          \"latest\": true,\n" +
-                "          \"_loaddate\": \"2022-06-17 00:12:00+0000\"\n" +
+                "          \"disabled\" : false,\n" +
+                "          \"discoverydate\" : \"2023-04-27 07:00:00+0000\",\n" +
+                "          \"_resourceid\" : \"default-allow-http\",\n" +
+                "          \"_docid\" : \"4715970846916771047\",\n" +
+                "          \"_entity\" : \"true\",\n" +
+                "          \"_entitytype\" : \"vpcfirewall\",\n" +
+                "          \"docType\" : \"vpcfirewall\",\n" +
+                "          \"vpcfirewall_relations\" : \"vpcfirewall\",\n" +
+                "          \"firstdiscoveredon\" : \"2023-04-27 07:00:00+0000\",\n" +
+                "          \"latest\" : true,\n" +
+                "          \"_loaddate\" : \"2023-04-27 07:16:00+0000\"\n" +
                 "        }", JsonElement.class));
 
         JsonArray array = new JsonArray();
@@ -53,34 +52,37 @@ public class TestUtils {
         Gson gson = new Gson();
         JsonObject jsonObject = new JsonObject();
         jsonObject.add("_source", gson.fromJson("{\n" +
-                "          \"_cloudType\": \"GCP\",\n" +
-                "          \"region\": \"us-west1-a\",\n" +
-                "          \"id\": \"8993151141438601059\",\n" +
-                "          \"projectName\": \"cool-bay-349411\",\n" +
-                "          \"name\": \"default-allow-http\",\n" +
-                "          \"direction\": \"INGRESS\",\n" +
-                "          \"allow\": [\n" +
-                "            {\n" +
-                "              \"protocol\": \"tcp\",\n" +
-                "  \"ports\": [\n" +
-                "            {\n" +
-                "              \".80\n" +
-                "            }\n" +
-                "          ],\n" + "            }\n" +
+                "          \"_cloudType\" : \"GCP\",\n" +
+                "          \"region\" : null,\n" +
+                "          \"id\" : \"4715970846916771047\",\n" +
+                "          \"projectName\" : \"Paladin Cloud\",\n" +
+                "          \"projectId\" : \"central-run-349616\",\n" +
+                "          \"tags\" : null,\n" +
+                "          \"name\" : \"default-allow-http\",\n" +
+                "          \"direction\" : \"INGRESS\",\n" +
+                "          \"sourceRanges\" : [\n" +
+                "            \"0.0.0.0/0\"\n" +
                 "          ],\n" +
-                "          \"disabled\": \"false\",\n" +
-                "              \"discoverydate\": null\n" +
+                "          \"destinationRanges\" : [ ],\n" +
+                "          \"allow\" : [\n" +
+                "            {\n" +
+                "              \"protocol\" : \"tcp\",\n" +
+                "              \"ports\" : [\n" +
+                "                \"1521\"\n" +
+                "              ]\n" +
                 "            }\n" +
                 "          ],\n" +
-                "          \"discoverydate\": \"2022-06-17 00:00:00+0000\",\n" +
-                "          \"_resourceid\": \"3078863054644366453\",\n" +
-                "          \"_docid\": \"8993151141438601059\",\n" +
-                "          \"_entity\": \"true\",\n" +
-                "          \"_entitytype\": \"vpcfirewall\",\n" +
-                "          \"firstdiscoveredon\": \"2022-06-16 13:00:00+0000\",\n" +
-                "          \"latest\": true,\n" +
-                "          \"_loaddate\": \"2022-06-17 00:12:00+0000\"\n" +
-
+                "          \"disabled\" : false,\n" +
+                "          \"discoverydate\" : \"2023-04-27 09:00:00+0000\",\n" +
+                "          \"_resourceid\" : \"default-allow-http\",\n" +
+                "          \"_docid\" : \"4715970846916771047\",\n" +
+                "          \"_entity\" : \"true\",\n" +
+                "          \"_entitytype\" : \"vpcfirewall\",\n" +
+                "          \"docType\" : \"vpcfirewall\",\n" +
+                "          \"vpcfirewall_relations\" : \"vpcfirewall\",\n" +
+                "          \"firstdiscoveredon\" : \"2023-04-27 09:00:00+0000\",\n" +
+                "          \"latest\" : true,\n" +
+                "          \"_loaddate\" : \"2023-04-27 09:10:00+0000\"\n" +
                 "        }", JsonElement.class));
 
         JsonArray array = new JsonArray();
