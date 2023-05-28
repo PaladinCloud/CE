e4c69ca1fbcae73575272777b06855c79e4cdc33
Maxim Cheremkhin <astralfestival@gmail.com>
2023-05-28 22:20:34 +0300

    feat(breadcrumbs): workflow service typings and small refactoring

--- webapp/src/app/core/services/workflow.service.ts
+++ webapp/src/app/core/services/workflow.service.ts
@@ -10,88 +10,111 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or
  * implied. See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 /*** Created by puneetbaser on 29/01/18. */
 
 import { Injectable } from '@angular/core';
-import { LoggerService } from '../../shared/services/logger.service';
-import { DataCacheService } from './data-cache.service';
-import { RouterUtilityService } from '../../shared/services/router-utility.service';
 import { ActivatedRouteSnapshot, Router } from '@angular/router';
+import { LoggerService } from 'src/app/shared/services/logger.service';
+import { RouterUtilityService } from 'src/app/shared/services/router-utility.service';
+import { DataCacheService } from './data-cache.service';
+
+interface Level {
+    [key: string]: LevelParams[];
+}
+
+interface LevelParams {
+    title: string | null;
+    url: string;
+    queryParams: { [key: string]: string };
+}
 
 @Injectable()
 export class WorkflowService {
+    private level: Level = {};
+    /* This would track which page user has clicked in a module */
+    private trackOpenedPageInAModule: { [key: string]: string } = {};
 
     constructor(
-
         private logger: LoggerService,
         private dataStore: DataCacheService,
         private routerUtilityService: RouterUtilityService,
         private router: Router,
-
     ) {}
 
-    private level = {};
-    /* This would track which page user has clicked in a module */
-    private trackOpenedPageInAModule = {};
-
-    addRouterSnapshotToLevel(routerSnapshot: ActivatedRouteSnapshot, currentLevel: number= 0, title=null) {
-
-        const urlPath = this.routerUtilityService.getFullUrlFromSnapshopt(routerSnapshot);
-
-        const queryParams = this.routerUtilityService.getQueryParametersFromSnapshot(routerSnapshot);
-
+    addRouterSnapshotToLevel(
+        routerSnapshot: ActivatedRouteSnapshot,
+        currentLevel = 0,
+        title = null,
+    ) {
         this.level = this.getDetailsFromStorage();
 
         if (!this.level['level' + currentLevel]) {
             this.level['level' + currentLevel] = [];
         }
 
-        const currentLevelArray = this.level['level'+currentLevel];
+        const currentLevelArray = this.level['level' + currentLevel];
 
-        if(currentLevelArray.length>0 && currentLevelArray[currentLevelArray.length-1]['title']==title){
+        if (
+            currentLevelArray.length > 0 &&
+            currentLevelArray[currentLevelArray.length - 1].title === title
+        ) {
             return;
         }
 
+        const url = this.routerUtilityService.getFullUrlFromSnapshopt(routerSnapshot);
+        const queryParams =
+            this.routerUtilityService.getQueryParametersFromSnapshot(routerSnapshot);
+
         const obj = {
-            "title": title,
-            'url': urlPath,
-            'queryParams': queryParams
+            title,
+            url,
+            queryParams,
         };
-        
+
         this.level['level' + currentLevel].push(obj);
         this.saveToStorage(this.level);
     }
 
-    goBackToLastOpenedPageAndUpdateLevel(currentRouterSnapshot: ActivatedRouteSnapshot, currentLevel: number = 0) {
-        let destinationUrlAndParams;
+    goBackToLastOpenedPageAndUpdateLevel(
+        currentRouterSnapshot: ActivatedRouteSnapshot,
+        currentLevel = 0,
+    ) {
+        let levelParams: LevelParams;
         this.level = this.getDetailsFromStorage();
-        while (!destinationUrlAndParams && currentLevel >= 0) {
-            if ( this.level['level' + currentLevel] && this.level['level' + currentLevel].length > 0 ) {
-                destinationUrlAndParams = this.level['level' + currentLevel].pop();
+        while (!levelParams && currentLevel >= 0) {
+            if (
+                this.level['level' + currentLevel] &&
+                this.level['level' + currentLevel].length > 0
+            ) {
+                levelParams = this.level['level' + currentLevel].pop();
                 break;
             }
             currentLevel--;
         }
         this.saveToStorage(this.level); // <-- update session storage after poping each obj
 
-        const currentPageQueryParams = this.routerUtilityService.getQueryParametersFromSnapshot(currentRouterSnapshot);
+        const currentPageQueryParams =
+            this.routerUtilityService.getQueryParametersFromSnapshot(currentRouterSnapshot);
 
-        const agAndDomain = {};
-        agAndDomain['ag'] = currentPageQueryParams['ag'];
-        agAndDomain['domain'] = currentPageQueryParams['domain'];
+        levelParams.queryParams = {
+            ...levelParams.queryParams,
+            ...{
+                ag: currentPageQueryParams['ag'],
+                domain: currentPageQueryParams['domain'],
+            },
+        };
 
-        Object.assign(destinationUrlAndParams.queryParams, agAndDomain);
-
-        this.router.navigate([destinationUrlAndParams['url']], {queryParams: destinationUrlAndParams['queryParams']});
+        this.router.navigate([levelParams.url], {
+            queryParams: levelParams.queryParams,
+        });
     }
 
-    checkIfFlowExistsCurrently(currentLevel?) {
+    checkIfFlowExistsCurrently(currentLevel?: number) {
         let flowExiststatus = false;
-        // getLevel();
         this.level = this.getDetailsFromStorage();
-        while ( currentLevel >= 0 ) {
+        while (currentLevel >= 0) {
             if (this.level['level' + currentLevel]) {
                 flowExiststatus = this.level['level' + currentLevel].length > 0;
             }
@@ -106,28 +129,29 @@ export class WorkflowService {
         this.saveToStorage(this.level);
     }
 
-    clearSpecificLevel(levelToBeCleared) {
+    clearSpecificLevel(levelToBeCleared: number) {
         this.level['level' + levelToBeCleared] = [];
     }
+
     /**
      * Added By Trinanjan
      * saveToStorage() saves the current url to seesion storage
      * getDetailsFromStorage() gets the previous page url
      * These 2 functions are added so that user can go back even if they reload the page
      */
-    saveToStorage(level) {
+    saveToStorage(level: Level) {
         try {
             const levelToBeStroed = JSON.stringify(level);
-        this.dataStore.set('StoredLevel' , levelToBeStroed);
+            this.dataStore.set('StoredLevel', levelToBeStroed);
         } catch (error) {
             this.logger.log('error', error);
         }
-
     }
+
     getDetailsFromStorage() {
         try {
             let levelToBeRetrived = this.dataStore.get('StoredLevel');
-            if ( levelToBeRetrived !== undefined) {
+            if (levelToBeRetrived !== undefined) {
                 levelToBeRetrived = JSON.parse(levelToBeRetrived);
             } else {
                 levelToBeRetrived = {};
@@ -140,28 +164,28 @@ export class WorkflowService {
 
     /** Below functions are used to get previously opened page in a particular module **/
 
-    addPageToModuleTracker(moduleName, pageUrl) {
+    addPageToModuleTracker(moduleName: string, pageUrl: string) {
         this.trackOpenedPageInAModule[moduleName] = pageUrl;
     }
 
-    addQueryParamsToModuleTracker(moduleName, queryParamString) {
-        if (queryParamString) {
-            this.trackOpenedPageInAModule[moduleName + 'queryparams'] = queryParamString;
+    addQueryParamsToModuleTracker(moduleName: string, queryParam: string) {
+        if (queryParam) {
+            this.trackOpenedPageInAModule[moduleName + 'queryparams'] = queryParam;
         }
     }
 
-    getPreviouslyOpenedPageInModule(moduleName) {
+    getPreviouslyOpenedPageInModule(moduleName: string) {
         return this.trackOpenedPageInAModule[moduleName];
     }
 
-    getPreviouslyOpenedPageQueryParamsInModule(moduleName) {
+    getPreviouslyOpenedPageQueryParamsInModule(moduleName: string) {
         return this.trackOpenedPageInAModule[moduleName + 'queryparams'];
     }
 
-    goToLevel(level){
-        let currentLevel = this.level["level0"].length;        
-        this.level["level0"].splice(level, currentLevel);
-        this.saveToStorage(this.level)
+    goToLevel(level: number) {
+        const currentLevel = this.level['level0'].length;
+        this.level['level0'].splice(level, currentLevel);
+        this.saveToStorage(this.level);
     }
 
     clearDataOfOpenedPageInModule() {
