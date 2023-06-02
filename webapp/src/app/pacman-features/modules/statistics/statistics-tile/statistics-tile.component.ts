b22f4c9918081a6df9047738a99d39f2e2802ec0
Maxim Cheremkhin <astralfestival@gmail.com>
2023-06-02 20:57:19 +0300

    feat(statistics-tile): enter animation

--- webapp/src/app/pacman-features/modules/statistics/statistics-tile/statistics-tile.component.ts
+++ webapp/src/app/pacman-features/modules/statistics/statistics-tile/statistics-tile.component.ts
@@ -1,9 +1,24 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-statistics-tile',
     templateUrl: './statistics-tile.component.html',
     styleUrls: ['./statistics-tile.component.css'],
+    animations: [
+        trigger('fadeInAnimation', [
+            transition(':enter', [
+                style({
+                    opacity: 0,
+                    width: 0,
+                    overflow: 'hidden',
+                    'white-space': 'nowrap',
+                    'text-overflow': 'clip',
+                }),
+                animate('350ms ease-out', style({ opacity: 1, width: '*' })),
+            ]),
+        ]),
+    ],
 })
 export class StatisticsTileComponent implements OnInit {
     @Input() title: string;
