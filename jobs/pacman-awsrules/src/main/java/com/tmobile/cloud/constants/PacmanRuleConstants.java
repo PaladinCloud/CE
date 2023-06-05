04dfd127ea139a68663757c2d44133bc7385893e
Arun Kotratil <arun.kotratil@paladincloud.io>
2023-06-05 00:03:39 +0000

    fix: [CUS-70] issue status update

--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/constants/PacmanRuleConstants.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/constants/PacmanRuleConstants.java
@@ -120,7 +120,7 @@ public class PacmanRuleConstants {
     public static final String SUBTYPE = "subtype";
     public static final String MEDIUM = "MEDIUM";
     public static final String MISSING_TAGS = "missingTags";
-    public static final String SUCCESS_MESSAGE = "Rule evaluation sucessfull";
+    public static final String SUCCESS_MESSAGE = "Policy evaluation sucessfull";
     public static final String S3_BUCKET_NAME = "s3BucketName";
     public static final String ANY_S3_AUTHENTICATED_USER_URI = "http://acs.amazonaws.com/groups/global/AuthenticatedUsers";
     public static final String ALL_S3_USER_URI = "http://acs.amazonaws.com/groups/global/AllUsers";
