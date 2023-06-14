1aa28383287b950ebd2bac06ee4921c1139592ac
Maxim Cheremkhin <astralfestival@gmail.com>
2023-06-14 13:56:51 +0300

    feat(app-table): table width pipe optimized cell width setting

---
+++ webapp/src/app/shared/table/table-column-width.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TableColumnWidthPipe } from './table-column-width.pipe';
+
+describe('TableColumnWidthPipe', () => {
+    it('create an instance', () => {
+        const pipe = new TableColumnWidthPipe();
+        expect(pipe).toBeTruthy();
+    });
+});
