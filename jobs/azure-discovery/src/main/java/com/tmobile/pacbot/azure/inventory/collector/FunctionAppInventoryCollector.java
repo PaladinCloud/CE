6e415c1e7df7cffa41300e545e533b49d4cfff38
anandhanatarajan.kuppusamy@zemosolabs.com <anandhanatarajan.kuppusamy@zemosolabs.com>
2023-07-31 13:07:57 +0530

    fix:[CORE-1137] Function App AssetIdDisplayName fix

--- jobs/azure-discovery/src/main/java/com/tmobile/pacbot/azure/inventory/collector/FunctionAppInventoryCollector.java
+++ jobs/azure-discovery/src/main/java/com/tmobile/pacbot/azure/inventory/collector/FunctionAppInventoryCollector.java
@@ -31,7 +31,7 @@ public class FunctionAppInventoryCollector {
             functionAppVH.setSubscription(subscription.getSubscriptionId());
             functionAppVH.setSubscriptionName(subscription.getSubscriptionName());
             functionAppVH.setResourceGroupName(functionApp.resourceGroupName());
-            functionAppVH.setName(functionAppVH.getName());
+            functionAppVH.setName(functionApp.name());
             functionAppVH.setClientCertEnabled(functionApp.clientCertEnabled());
             functionAppVH.setRegion(Util.getRegionValue(subscription,functionApp.regionName()));
             functionAppVH.setTags(functionApp.tags());
