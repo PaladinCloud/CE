4cba3785de656fb9b8b419f82de73a90653f3795
rnithinpaladin <nithin.r@paladincloud.io>
2023-08-02 13:49:52 +0530

    chore:[devops-88]individual JD for Qualys

--- installer/resources/batch/job.py
+++ installer/resources/batch/job.py
@@ -58,10 +58,56 @@ class RuleEngineJobQueue(BatchJobQueueResource):
     state = Settings.get('JOB_QUEUE_STATUS', "ENABLED")
     priority = 6
     compute_environments = [RuleEngineBatchJobEnv.get_output_attr('arn')]
-
-
+    
 class BatchJobsQueue(BatchJobQueueResource):
     name = "data"
+    state = Settings.get('JOB_QUEUE_STATUS', "ENABLED")
+    priority = 6
+    compute_environments = [RuleEngineBatchJobEnv.get_output_attr('arn')]
+
+class SubmitAndQualysJobDefinition(BatchJobDefinitionResource):
+    name = 'qualys-engine'
+    jd_type = 'container'
+    attempts = 2
+    container_properties = json.dumps({
+        'command': [
+            "~/fetch_and_run.sh",
+            "Ref::executableName",
+            "Ref::params",
+            "Ref::jvmMemParams",
+            "Ref::ruleEngineExecutableName",
+            "Ref::entryPoint"
+        ],
+        'image': RuleEngineEcrRepository.get_output_attr('repository_url'),
+        'memory': Settings.get('QUALYS_JOB_MEMORY', 8192),
+        'vcpus': Settings.get('QUALYS_JOB_VCPU', 2),
+        'environment': [
+            {'name': "ES_HOST", 'value': ESDomain.get_http_url_with_port()},
+            {'name': "BASE_AWS_ACCOUNT", 'value': AwsAccount.get_output_attr('account_id')},
+            {'name': "ES_URI", 'value': ESDomain.get_http_url_with_port()},
+            {'name': "HEIMDALL_URI", 'value': ESDomain.get_http_url_with_port()},
+            {'name': "PACMAN_API_URI", 'value': ApplicationLoadBalancer.get_api_base_url()},
+            {'name': "CONFIG_CREDENTIALS", 'value': "dXNlcjpwYWNtYW4="},
+            {'name': "CONFIG_SERVICE_URL", 'value': ApplicationLoadBalancer.get_http_url() + "/api/config/rule,batch/prd/latest"},
+            {'name': "AUTH_API_URL",'value': "https://"+ Settings.COGNITO_DOMAIN + ".auth." + Settings.AWS_REGION + ".amazoncognito.com"},
+            {'name': "POLICY_DETAILS_URL", 'value': ApplicationLoadBalancer.get_http_url() + "/api/compliance/v1/policy-details-by-uuid?policyUUID="}
+        ]
+    })
+
+    def post_terraform_destroy(self):
+        deregister_task_definition(
+            self.get_input_attr('name'),
+            Settings.AWS_AUTH_CRED
+        )
+
+    def pre_terraform_destroy(self):
+        compute_env = RuleEngineBatchJobEnv.get_input_attr('compute_environment_name')
+        job_definition = self.get_input_attr('name')
+        utils.remove_batch_job_related_resources(compute_env, job_definition)
+
+
+class QualysBatchJobsQueue(BatchJobQueueResource):
+    name = "qualys"
     state = Settings.get('JOB_QUEUE_STATUS', "ENABLED")
     priority = 6
     compute_environments = [RuleEngineBatchJobEnv.get_output_attr('arn')]
