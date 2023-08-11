96e072ab78e1c3e245c41e6e8479a85a6dda15a2
sidharthjain-zemoso <sidharth.jain@zemosolabs.com>
2023-08-11 14:16:17 +0530

    fix: handle data pipe

--- webapp/src/app/shared/table/pipes/cell-data-case-pipe.ts
+++ webapp/src/app/shared/table/pipes/cell-data-case-pipe.ts
@@ -5,12 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CellDataCasePipe implements PipeTransform {
     transform(value: string): string {
-        if (!value || value.startsWith('__') || value.includes("@")) {
+        if(value && typeof value==="string"){
+            if (value.startsWith('__') || value.includes("@")) {
+                return value;
+            }
+            return value
+                .replace(/(^\w|\b\w)/g, (s) => s.toUpperCase()) // title case words
+                .replace(/\s+/g, ' ') // collapse repeated whitespace
+                .trim(); // remove leading/trailing whitespace
+        }else{
             return value;
         }
-        return value
-            .replace(/(^\w|\b\w)/g, (s) => s.toUpperCase()) // title case words
-            .replace(/\s+/g, ' ') // collapse repeated whitespace
-            .trim(); // remove leading/trailing whitespace
     }
 }
