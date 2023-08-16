9a4d8c35664e379c0b12ba5ddc9d56bd294501cb
ranadheer-bolli <ranadheer.bolli@zemosolabs.com>
2023-08-16 10:15:31 +0530

    fixed sonar build failure

--- webapp/src/app/pacman-features/modules/statistics/statistics/statistics.component.ts
+++ webapp/src/app/pacman-features/modules/statistics/statistics/statistics.component.ts
@@ -51,7 +51,7 @@ export class StatisticsComponent implements OnInit {
     readonly innerRadius = 70;
     readonly outerRadius = 50;
     readonly currentDate = new Date();
-    companyIcon = CONFIGURATIONS.optional.general.logo.CustomerLogo || '/assets/images/Paladin_Logo.svg'; 
+    companyIcon = '/assets/images/Paladin_Logo.svg'; 
 
     @ViewChild('statisticsContainer') statisticsContainer: ElementRef<HTMLDivElement>;
 
