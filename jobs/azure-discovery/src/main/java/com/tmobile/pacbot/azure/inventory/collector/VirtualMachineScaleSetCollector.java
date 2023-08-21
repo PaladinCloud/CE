741ab8f1b970a6a46117ee1d0f121bb9b75e790d
kushZemoso <kushagra.jain@zemosolabs.com>
2023-08-21 16:18:24 +0530

    fix:VM ScaleSet collector.

--- jobs/azure-discovery/src/main/java/com/tmobile/pacbot/azure/inventory/collector/VirtualMachineScaleSetCollector.java
+++ jobs/azure-discovery/src/main/java/com/tmobile/pacbot/azure/inventory/collector/VirtualMachineScaleSetCollector.java
@@ -1,8 +1,5 @@
 package com.tmobile.pacbot.azure.inventory.collector;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.microsoft.azure.PagedList;
 import com.microsoft.azure.SubResource;
 import com.microsoft.azure.management.Azure;
@@ -10,15 +7,13 @@ import com.microsoft.azure.management.compute.*;
 import com.tmobile.pacbot.azure.inventory.auth.AzureCredentialProvider;
 import com.tmobile.pacbot.azure.inventory.vo.SubscriptionVH;
 import com.tmobile.pacbot.azure.inventory.vo.VirtualMachineScaleSetVH;
-import com.tmobile.pacman.commons.utils.CommonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.net.URLEncoder;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,7 +22,6 @@ public class VirtualMachineScaleSetCollector {
 
     @Autowired
     AzureCredentialProvider azureCredentialProvider;
-    private String apiUrlTemplate = "https://management.azure.com/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Compute/virtualMachines/%s?api-version=2023-03-01";
     private static Logger log = LoggerFactory.getLogger(VirtualMachineScaleSetCollector.class);
 
     public List<VirtualMachineScaleSetVH> fetchVMScaleSetDetails(SubscriptionVH subscription, Map<String, Map<String, String>> tagMap) {
@@ -40,17 +34,6 @@ public class VirtualMachineScaleSetCollector {
 
         for (VirtualMachineScaleSet virtualMachineScaleSet : vmss) {
             try {
-                String url = String.format(apiUrlTemplate,
-                        URLEncoder.encode(subscription.getSubscriptionId(),
-                                java.nio.charset.StandardCharsets.UTF_8.toString()),
-                        URLEncoder.encode(virtualMachineScaleSet.resourceGroupName(),
-                                java.nio.charset.StandardCharsets.UTF_8.toString()),
-                        URLEncoder.encode(virtualMachineScaleSet.name(),
-                                java.nio.charset.StandardCharsets.UTF_8.toString()));
-                JsonObject virtualMachineScaleSetObject = getVirtualMachineScaleSetObject(subscription, accessToken, virtualMachineScaleSet, url);
-                log.info("virtualMachineScaleSetObject: {}", virtualMachineScaleSetObject);
-
-
                 VirtualMachineScaleSetVH virtualMachineScaleSetVH = new VirtualMachineScaleSetVH();
 
                 virtualMachineScaleSetVH.setComputerName(virtualMachineScaleSet.computerNamePrefix() == null
@@ -81,37 +64,14 @@ public class VirtualMachineScaleSetCollector {
                         virtualMachineScaleSetVH.setLoadBalancerIds(lbIds);
                     }
                 }
-                if(virtualMachineScaleSetObject!=null) {
-                    JsonObject properties = virtualMachineScaleSetObject.getAsJsonObject("properties");
-                    if (properties != null) {
-                        HashMap<String, Object> propertiesMap = new Gson().fromJson(
-                                properties.toString(),
-                                HashMap.class);
-                        virtualMachineScaleSetVH.setProperties(propertiesMap);
-                    }
+
                     vmssList.add(virtualMachineScaleSetVH);
-                }
+
             }catch(Exception e) {
                 log.error("Error Collecting info for {} {} ",virtualMachineScaleSet.computerNamePrefix(),e.getMessage());
                 Util.eCount.getAndIncrement();
             }
         }
         return vmssList;
-    }
-
-    private static JsonObject getVirtualMachineScaleSetObject(SubscriptionVH subscription, String accessToken, VirtualMachineScaleSet virtualMachineScaleSet, String url) {
-        JsonObject getVirtualMachineScaleSetObject =null;
-        try {
-            String response = CommonUtils.doHttpGet(url, "Bearer", accessToken);
-            log.info("response form API: {} : {}",
-                    response,
-                    virtualMachineScaleSet.name().isEmpty() ? virtualMachineScaleSet.name() : "");
-            log.info("subscriptionName: {}", subscription.getSubscriptionName());
-            JsonObject responseObj = new JsonParser().parse(response).getAsJsonObject();
-            getVirtualMachineScaleSetObject = responseObj.getAsJsonObject();
-        }catch (Exception e){
-            log.error("Error in fetching VM info from API {}, Error:{} ", url,e.getMessage());
-        }
-        return getVirtualMachineScaleSetObject;
     }
 }
