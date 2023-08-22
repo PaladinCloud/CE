6f5568f7d69de1e70ce77f424eb7ef7fcd681519
kushZemoso <kushagra.jain@zemosolabs.com>
2023-08-22 11:43:03 +0530

    fix:[CUS-217] Gcp Disks Collector.

--- jobs/gcp-discovery/src/main/java/com/tmobile/pacbot/gcp/inventory/collector/DiskInventoryCollector.java
+++ jobs/gcp-discovery/src/main/java/com/tmobile/pacbot/gcp/inventory/collector/DiskInventoryCollector.java
@@ -3,6 +3,7 @@ package com.tmobile.pacbot.gcp.inventory.collector;
 
 import com.google.cloud.compute.v1.Disk;
 import com.google.cloud.compute.v1.DisksClient;
+import com.google.cloud.compute.v1.DisksScopedList;
 import com.google.cloud.compute.v1.ListDisksRequest;
 import com.tmobile.pacbot.gcp.inventory.auth.GCPCredentialsProvider;
 import com.tmobile.pacbot.gcp.inventory.vo.DiskVH;
@@ -10,12 +11,15 @@ import com.tmobile.pacbot.gcp.inventory.vo.ProjectVH;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.google.cloud.compute.v1.DisksClient.ListPagedResponse;
+import com.google.cloud.compute.v1.DisksClient.AggregatedListPagedResponse;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+@Component
 public class DiskInventoryCollector {
     @Autowired
     GCPCredentialsProvider gcpCredentialsProvider;
@@ -27,30 +31,31 @@ public class DiskInventoryCollector {
         logger.debug("Project id:{}",project.getProjectNumber());
 
         DisksClient disksClient=gcpCredentialsProvider.getDiskClient(project.getProjectId());
-        ListDisksRequest request = ListDisksRequest.newBuilder()
-                .setProject(project.getProjectId())
-                .build();
-        ListPagedResponse diskResponse = disksClient.list(request);
-        logger.info("Disk  entry {}", diskResponse);
-        for (Disk disk : diskResponse.iterateAll()) {
-            logger.info("Disk  iterator {}", disk);
+        AggregatedListPagedResponse aggregatedResponse = disksClient.aggregatedList(project.getProjectId());
+        logger.info("Disk  entry {}", aggregatedResponse);
+        for (Map.Entry<String, DisksScopedList> entry : aggregatedResponse.iterateAll()) {
+            DisksScopedList disksScopedList = entry.getValue();
+            if (disksScopedList.getDisksList() != null) {
+                for (Disk disk : disksScopedList.getDisksList()) {
+                    logger.info("Disk  iterator {}", disk);
+                    DiskVH diskVH=new DiskVH();
+                    diskVH.setName(disk.getName());
+                    diskVH.setKind(disk.getKind());
+                    diskVH.setSizeGb(disk.getSizeGb());
+                    diskVH.setZone(disk.getZone());
+                    diskVH.setStatus(disk.getStatus());
+                    diskVH.setType(disk.getType());
+                    diskVH.setId(String.valueOf(disk.getId()));
+                    diskVH.setProjectName(project.getProjectName());
+                    diskVH.setProjectId(project.getProjectId());
+                    diskVH.setLicenses(disk.getLicensesList());
+                    diskVH.setUsers(disk.getUsersList());
+                    diskVH.setLicenseCodes(disk.getLicenseCodesList());
+                    diskList.add(diskVH);
 
-            DiskVH diskVH=new DiskVH();
-            diskVH.setName(disk.getName());
-            diskVH.setKind(disk.getKind());
-            diskVH.setSizeGb(disk.getSizeGb());
-            diskVH.setZone(disk.getZone());
-            diskVH.setStatus(disk.getStatus());
-            diskVH.setType(disk.getType());
-            diskVH.setId(String.valueOf(disk.getId()));
-            diskVH.setProjectName(project.getProjectName());
-            diskVH.setProjectId(project.getProjectId());
-            diskVH.setLicenses(disk.getLicensesList());
-            diskVH.setUsers(disk.getUsersList());
-            diskVH.setLicenseCodes(disk.getLicenseCodesList());
-            diskList.add(diskVH);
-
-            logger.info("Disk  exit {}", diskVH);
+                    logger.info("Disk  exit {}", diskVH);
+                }
+            }
         }
 
         logger.info("Disk  list {}", diskList);
