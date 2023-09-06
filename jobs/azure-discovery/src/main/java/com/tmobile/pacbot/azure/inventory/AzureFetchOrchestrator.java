c9f81ddadd2fb68179b8996e309325c4f6e6ec3c
anandhanatarajan.kuppusamy@zemosolabs.com <anandhanatarajan.kuppusamy@zemosolabs.com>
2023-09-06 15:16:01 +0530

    fix:[PLG-186]

--- jobs/azure-discovery/src/main/java/com/tmobile/pacbot/azure/inventory/AzureFetchOrchestrator.java
+++ jobs/azure-discovery/src/main/java/com/tmobile/pacbot/azure/inventory/AzureFetchOrchestrator.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import com.tmobile.pacbot.azure.inventory.file.S3Uploader;
 import com.tmobile.pacman.commons.database.RDSDBManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
