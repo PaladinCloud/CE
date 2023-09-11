5f0ba74f64c5c705e0759a3abd070cfd6efae672
rnithinpaladin <nithin.r@paladincloud.io>
2023-09-11 14:36:14 +0530

    [DevOps-115]terminate_batch_job

--- installer/resources/lambda_submit/s3_upload.py
+++ installer/resources/lambda_submit/s3_upload.py
@@ -15,11 +15,11 @@ class UploadLambdaSubmitJobZipFile(S3BucketObject):
         get_terraform_scripts_and_files_dir(),
         BATCH_JOB_FILE_NAME + ".zip")
 
-BATCH_LONGRUNNING_JOB_FILE_NAME = "paladincloud-longrunningjob"
+BATCH_LONG_RUNNING_JOB_FILE_NAME = "paladincloud-longrunningjob"
 
 class UploadLambdaSubmitJobZipFile(S3BucketObject):
     bucket = BucketStorage.get_output_attr('bucket')
-    key = Settings.RESOURCE_NAME_PREFIX + PATH + BATCH_LONGRUNNING_JOB_FILE_NAME + ".zip"
+    key = Settings.RESOURCE_NAME_PREFIX + PATH + BATCH_LONG_RUNNING_JOB_FILE_NAME + ".zip"
     source = os.path.join(
         get_terraform_scripts_and_files_dir(),
-        BATCH_LONGRUNNING_JOB_FILE_NAME + ".zip")
+        BATCH_LONG_RUNNING_JOB_FILE_NAME + ".zip")
