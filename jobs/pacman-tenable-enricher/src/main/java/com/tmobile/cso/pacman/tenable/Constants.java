f545dacfa67afa8c0e7f56fbf8d81291b302a141
Pavel Lyubich <pavel@lyubich.com>
2023-10-03 13:51:33 -0700

    adding logging

--- jobs/pacman-tenable-enricher/src/main/java/com/tmobile/cso/pacman/tenable/Constants.java
+++ jobs/pacman-tenable-enricher/src/main/java/com/tmobile/cso/pacman/tenable/Constants.java
@@ -1,41 +1,27 @@
 package com.tmobile.cso.pacman.tenable;
 
-
 /**
  * The Interface Constants.
  */
-
 public class Constants {
 
+    // TODO: Set to true to get detailed debug logs
+    public static final boolean IS_DEBUG_MODE = true;
 
-    /** The failed. */
     public static final String FAILED = "failed";
-    
-    /** The error. */
     public static final String ERROR = "error";
-    
-    /** The exception. */
     public static final String EXCEPTION = "exception";
-    
-    /** The error type. */
     public static final String ERROR_TYPE = "type";
-    
-    /** The warn. */
     public static final String WARN = "warn";
-    
-    /** The fatal. */
     public static final String FATAL = "fatal";
-
-    /** The source. */
     public static final String SOURCE = "source";
-
-    /** The name. */
     public static final String NAME = "name";
 
-    /** The config creds. */
     public static final String CONFIG_CREDS = "config_creds";
-
     public static final String TENABLE_API_KEYS = "X-ApiKeys";
-
     public static final String USER_AGENT = "User-Agent";
+
+    private Constants() {
+        throw new IllegalStateException("Constants is a utility class");
+    }
 }
