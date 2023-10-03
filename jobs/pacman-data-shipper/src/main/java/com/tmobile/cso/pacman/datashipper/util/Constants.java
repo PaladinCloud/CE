0688192e9b6bcf90ec10b79fc0e466e7f4fea4e7
Pavel Lyubich <pavel@lyubich.com>
2023-10-02 20:05:29 -0700

    cleanup

--- jobs/pacman-data-shipper/src/main/java/com/tmobile/cso/pacman/datashipper/util/Constants.java
+++ jobs/pacman-data-shipper/src/main/java/com/tmobile/cso/pacman/datashipper/util/Constants.java
@@ -1,47 +1,27 @@
 package com.tmobile.cso.pacman.datashipper.util;
 
-
 /**
  * The Interface Constants.
  */
 public interface Constants {
-    /** The target type info. */
     String TARGET_TYPE_INFO = "targetTypes";
 
     String CONFIG_CREDS = "config_creds";
 
-
-    /** The target type info. */
     String TARGET_TYPE_OUTSCOPE = "typesNotInScope";
 
-    /** The API  User:Password  */
     String API_AUTH_INFO = "apiauthinfo";
 
     String CONFIG_QUERY = "configquery";
 
-    String CONFIG_URL = "CONFIG_URL";
-
-
-    /** The failed. */
     String FAILED = "failed";
-
-    /** The error. */
     String ERROR = "error";
-
-    /** The exception. */
     String EXCEPTION = "exception";
-
-    /** The error type. */
     String ERROR_TYPE = "type";
-
-    /** The warn. */
     String WARN = "warn";
-
-    /** The fatal. */
     String FATAL = "fatal";
 
     String SOURCE = "source";
-
     String NAME = "name";
 
     String DOC_TYPE = "docType";
