fbcf443841fdefb7623cab2ec52fc2ad73431548
Shefali Bisht <shefali.bisht@zemosolabs.com>
2023-10-10 15:41:18 +0530

    bug:[CORE-1442]update launchtemplate as finding in database

--- installer/resources/pacbot_app/files/DB.sql
+++ installer/resources/pacbot_app/files/DB.sql
@@ -3051,7 +3051,7 @@ INSERT IGNORE INTO pac_config_properties (`cfkey`,`value`,`application`,`profile
 delete from pac_config_properties where cfkey = 'shipper.attributes.to.preserve';
  INSERT IGNORE INTO pac_config_properties (`cfkey`,`value`,`application`,`profile`,`label`,`createdBy`,`createdDate`,`modifiedBy`,`modifiedDate`)
   VALUES ('shipper.attributes.to.preserve','firstdiscoveredon','batch','prd','latest',NULL,NULL,NULL,NULL);
- 
+
  
  INSERT IGNORE INTO `cf_Target` (`targetName`, `targetDesc`, `category`, `dataSourceName`, `targetConfig`, `status`, `userId`, `endpoint`, `createdDate`, `modifiedDate`, `domain`,displayName)
  VALUES('deployment','Deployment','Compute','redhat','{\"key\":\"id\",\"id\":\"id\",\"name\":\"name\"}','enabled','admin@paladincloud.io',
@@ -3077,3 +3077,6 @@ delete from pac_config_properties where cfkey = 'shipper.attributes.to.preserve'
   update pac_v2_ui_options set optionName ="Source" where optionId='57';
   update pac_v2_ui_options set optionName ="Source" where optionId='43';
 
+
+
+  update cf_Target set status="finding" where targetName="launchtemplate";
