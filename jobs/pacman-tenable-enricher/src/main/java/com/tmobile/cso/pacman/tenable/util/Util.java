c48a53afa86307ba3c97d69aa478b20c47afb23d
mkinchin-paladin <mikhail@paladincloud.io>
2023-10-11 12:30:46 -0700

    Style changes and refactoring

--- jobs/pacman-tenable-enricher/src/main/java/com/tmobile/cso/pacman/tenable/util/Util.java
+++ jobs/pacman-tenable-enricher/src/main/java/com/tmobile/cso/pacman/tenable/util/Util.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2022 Paladin Cloud, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package com.tmobile.cso.pacman.tenable.util;
 
 import java.io.IOException;
@@ -24,10 +39,6 @@ import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 
-
-/**
- * The Class Util.
- */
 public class Util {
     
     private static Logger log = LoggerFactory.getLogger(Util.class);
@@ -76,7 +87,6 @@ public class Util {
         Map map = gson.fromJson(jsonString, Map.class);
         return map.get(attributeName);
     }
-
 
     public static Gson getJsonBuilder(){
         GsonBuilder gsonBuilder = new GsonBuilder();
