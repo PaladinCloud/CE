a1427be4b23c095cc0e5d05f72c23ecdb8fa9ba5
mkinchin-paladin <mikhail@paladincloud.io>
2023-10-13 17:01:38 -0700

    Revert not needed changes

--- commons/pac-batch-commons/src/main/java/com/tmobile/pacman/commons/policy/Annotation.java
+++ commons/pac-batch-commons/src/main/java/com/tmobile/pacman/commons/policy/Annotation.java
@@ -1,44 +1,33 @@
-/**
+/*******************************************************************************
  * Copyright 2018 T Mobile, Inc. or its affiliates. All Rights Reserved.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
  * License for the specific language governing permissions and limitations under
  * the License.
- * <p>
- * Copyright (C) 2017 T Mobile Inc - All Rights Reserve
- * Purpose:
- * Author :kkumar
- * Modified Date: Jun 14, 2017
- * <p>
- * Copyright (C) 2017 T Mobile Inc - All Rights Reserve
- * Purpose:
- * Author :kkumar
- * Modified Date: Jun 14, 2017
- **/
+ ******************************************************************************/
 /**
- Copyright (C) 2017 T Mobile Inc - All Rights Reserve
- Purpose:
- Author :kkumar
- Modified Date: Jun 14, 2017
- **/
+  Copyright (C) 2017 T Mobile Inc - All Rights Reserve
+  Purpose:
+  Author :kkumar
+  Modified Date: Jun 14, 2017
+
+**/
 
 package com.tmobile.pacman.commons.policy;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 
-import com.tmobile.pacman.commons.dao.RDSDBManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,95 +38,94 @@ import com.tmobile.pacman.commons.PacmanSdkConstants;
 public class Annotation extends HashMap<String, String> {
 
     private static final Logger logger = LoggerFactory.getLogger(Annotation.class);
+	/**
+	 * Instantiates a new annotation.
+	 *
+	 * @param annotation the annotation
+	 */
+	public Annotation(Annotation annotation) {
+		Annotation annotationNew = new Annotation();
+		annotationNew.putAll(annotation);
+	}
 
-    /**
-     * Instantiates a new annotation.
-     *
-     * @param annotation the annotation
-     */
-    public Annotation(Annotation annotation) {
-        Annotation annotationNew = new Annotation();
-        annotationNew.putAll(annotation);
-    }
+	/**
+	 * Instantiates a new annotation.
+	 */
+	public Annotation() {
+	}
 
-    /**
-     * Instantiates a new annotation.
-     */
-    public Annotation() {
-    }
+	/**
+	 * Builds the annotation.
+	 *
+	 * @param ruleParam the rule param
+	 * @param type the type
+	 * @return the annotation
+	 */
+	public static Annotation buildAnnotation(Map<String, String> ruleParam,Annotation.Type type){
+		Annotation annotation = new Annotation();
+		annotation.put(PacmanSdkConstants.POLICY_NAME, ruleParam.get(PacmanSdkConstants.POLICY_DISPLAY_NAME));
+		annotation.put(PacmanSdkConstants.POLICY_ID, ruleParam.get(PacmanSdkConstants.POLICY_ID));
+		if (!Objects.isNull(ruleParam.get(PacmanSdkConstants.POLICY_VERSION))) {
+			annotation.put(PacmanSdkConstants.POLICY_VERSION, ruleParam.get(PacmanSdkConstants.POLICY_VERSION));
+		}
+		if (!Objects.isNull(ruleParam.get(PacmanSdkConstants.RESOURCE_ID))) {
+			annotation.put(PacmanSdkConstants.RESOURCE_ID, ruleParam.get(PacmanSdkConstants.RESOURCE_ID));
+		}
+		annotation.put(PacmanSdkConstants.TYPE, type.value());
+		return annotation;
+	}
 
-    /**
-     * Builds the annotation.
-     *
-     * @param ruleParam the rule param
-     * @param type the type
-     * @return the annotation
-     */
-    public static Annotation buildAnnotation(Map<String, String> ruleParam, Annotation.Type type) {
-        Annotation annotation = new Annotation();
-        annotation.put(PacmanSdkConstants.POLICY_NAME, ruleParam.get(PacmanSdkConstants.POLICY_DISPLAY_NAME));
-        annotation.put(PacmanSdkConstants.POLICY_ID, ruleParam.get(PacmanSdkConstants.POLICY_ID));
-        if (!Objects.isNull(ruleParam.get(PacmanSdkConstants.POLICY_VERSION))) {
-            annotation.put(PacmanSdkConstants.POLICY_VERSION, ruleParam.get(PacmanSdkConstants.POLICY_VERSION));
-        }
-        if (!Objects.isNull(ruleParam.get(PacmanSdkConstants.RESOURCE_ID))) {
-            annotation.put(PacmanSdkConstants.RESOURCE_ID, ruleParam.get(PacmanSdkConstants.RESOURCE_ID));
-        }
-        annotation.put(PacmanSdkConstants.TYPE, type.value());
-        return annotation;
-    }
+	/* (non-Javadoc)
+	 * @see java.util.HashMap#put(java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public String put(String key, String value) {
+		if(!Strings.isNullOrEmpty(value)){
+			key = CharMatcher.WHITESPACE.removeFrom(key);
+			return super.put(key, value);
+		}else{
+		    logger.error(String.format("Null value not allowed %s = %s "  ,key, value));
+			return null;
+		}
+	}
 
-    /* (non-Javadoc)
-     * @see java.util.HashMap#put(java.lang.Object, java.lang.Object)
-     */
-    @Override
-    public String put(String key, String value) {
-        if (!Strings.isNullOrEmpty(value)) {
-            key = CharMatcher.WHITESPACE.removeFrom(key);
-            return super.put(key, value);
-        } else {
-            logger.error(String.format("Null value not allowed %s = %s ", key, value));
-            return null;
-        }
-    }
+	/**
+	 * The Enum Type.
+	 */
+	public enum Type {
 
-    /**
-     * The Enum for Annotation Type.
-     */
-    public enum Type {
+		/** The issue. */
+		ISSUE("issue"),
 
-        /** The issue. */
-        ISSUE("issue"),
+		/** The info. */
+		INFO("info"),
 
-        /** The info. */
-        INFO("info"),
+		/** The recommendation. */
+		RECOMMENDATION("recommendation"),
 
-        /** The recommendation. */
-        RECOMMENDATION("recommendation"),
+		/** The error. */
+		ERROR("error");
 
-        /** The error. */
-        ERROR("error");
+		/** The value. */
+		private String value;
 
-        /** The value. */
-        private String value;
+		/**
+		 * Instantiates a new type.
+		 *
+		 * @param value the value
+		 */
+		private Type(String value) {
+			this.value=value;
+		}
 
-        /**
-         * Instantiates a new type.
-         *
-         * @param value the value
-         */
-        private Type(String value) {
-            this.value = value;
-        }
-
-        /**
-         * Value.
-         *
-         * @return the string
-         */
-        public String value() {
-            return value;
-        }
-    }
+		 /**
+ 		 * Value.
+ 		 *
+ 		 * @return the string
+ 		 */
+ 		public String value() {
+		        return value;
+		    }
+	}
 
 }
