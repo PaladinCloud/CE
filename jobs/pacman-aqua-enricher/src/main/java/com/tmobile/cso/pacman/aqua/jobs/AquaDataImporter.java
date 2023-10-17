9ec98642cc901d3361838323560dc5de09846ad9
Mikhail Kinchin <133698330+mkinchin-paladin@users.noreply.github.com>
2023-10-17 11:45:29 -0700

    [PLG-321] Aqua legacy collector cleanup (#1707)
    Aqua legacy collector cleanup
    
    Size reduced by 20 times form 290 to 14 mb.

--- jobs/pacman-aqua-enricher/src/main/java/com/tmobile/cso/pacman/aqua/jobs/AquaDataImporter.java
+++ jobs/pacman-aqua-enricher/src/main/java/com/tmobile/cso/pacman/aqua/jobs/AquaDataImporter.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2022 Paladin Cloud, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package com.tmobile.cso.pacman.aqua.jobs;
 
 import com.amazonaws.auth.BasicSessionCredentials;
@@ -7,74 +22,71 @@ import com.tmobile.cso.pacman.aqua.exception.AquaDataImportException;
 import com.tmobile.cso.pacman.aqua.util.HttpUtil;
 import com.tmobile.cso.pacman.aqua.util.Util;
 import com.tmobile.pacman.commons.secrets.AwsSecretManagerUtil;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class AquaDataImporter {
 
-  protected static String BASE_API_URL = null;
+    protected String apiUsername;
+    protected String apiPassword;
+    protected String aquaApiUrl;
+    protected String aquaClientDomainUrl;
 
-  AwsSecretManagerUtil secretManagerUtil=new AwsSecretManagerUtil();
+    Map<String, String> apiMap;
+    AwsSecretManagerUtil secretManagerUtil = new AwsSecretManagerUtil();
+    CredentialProvider credentialProvider = new CredentialProvider();
 
-  CredentialProvider credentialProvider=new CredentialProvider();
+    public abstract Map<String, Object> execute();
 
-  protected String secretManagerPrefix=System.getProperty("secret.manager.path");
-
-  /** The Constant DEFAULT_USER. */
-  private static String DEFAULT_USER;
-
-  /** The Constant DEFAULT_PASS. */
-  private static String DEFAULT_PASS;
-
-  protected static String AQUA_CLIENT_DOMAIN_URL;
-
-
-  abstract public Map<String, Object> execute();
-
-  protected  String baseAccount =System.getProperty("base.account");
-  protected  String baseRegion =System.getProperty("base.region");
-  protected  String roleName =System.getProperty("s3.role");
-
-  /** The api map. */
-  Map<String, String> apiMap = null;
-
-  protected AquaDataImporter() {
-    apiMap = new HashMap<String, String>();
-    apiMap.put("signIn",
-        "/v2/signin");
-    apiMap.put("image_vulnerabilities",
-        "/api/v2/risks/vulnerabilities");
-    apiMap.put("vm_vulnerabilities", "/api/v2/risks/functions/vulnerabilities");
-    apiMap.put("hostassetcount", "/qps/rest/2.0/count/am/hostasset");
-    getAquaInfo();
-  }
-
-  private void getAquaInfo() {
-    BasicSessionCredentials credential = credentialProvider.getBaseAccountCredentials(baseAccount, baseRegion, roleName);
-    secretManagerPrefix="paladincloud/secret";
-    String secretData=secretManagerUtil.fetchSecret(secretManagerPrefix+"/aqua",credential,baseRegion);
-    Map<String, String> dataMap = Util.getJsonData(secretData);
-    DEFAULT_USER=dataMap.get("apiusername");
-    DEFAULT_PASS=dataMap.get("apipassword");
-    BASE_API_URL=dataMap.get("aquaApiUrl");
-    AQUA_CLIENT_DOMAIN_URL= dataMap.get("aquaClientDomainUrl");
-  }
-
-  public String getBearerToken() throws AquaDataImportException {
-    String token = null;
-    String tokenUri = BASE_API_URL + apiMap.get("signIn");
-    JsonObject inputObject = new JsonObject();
-    inputObject.addProperty("email", DEFAULT_USER);
-    inputObject.addProperty("password", DEFAULT_PASS);
-
-    String input = inputObject.toString();
-    try {
-      String response = HttpUtil.post(tokenUri,input,null , null);
-      Map<String,Object> data = (Map) Util.getJsonAttribute(response, "data");
-      token = (String) data.get("token");
-    } catch (Exception e) {
-      throw new AquaDataImportException(e.getMessage());
+    protected AquaDataImporter() {
+        apiMap = new HashMap<>();
+        apiMap.put("signIn",
+                "/v2/signin");
+        apiMap.put("image_vulnerabilities",
+                "/api/v2/risks/vulnerabilities");
+        apiMap.put("vm_vulnerabilities", "/api/v2/risks/functions/vulnerabilities");
+        apiMap.put("hostassetcount", "/qps/rest/2.0/count/am/hostasset");
+        getAquaInfo();
     }
-    return token;
-  }
+
+    private void getAquaInfo() {
+        String secretManagerPrefix = System.getProperty("secret.manager.path");
+        String baseAccount = System.getProperty("base.account");
+        String baseRegion = System.getProperty("base.region");
+        String roleName = System.getProperty("s3.role");
+
+        BasicSessionCredentials credential = credentialProvider.getBaseAccountCredentials(baseAccount, baseRegion, roleName);
+        String secretName = secretManagerPrefix + "/" + roleName + "/aqua";
+        String secretData = secretManagerUtil.fetchSecret(secretName, credential, baseRegion);
+        Map<String, String> dataMap = Util.getJsonData(secretData);
+
+        apiUsername = dataMap.get("apiusername");
+        apiPassword = dataMap.get("apipassword");
+        aquaApiUrl = dataMap.get("aquaApiUrl");
+        aquaClientDomainUrl = dataMap.get("aquaClientDomainUrl");
+    }
+
+    public String getBearerToken() throws AquaDataImportException {
+        String token = null;
+        String tokenUri = aquaApiUrl + apiMap.get("signIn");
+        JsonObject inputObject = new JsonObject();
+        inputObject.addProperty("email", apiUsername);
+        inputObject.addProperty("password", apiPassword);
+
+        String input = inputObject.toString();
+        try {
+            String response = HttpUtil.post(tokenUri, input, null, null);
+            Map<String, Object> data = (Map) Util.getJsonAttribute(response, "data");
+            token = (String) data.get("token");
+        } catch (Exception e) {
+            throw new AquaDataImportException(e.getMessage());
+        }
+        return token;
+    }
+
+    protected int getDefaultPageSize() {
+        return Integer.parseInt(System.getProperty("default_page_size"));
+    }
+
 }
