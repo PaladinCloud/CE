0718a011956bdfffa9aa9afba05ec922bbb97fee
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2023-11-02 15:25:55 +0530

    fix:[CORE-1412] Reformat file

--- jobs/azure-discovery/src/main/java/com/tmobile/pacbot/azure/inventory/ErrorManageUtil.java
+++ jobs/azure-discovery/src/main/java/com/tmobile/pacbot/azure/inventory/ErrorManageUtil.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2018 T Mobile, Inc. or its affiliates. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -14,7 +14,6 @@
  * the License.
  ******************************************************************************/
 package com.tmobile.pacbot.azure.inventory;
-
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -25,148 +24,143 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
-
 import com.tmobile.pacman.commons.dto.ErrorVH;
 import com.tmobile.pacman.commons.dto.PermissionVH;
 import com.tmobile.pacman.commons.utils.NotificationPermissionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.tmobile.pacbot.azure.inventory.file.FileGenerator;
-
 
 /**
  * The Class ErrorManageUtil.
  */
 public class ErrorManageUtil {
 
-    /** The log. */
+    /**
+     * The log.
+     */
     private static Logger log = LoggerFactory.getLogger(ErrorManageUtil.class);
-	
-	/** The error map. */
-	private static Map<String,List<ErrorVH>> errorMap = new ConcurrentHashMap<>();
-	
-	/**
-	 * Instantiates a new error manage util.
-	 */
-	private ErrorManageUtil() {
-		
-	}
-	
-	/**
-	 * Initialise.
-	 */
-	public static void initialise(){
-		try {
-			FileGenerator.writeToFile("azure-loaderror.data",InventoryConstants.OPEN_ARRAY, false);
-		} catch (IOException e) {
-			log.error("Error in Initialise",e);
-		}
-	}
-	
-	/**
-	 * Finalise.
-	 */
-	public static void finalise(){
-		try {
-			FileGenerator.writeToFile("azure-loaderror.data",InventoryConstants.CLOSE_ARRAY, true);
-		} catch (IOException e) {
-			log.error("Error in finalise",e);
-		}
-	}
-	
-	/**
-	 * Upload error.
-	 *
-	 * @param account the account
-	 * @param region the region
-	 * @param type the type
-	 * @param exception the exception
-	 */
-	public static synchronized void  uploadError(String account, String region, String type, String exception) {
-		try{
-			List<ErrorVH> errorList = errorMap.get(account);
-			if(errorList==null){
-				errorList =  new CopyOnWriteArrayList<>();
-				errorMap.put(account, errorList);
-			}
-			ErrorVH error = new ErrorVH();
-			error.setException(exception);
-			error.setRegion(region);
-			error.setType(type);
-			errorList.add(error);
-		}catch(Exception e){
-		    log.error("Error in uploadError",e);
-		}
-	}
-	
-	
-	
-	public static Map<String,Object> formErrorCode() {
-        Map<String,Object> errorCode = new HashMap<>();
+
+    /**
+     * The error map.
+     */
+    private static Map<String, List<ErrorVH>> errorMap = new ConcurrentHashMap<>();
+
+    /**
+     * Instantiates a new error manage util.
+     */
+    private ErrorManageUtil() {
+
+    }
+
+    /**
+     * Initialise.
+     */
+    public static void initialise() {
+        try {
+            FileGenerator.writeToFile("azure-loaderror.data", InventoryConstants.OPEN_ARRAY, false);
+        } catch (IOException e) {
+            log.error("Error in Initialise", e);
+        }
+    }
+
+    /**
+     * Finalise.
+     */
+    public static void finalise() {
+        try {
+            FileGenerator.writeToFile("azure-loaderror.data", InventoryConstants.CLOSE_ARRAY, true);
+        } catch (IOException e) {
+            log.error("Error in finalise", e);
+        }
+    }
+
+    /**
+     * Upload error.
+     *
+     * @param account   the account
+     * @param region    the region
+     * @param type      the type
+     * @param exception the exception
+     */
+    public static synchronized void uploadError(String account, String region, String type, String exception) {
+        try {
+            List<ErrorVH> errorList = errorMap.get(account);
+            if (errorList == null) {
+                errorList = new CopyOnWriteArrayList<>();
+                errorMap.put(account, errorList);
+            }
+            ErrorVH error = new ErrorVH();
+            error.setException(exception);
+            error.setRegion(region);
+            error.setType(type);
+            errorList.add(error);
+        } catch (Exception e) {
+            log.error("Error in uploadError", e);
+        }
+    }
+
+
+    public static Map<String, Object> formErrorCode() {
+        Map<String, Object> errorCode = new HashMap<>();
         errorCode.put("jobName", System.getProperty("jobName"));
-        
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         errorCode.put("executionEndDate", sdf.format(new Date()));
-        
-        List<Map<String,Object>> errors = new ArrayList<>();
-        for(Entry<String, List<ErrorVH>> errorDetail :errorMap.entrySet()) {
-            Map<String,Object> error = new HashMap<>();
-            List<Map<String,String>> details = new ArrayList<>();
-            
-            error.put("error", "Error while fetching Inventory for account "+errorDetail.getKey());
-            for(ErrorVH errorVH : errorDetail.getValue()) {
-                Map<String,String> detail = new HashMap<>();
-                detail.put("type",errorVH.getType());
-                detail.put("region",errorVH.getRegion());
-                detail.put("exception",errorVH.getException());
-                detail.put("account",errorDetail.getKey());
+        List<Map<String, Object>> errors = new ArrayList<>();
+        for (Entry<String, List<ErrorVH>> errorDetail : errorMap.entrySet()) {
+            Map<String, Object> error = new HashMap<>();
+            List<Map<String, String>> details = new ArrayList<>();
+            error.put("error", "Error while fetching Inventory for account " + errorDetail.getKey());
+            for (ErrorVH errorVH : errorDetail.getValue()) {
+                Map<String, String> detail = new HashMap<>();
+                detail.put("type", errorVH.getType());
+                detail.put("region", errorVH.getRegion());
+                detail.put("exception", errorVH.getException());
+                detail.put("account", errorDetail.getKey());
                 details.add(detail);
             }
-            error.put("details",details);
+            error.put("details", details);
             errors.add(error);
         }
-        
         errorCode.put("errors", errors);
-        if(errors.isEmpty()) {
-            errorCode.put("status","Success");
+        if (errors.isEmpty()) {
+            errorCode.put("status", "Success");
         } else {
-            errorCode.put("status","Partial Success");
+            errorCode.put("status", "Partial Success");
         }
-        log.info("Return Info {}",errorCode);
+        log.info("Return Info {}", errorCode);
         return errorCode;
     }
-	public static void triggerNotificationforPermissionDenied()
-	{
-		List<PermissionVH> permissionIssue=new ArrayList<>();
-		for(Map.Entry<String,List<ErrorVH>> entry:errorMap.entrySet())
-		{
-			Map<String, List<String>> assetPermissionMapping = new HashMap<>();
-			List<ErrorVH> errorVHList=entry.getValue();
-			for(ErrorVH errorVH:entry.getValue())
-			{
-				List<String> permissionIssues = assetPermissionMapping.get(errorVH.getType());
-				if((errorVH.getType().equals("vault")&&errorVH.getException().contains("DeniedWithNoValidRBAC")||errorVH.getException().contains("ForbiddenByFirewall"))||(errorVH.getType().equals("webapp")&&errorVH.getException().contains("AuthorizationFailed"))||(errorVH.getType().equals("all")&&errorVH.getException().contains("AuthenticationException")))
-				{
-					log.info("Omit exception :{}",errorVH.getException());
-					if (permissionIssues != null) {
-						permissionIssues.add(errorVH.getException());
-					} else {
-						permissionIssues = new ArrayList<>();
-						permissionIssues.add(errorVH.getException());
-					}
-					assetPermissionMapping.put(errorVH.getType(), permissionIssues);
-					errorVHList.remove(errorVH);
-				}
-			}
-			if (!assetPermissionMapping.isEmpty()) {
-				PermissionVH permissionVH = new PermissionVH();
-				permissionVH.setAccountNumber(entry.getKey());
-				permissionVH.setAssetPermissionIssues(assetPermissionMapping);
-				permissionIssue.add(permissionVH);
-			}
-			if(errorVHList.isEmpty()){errorMap.remove(entry.getKey());}
-		}
-		NotificationPermissionUtils.triggerNotificationForPermissionDenied(permissionIssue,"Azure");
-	}
+
+    public static void triggerNotificationforPermissionDenied() {
+        List<PermissionVH> permissionIssue = new ArrayList<>();
+        for (Map.Entry<String, List<ErrorVH>> entry : errorMap.entrySet()) {
+            Map<String, List<String>> assetPermissionMapping = new HashMap<>();
+            List<ErrorVH> errorVHList = entry.getValue();
+            for (ErrorVH errorVH : entry.getValue()) {
+                List<String> permissionIssues = assetPermissionMapping.get(errorVH.getType());
+                if ((errorVH.getType().equals("vault") && errorVH.getException().contains("DeniedWithNoValidRBAC") || errorVH.getException().contains("ForbiddenByFirewall")) || (errorVH.getType().equals("webapp") && errorVH.getException().contains("AuthorizationFailed")) || (errorVH.getType().equals("all") && errorVH.getException().contains("AuthenticationException"))) {
+                    log.info("Omit exception :{}", errorVH.getException());
+                    if (permissionIssues != null) {
+                        permissionIssues.add(errorVH.getException());
+                    } else {
+                        permissionIssues = new ArrayList<>();
+                        permissionIssues.add(errorVH.getException());
+                    }
+                    assetPermissionMapping.put(errorVH.getType(), permissionIssues);
+                    errorVHList.remove(errorVH);
+                }
+            }
+            if (!assetPermissionMapping.isEmpty()) {
+                PermissionVH permissionVH = new PermissionVH();
+                permissionVH.setAccountNumber(entry.getKey());
+                permissionVH.setAssetPermissionIssues(assetPermissionMapping);
+                permissionIssue.add(permissionVH);
+            }
+            if (errorVHList.isEmpty()) {
+                errorMap.remove(entry.getKey());
+            }
+        }
+        NotificationPermissionUtils.triggerNotificationForPermissionDenied(permissionIssue, "Azure");
+    }
 }
