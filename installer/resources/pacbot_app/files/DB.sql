8e1ab9e3a06505bc9e6001ed5923388d3a1c1a93
Shefali Bisht <shefali.bisht@zemosolabs.com>
2023-11-07 13:54:50 +0530

    bug:[CORE-1334] quick fix for plugin filter break.

--- installer/resources/pacbot_app/files/DB.sql
+++ installer/resources/pacbot_app/files/DB.sql
@@ -2870,7 +2870,7 @@ INSERT IGNORE INTO pac_v2_ui_filters (filterId,filterName) VALUES (12,'plugins')
 
 
 INSERT IGNORE INTO pac_v2_ui_options (filterId,optionName,optionValue,optionURL) VALUES (12,'Account ID','accountId','/admin/accounts/filter/attribute?attribute=accountId');
-INSERT IGNORE INTO pac_v2_ui_options (filterId,optionName,optionValue,optionURL) VALUES (12,'Account Name','accountName','/admin/accounts/filter/attribute?attribute=accountName');1
+INSERT IGNORE INTO pac_v2_ui_options (filterId,optionName,optionValue,optionURL) VALUES (12,'Account Name','accountName','/admin/accounts/filter/attribute?attribute=accountName');
 --INSERT IGNORE INTO pac_v2_ui_options (filterId,optionName,optionValue,optionURL) VALUES (12,'Assets','assets','/admin/accounts/filter/attribute?attribute=assets');
 --INSERT IGNORE INTO pac_v2_ui_options (filterId,optionName,optionValue,optionURL) VALUES (12,'Violations','violations','/admin/accounts/filter/attribute?attribute=violations');
 INSERT IGNORE INTO pac_v2_ui_options (filterId,optionName,optionValue,optionURL) VALUES (12,'Status','accountStatus','/admin/accounts/filter/attribute?attribute=accountStatus');
