fbd505a66139567fdd106acb766459d280616ec5
ARUN KUMAR KOTTARATTIL <arun.kumarkottarattil3@t-mobile.com>
2023-11-07 20:05:12 +0530

    resolved .db file conflicts.

--- installer/resources/pacbot_app/files/DB.sql
+++ installer/resources/pacbot_app/files/DB.sql
@@ -2345,7 +2345,7 @@ INSERT IGNORE INTO pac_config_properties(`cfkey`,`value`,`application`,`profile`
  VALUES ('policy-engine.invoke.url','submitRuleExecutionJob','compliance-service','prd','latest',NULL,NULL,NULL,NULL);
 INSERT IGNORE INTO `pac_config_properties` (`cfkey`, `value`, `application`, `profile`, `label`, `createdBy`, `createdDate`, `modifiedBy`, `modifiedDate`) values('pacman.host',concat(@PACMAN_HOST_NAME,''),'application','prd','latest',NULL,NULL,NULL,NULL);
 
-INSERT IGNORE INTO pac_config_properties (`cfkey`,`value`,`application`,`profile`,`label`,`createdBy`,`createdDate`,`modifiedBy`,`modifiedDate`) VALUES ('application.optionalAssetGroupList','azure,gcp,redhat,contrast','application','prd','latest',NULL,NULL,NULL,NULL);
+INSERT IGNORE INTO pac_config_properties (`cfkey`,`value`,`application`,`profile`,`label`,`createdBy`,`createdDate`,`modifiedBy`,`modifiedDate`) VALUES ('application.optionalAssetGroupList','azure,gcp,redhat','application','prd','latest',NULL,NULL,NULL,NULL);
 INSERT IGNORE INTO pac_config_properties (`cfkey`,`value`,`application`,`profile`,`label`,`createdBy`,`createdDate`,`modifiedBy`,`modifiedDate`) VALUES ('application.defaultAssetGroup','aws','application','prd','latest',NULL,NULL,NULL,NULL);
 
 UPDATE IGNORE pac_config_properties SET value='us-gov-east-1,us-gov-west-1,cn-north-1,cn-northwest-1,us-iso-east-1,us-iso-west-1,us-isob-east-1' where cfkey='region.ignore';
