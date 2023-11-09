08c0330a343c678ed73f274e1aa77182378cd2be
ranadheer-bolli <ranadheer.bolli@zemosolabs.com>
2023-11-09 23:41:53 +0530

    feat:[UIUX-452] developed new error page

--- webapp/src/app/error/error.component.ts
+++ webapp/src/app/error/error.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-error',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+  githubLink = "https://github.com/PaladinCloud/CE";
+  discordLink = "https://discord.gg/xvCFD29Jj4";
+  slackLink = "https://paladincloudworkspace.slack.com";
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  goToDashboard(){
+    const queryParams = this.route.snapshot.queryParams;
+    this.router.navigate(['/home'], {
+      queryParams: {
+          ag: queryParams.ag,
+          domain: queryParams.domain,
+      }
+    });
   }
 
 }
