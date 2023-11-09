fe6c66a3915d30b44c430087160a6a5dcf310255
anandhanatarajan.kuppusamy@zemosolabs.com <anandhanatarajan.kuppusamy@zemosolabs.com>
2023-11-09 11:24:55 +0530

    fix:[PLG-358]Review comments

--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/gcprules/APIKeys/ConfigureApiRestriction.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/gcprules/APIKeys/ConfigureApiRestriction.java
@@ -87,11 +87,11 @@ public class ConfigureApiRestriction extends BasePolicy {
             logger.info("hit array size {}",hitsJsonArray.size());
             JsonObject apiKeys = (JsonObject) ((JsonObject) hitsJsonArray.get(0))
                     .get(PacmanRuleConstants.SOURCE);
-            if (apiKeys.get(PacmanRuleConstants.RESTRICTIONS).isJsonNull()){
+            if (apiKeys.get(PacmanRuleConstants.RESTRICTIONS).isJsonNull()) {
                 validationResult = false;
             }
+            if (apiKeys.get(PacmanRuleConstants.RESTRICTIONS).getAsJsonObject()!=null &&apiKeys.get(PacmanRuleConstants.RESTRICTIONS).getAsJsonObject().size()>0) {
 
-            if (apiKeys.get(PacmanRuleConstants.RESTRICTIONS).getAsJsonObject() != null && apiKeys.get(PacmanRuleConstants.RESTRICTIONS).getAsJsonObject().size() > 0) {
                 JsonArray apiTargets = apiKeys.get("apiTargetList").getAsJsonArray();
                 for (JsonElement apiTarget : apiTargets) {
                     if (apiTarget.getAsString().equalsIgnoreCase(API_KEY)) {
