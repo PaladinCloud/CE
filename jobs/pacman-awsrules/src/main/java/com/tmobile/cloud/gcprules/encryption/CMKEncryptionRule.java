fb27fe8517789a60726a7a5de7d135a359327f41
anandhanatarajan.kuppusamy@zemosolabs.com <anandhanatarajan.kuppusamy@zemosolabs.com>
2023-11-10 19:31:15 +0530

    fix:[PLG-371]SelfReviewComments

--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/gcprules/encryption/CMKEncryptionRule.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/gcprules/encryption/CMKEncryptionRule.java
@@ -85,8 +85,8 @@ public class CMKEncryptionRule extends BasePolicy {
                     .get(PacmanRuleConstants.SOURCE);
             logger.debug("Data retrieved from ES: {}", sourceData);
             boolean isKmsKeyNamePresent = sourceData.getAsJsonObject().keySet().contains(PacmanRuleConstants.KMS_KEY_NAME);
-            if(!isKmsKeyNamePresent)
-             return true;
+            if (!isKmsKeyNamePresent)
+                return true;
 
             return sourceData.getAsJsonObject().get(PacmanRuleConstants.KMS_KEY_NAME).isJsonNull();
         } else {
