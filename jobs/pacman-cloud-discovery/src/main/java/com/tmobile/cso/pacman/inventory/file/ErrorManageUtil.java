e7eda2a2cfa9045de79c00f3c8d2bf69d49c9083
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2023-11-14 14:20:54 +0530

    fix:[CORE-1635] Removed comments

--- jobs/pacman-cloud-discovery/src/main/java/com/tmobile/cso/pacman/inventory/file/ErrorManageUtil.java
+++ jobs/pacman-cloud-discovery/src/main/java/com/tmobile/cso/pacman/inventory/file/ErrorManageUtil.java
@@ -1,20 +1,4 @@
-/*******************************************************************************
- * Copyright 2018 T Mobile, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
- * of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations under
- * the License.
- ******************************************************************************/
 package com.tmobile.cso.pacman.inventory.file;
-
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -32,25 +16,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.tmobile.cso.pacman.inventory.InventoryConstants;
 
-
-/**
- * The Class ErrorManageUtil.
- */
 public class ErrorManageUtil {
 
-    /**
-     * The log.
-     */
     private static Logger log = LoggerFactory.getLogger(ErrorManageUtil.class);
 
-    /**
-     * The error map.
-     */
     private static Map<String, List<ErrorVH>> errorMap = new ConcurrentHashMap<>();
 
-    /**
-     * Instantiates a new error manage util.
-     */
     private ErrorManageUtil() {
 
     }
@@ -59,9 +30,6 @@ public class ErrorManageUtil {
         return errorMap;
     }
 
-    /**
-     * Initialise.
-     */
     public static void initialise() {
         try {
             FileGenerator.writeToFile("aws-loaderror.data", InventoryConstants.OPEN_ARRAY, false);
@@ -70,9 +38,6 @@ public class ErrorManageUtil {
         }
     }
 
-    /**
-     * Finalise.
-     */
     public static void finalise() {
         try {
             FileGenerator.writeToFile("aws-loaderror.data", InventoryConstants.CLOSE_ARRAY, true);
@@ -106,9 +71,6 @@ public class ErrorManageUtil {
         }
     }
 
-    /**
-     * Write error file.
-     */
     public static void writeErrorFile() {
         try {
             FileManager.generateErrorFile(errorMap);
