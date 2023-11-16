ffffe2b256bb7df5525c3da43eff17c8e327f85c
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2023-11-16 12:01:16 +0530

    fix:[CORE-1662] Bucket does not exist exception handled

--- jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/awsrules/utils/S3PacbotUtils.java
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/awsrules/utils/S3PacbotUtils.java
@@ -1,21 +1,3 @@
-/*******************************************************************************
- * Copyright 2018 T Mobile, Inc. or its affiliates. All Rights Reserved.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
- * of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations under
- * the License.
- ******************************************************************************/
-/**
- * Utility functions for ASGC Rules
- */
 package com.tmobile.cloud.awsrules.utils;
 
 import java.lang.reflect.Type;
@@ -66,7 +48,7 @@ public class S3PacbotUtils {
 
 	/**
 	 * This method is to check whether s3 bucket has read/write/full control
-	 * 
+	 *
 	 * @param grants
 	 * @param accessTypeToCheck
 	 * @return List<Permission>, if permissions found else empty
@@ -107,125 +89,128 @@ public class S3PacbotUtils {
 			}
 		} catch (AmazonS3Exception s3Exception) {
 			logger.error("error : ", s3Exception);
+			if (s3Exception.getMessage().contains("The specified bucket does not exist")) {
+				throw new RuntimeException(s3Exception);
+			}
 			throw new RuleExecutionFailedExeption(s3Exception.getMessage());
 		}
 		return permissionList;
 	}
 
 	public static Map<String, Boolean> getPublicAccessPolicy(AmazonS3Client awsS3Client, String s3BucketName, String accessType) {
-		
+
 		Map<String, Boolean> map = new HashMap<>();
 		JsonArray jsonArray = getPolicyArray(awsS3Client, s3BucketName);
 
 		if (jsonArray.size() > 0) {
-            for (int i = 0; i < jsonArray.size(); i++) {
-            	JsonObject principal = new JsonObject();
-            	String actionString = null;
-        		String conditionStr = null;
-        		String effect = null;
-        		String principalStr = null;
-        		String aws = null;
-        		
-        		JsonObject conditionJsonObject = new JsonObject();
-        		JsonArray conditionJsonArray = new JsonArray();
-        		JsonArray actionJsonArray = new JsonArray();
-        		List<String> conditionList = new ArrayList<>();
-        		
-                JsonObject firstObject = (JsonObject) jsonArray.get(i);
-                if (firstObject.has(PacmanRuleConstants.PRINCIPAL) && firstObject.get(PacmanRuleConstants.PRINCIPAL).isJsonObject()) {
-                     principal = firstObject.get(PacmanRuleConstants.PRINCIPAL).getAsJsonObject();
-                }else{
-                	if(firstObject.has(PacmanRuleConstants.PRINCIPAL)){
-                	principalStr = firstObject.get(PacmanRuleConstants.PRINCIPAL).getAsString();
-                	}
-                }
-                    try {
-                        if (principal.has("AWS") || "*".equals(principalStr)) {
-                           
-                            JsonArray awsArray = null;
-                            if (principal.has("AWS") && principal.get("AWS").isJsonArray()) {
-                                awsArray = principal.get("AWS").getAsJsonArray();
-                                if (awsArray.size() > 0) {
-                                    logger.debug(
-                                            "Not checking the s3 read/write public access for principal array values : {}",
-                                            awsArray);
-                                }
-                            }
-                            
-                            if(principal.has("AWS") && !principal.get("AWS").isJsonArray()){
-                                aws = principal.get("AWS").getAsString();
-                            }
-							if ("*".equals(principalStr)) {
-								aws = firstObject.get(PacmanRuleConstants.PRINCIPAL).getAsString();
+			for (int i = 0; i < jsonArray.size(); i++) {
+				JsonObject principal = new JsonObject();
+				String actionString = null;
+				String conditionStr = null;
+				String effect = null;
+				String principalStr = null;
+				String aws = null;
+
+				JsonObject conditionJsonObject = new JsonObject();
+				JsonArray conditionJsonArray = new JsonArray();
+				JsonArray actionJsonArray = new JsonArray();
+				List<String> conditionList = new ArrayList<>();
+
+				JsonObject firstObject = (JsonObject) jsonArray.get(i);
+				if (firstObject.has(PacmanRuleConstants.PRINCIPAL) && firstObject.get(PacmanRuleConstants.PRINCIPAL).isJsonObject()) {
+					principal = firstObject.get(PacmanRuleConstants.PRINCIPAL).getAsJsonObject();
+				} else {
+					if (firstObject.has(PacmanRuleConstants.PRINCIPAL)) {
+						principalStr = firstObject.get(PacmanRuleConstants.PRINCIPAL).getAsString();
+					}
+				}
+				try {
+					if (principal.has("AWS") || "*".equals(principalStr)) {
+
+						JsonArray awsArray = null;
+						if (principal.has("AWS") && principal.get("AWS").isJsonArray()) {
+							awsArray = principal.get("AWS").getAsJsonArray();
+							if (awsArray.size() > 0) {
+								logger.debug(
+										"Not checking the s3 read/write public access for principal array values : {}",
+										awsArray);
 							}
-                            
-                            if ("*".equals(aws) && !firstObject.has(PacmanRuleConstants.CONDITION)) {
-                            	if (firstObject.get(PacmanRuleConstants.ACTION).isJsonObject()) {
-                                    JsonObject actionJson = firstObject.get(PacmanRuleConstants.ACTION).getAsJsonObject();
-                                    actionString = actionJson.getAsString();
-                                } else if (firstObject.get(PacmanRuleConstants.ACTION).isJsonArray()) {
-                                    actionJsonArray = firstObject.get(PacmanRuleConstants.ACTION).getAsJsonArray();
-                                } else {
-                                    actionString = firstObject.get(PacmanRuleConstants.ACTION).getAsString();
-                                }
+						}
 
-                                effect = firstObject.get(PacmanRuleConstants.EFFECT).getAsString();
-                                if (firstObject.has(PacmanRuleConstants.CONDITION)
-                                        && (firstObject.get(PacmanRuleConstants.CONDITION).getAsJsonObject()
-                                                .has(PacmanRuleConstants.IP_ADDRESS_CAP))
-                                        && (firstObject.get(PacmanRuleConstants.CONDITION).getAsJsonObject()
-                                                .get(PacmanRuleConstants.IP_ADDRESS_CAP).getAsJsonObject()
-                                                .has(PacmanRuleConstants.SOURCE_IP))) {
-                                    if (firstObject.get(PacmanRuleConstants.CONDITION).getAsJsonObject()
-                                            .get(PacmanRuleConstants.IP_ADDRESS_CAP).getAsJsonObject()
-                                            .get(PacmanRuleConstants.SOURCE_IP).isJsonObject()) {
-                                        conditionJsonObject = firstObject.get(PacmanRuleConstants.CONDITION)
-                                                .getAsJsonObject().get(PacmanRuleConstants.IP_ADDRESS_CAP)
-                                                .getAsJsonObject().get(PacmanRuleConstants.SOURCE_IP).getAsJsonObject();
-                                    } else if (firstObject.get(PacmanRuleConstants.CONDITION).getAsJsonObject()
-                                            .get(PacmanRuleConstants.IP_ADDRESS_CAP).getAsJsonObject()
-                                            .get(PacmanRuleConstants.SOURCE_IP).isJsonArray()) {
-                                        conditionJsonArray = firstObject.get(PacmanRuleConstants.CONDITION)
-                                                .getAsJsonObject().get(PacmanRuleConstants.IP_ADDRESS_CAP)
-                                                .getAsJsonObject().get(PacmanRuleConstants.SOURCE_IP).getAsJsonArray();
-                                    } else {
-                                        conditionStr = firstObject.get(PacmanRuleConstants.CONDITION).getAsJsonObject()
-                                                .get(PacmanRuleConstants.IP_ADDRESS_CAP).getAsJsonObject()
-                                                .get(PacmanRuleConstants.SOURCE_IP).getAsString();
-                                    }
-                                }
+						if (principal.has("AWS") && !principal.get("AWS").isJsonArray()) {
+							aws = principal.get("AWS").getAsString();
+						}
+						if ("*".equals(principalStr)) {
+							aws = firstObject.get(PacmanRuleConstants.PRINCIPAL).getAsString();
+						}
 
-                                JsonElement cJson = conditionJsonArray;
-                                Type listType = new TypeToken<List<String>>() {
-                                }.getType();
-                               
-                               conditionList = new Gson().fromJson(cJson, listType);
-                                if (!org.apache.commons.lang.StringUtils.isEmpty(actionString)) {
-                                    map = getReadWriteAccess(actionString, accessType, effect, conditionJsonObject,
-                                            conditionList, conditionStr, map);
-                                }
-                                if (actionJsonArray.size() > 0) {
-                                    for (int j = 0; j < actionJsonArray.size(); j++) {
-                                        actionString = actionJsonArray.get(j).getAsString();
-                                        map = getReadWriteAccess(actionString, accessType, effect, conditionJsonObject,
-                                                conditionList, conditionStr, map);
-                                    }
-                                }
-                            }
-                            }
-                    } catch (Exception e1) {
-                        logger.error("error", e1);
-                        throw new RuleExecutionFailedExeption(e1.getMessage());
-                    }
+						if ("*".equals(aws) && !firstObject.has(PacmanRuleConstants.CONDITION)) {
+							if (firstObject.get(PacmanRuleConstants.ACTION).isJsonObject()) {
+								JsonObject actionJson = firstObject.get(PacmanRuleConstants.ACTION).getAsJsonObject();
+								actionString = actionJson.getAsString();
+							} else if (firstObject.get(PacmanRuleConstants.ACTION).isJsonArray()) {
+								actionJsonArray = firstObject.get(PacmanRuleConstants.ACTION).getAsJsonArray();
+							} else {
+								actionString = firstObject.get(PacmanRuleConstants.ACTION).getAsString();
+							}
+
+							effect = firstObject.get(PacmanRuleConstants.EFFECT).getAsString();
+							if (firstObject.has(PacmanRuleConstants.CONDITION)
+									&& (firstObject.get(PacmanRuleConstants.CONDITION).getAsJsonObject()
+									.has(PacmanRuleConstants.IP_ADDRESS_CAP))
+									&& (firstObject.get(PacmanRuleConstants.CONDITION).getAsJsonObject()
+									.get(PacmanRuleConstants.IP_ADDRESS_CAP).getAsJsonObject()
+									.has(PacmanRuleConstants.SOURCE_IP))) {
+								if (firstObject.get(PacmanRuleConstants.CONDITION).getAsJsonObject()
+										.get(PacmanRuleConstants.IP_ADDRESS_CAP).getAsJsonObject()
+										.get(PacmanRuleConstants.SOURCE_IP).isJsonObject()) {
+									conditionJsonObject = firstObject.get(PacmanRuleConstants.CONDITION)
+											.getAsJsonObject().get(PacmanRuleConstants.IP_ADDRESS_CAP)
+											.getAsJsonObject().get(PacmanRuleConstants.SOURCE_IP).getAsJsonObject();
+								} else if (firstObject.get(PacmanRuleConstants.CONDITION).getAsJsonObject()
+										.get(PacmanRuleConstants.IP_ADDRESS_CAP).getAsJsonObject()
+										.get(PacmanRuleConstants.SOURCE_IP).isJsonArray()) {
+									conditionJsonArray = firstObject.get(PacmanRuleConstants.CONDITION)
+											.getAsJsonObject().get(PacmanRuleConstants.IP_ADDRESS_CAP)
+											.getAsJsonObject().get(PacmanRuleConstants.SOURCE_IP).getAsJsonArray();
+								} else {
+									conditionStr = firstObject.get(PacmanRuleConstants.CONDITION).getAsJsonObject()
+											.get(PacmanRuleConstants.IP_ADDRESS_CAP).getAsJsonObject()
+											.get(PacmanRuleConstants.SOURCE_IP).getAsString();
+								}
+							}
+
+							JsonElement cJson = conditionJsonArray;
+							Type listType = new TypeToken<List<String>>() {
+							}.getType();
+
+							conditionList = new Gson().fromJson(cJson, listType);
+							if (!org.apache.commons.lang.StringUtils.isEmpty(actionString)) {
+								map = getReadWriteAccess(actionString, accessType, effect, conditionJsonObject,
+										conditionList, conditionStr, map);
+							}
+							if (actionJsonArray.size() > 0) {
+								for (int j = 0; j < actionJsonArray.size(); j++) {
+									actionString = actionJsonArray.get(j).getAsString();
+									map = getReadWriteAccess(actionString, accessType, effect, conditionJsonObject,
+											conditionList, conditionStr, map);
+								}
+							}
+						}
+					}
+				} catch (Exception e1) {
+					logger.error("error", e1);
+					throw new RuleExecutionFailedExeption(e1.getMessage());
+				}
+			}
 		}
-        }
 		return map;
 	}
 
 	private static Map<String, Boolean> getReadWriteAccess(String actionString,
-			String accessType, String effect, JsonObject conditionJsonObject,
-			List<String> conditionList, String conditionStr,
-			Map<String, Boolean> accessMap) {
+														   String accessType, String effect, JsonObject conditionJsonObject,
+														   List<String> conditionList, String conditionStr,
+														   Map<String, Boolean> accessMap) {
 		if ((actionString.startsWith(PacmanRuleConstants.S3_PUT) || actionString
 				.startsWith("s3:*"))
 				&& accessType.contains(PacmanRuleConstants.WRITE_ACCESS)
@@ -257,7 +242,7 @@ public class S3PacbotUtils {
 			if (conditionJsonObject.toString().equals(publicIp)) {
 				accessMap.put(type, true);
 			}
-		} 
+		}
 		if (null != conditionStr && conditionStr.contains(publicIp)) {
 			accessMap.put(type, true);
 		}
@@ -268,7 +253,7 @@ public class S3PacbotUtils {
 	}
 
 	public static Map<String, Boolean> checkS3HasOpenAccess(String checkId,
-			String accountId, String esUrl, String resourceId) throws Exception {
+															String accountId, String esUrl, String resourceId) throws Exception {
 		Map<String, Boolean> publicAccess = new HashMap<>();
 		String resourceinfo = PacmanUtils.getQueryDataForCheckid(checkId, esUrl, resourceId, null, accountId);
 		if (org.apache.commons.lang.StringUtils.isNotEmpty(resourceinfo)) {
@@ -314,12 +299,12 @@ public class S3PacbotUtils {
 		return publicAccess;
 	}
 
-	private static JsonArray getPolicyArray(AmazonS3Client awsS3Client,String s3BucketName) {
+	private static JsonArray getPolicyArray(AmazonS3Client awsS3Client, String s3BucketName) {
 		JsonParser jsonParser = new JsonParser();
 		JsonArray policyJsonArray = new JsonArray();
 		BucketPolicy bucketPolicy = awsS3Client.getBucketPolicy(s3BucketName);
 
-		
+
 		if (!com.amazonaws.util.StringUtils.isNullOrEmpty(bucketPolicy.getPolicyText())) {
 			JsonObject resultJson = (JsonObject) jsonParser.parse(bucketPolicy.getPolicyText());
 			policyJsonArray = resultJson.get("Statement").getAsJsonArray();
