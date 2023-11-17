f61c4d728acb22f874a30bb40fe3fe6ad0273d0e
kushZemoso <kushagra.jain@zemosolabs.com>
2023-11-17 12:40:24 +0530

    feat:[CORE-1598] Added a new policy to check public access is not enabled for the S3 bucket used to store CloudTrail logs.

---
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/awsrules/cloudtrail/DenyPublicAccessS3BucketStoresCloudTrailLogs.java
@@ -0,0 +1,150 @@
+/*******************************************************************************
+ *Copyright <2023> Paladin Cloud, Inc or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not use
+ * this file except in compliance with the License. A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.tmobile.cloud.awsrules.cloudtrail;
+import com.amazonaws.util.StringUtils;
+import com.google.common.collect.HashMultimap;
+import com.google.gson.*;
+import com.nimbusds.oauth2.sdk.util.MapUtils;
+import com.tmobile.cloud.awsrules.utils.PacmanUtils;
+import com.tmobile.cloud.awsrules.utils.RulesElasticSearchRepositoryUtil;
+import com.tmobile.cloud.constants.PacmanRuleConstants;
+import com.tmobile.pacman.commons.PacmanSdkConstants;
+import com.tmobile.pacman.commons.exception.InvalidInputException;
+import com.tmobile.pacman.commons.exception.RuleExecutionFailedExeption;
+import com.tmobile.pacman.commons.policy.Annotation;
+import com.tmobile.pacman.commons.policy.BasePolicy;
+import com.tmobile.pacman.commons.policy.PacmanPolicy;
+import com.tmobile.pacman.commons.policy.PolicyResult;
+import com.tmobile.pacman.commons.utils.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.*;
+
+@PacmanPolicy(key = "deny-public-access-for-s3-bucket-that-stores-cloudtrail-logs", desc = "This rule checks S3 bucket that stores cloudtrail Logs should not be publicly accessible", severity = "critical", category = PacmanSdkConstants.SECURITY)
+public class DenyPublicAccessS3BucketStoresCloudTrailLogs extends BasePolicy {
+    private static final Logger logger = LoggerFactory.getLogger(DenyPublicAccessS3BucketStoresCloudTrailLogs.class);
+
+    @Override
+    public PolicyResult execute(final Map<String, String> ruleParam, Map<String, String> resourceAttributes) {
+
+        MDC.put(PacmanRuleConstants.EXECUTION_ID, ruleParam.get(PacmanRuleConstants.EXECUTION_ID));
+        MDC.put(PacmanRuleConstants.RULE_ID, ruleParam.get(PacmanSdkConstants.POLICY_ID));
+
+        String severity = ruleParam.get(PacmanRuleConstants.SEVERITY);
+        String category = ruleParam.get(PacmanRuleConstants.CATEGORY);
+
+        if (MapUtils.isNotEmpty(ruleParam) && !PacmanUtils.doesAllHaveValue(severity, category)) {
+            logger.info(PacmanRuleConstants.MISSING_CONFIGURATION);
+            throw new InvalidInputException(PacmanRuleConstants.MISSING_CONFIGURATION);
+        }
+
+        String esUrl = CommonUtils.getEnvVariableValue(PacmanSdkConstants.ES_URI_ENV_VAR_NAME);
+        String url = CommonUtils.getEnvVariableValue(PacmanSdkConstants.ES_URI_ENV_VAR_NAME);
+        if (!StringUtils.isNullOrEmpty(url)) {
+            esUrl = url + "/aws/s3/_search";
+        }
+
+        String s3BucketName = resourceAttributes.get("s3bucketname");
+        String accountId = resourceAttributes.get(PacmanRuleConstants.ACCOUNTID);
+        boolean isPublicAccessEnabled = false;
+        if (!StringUtils.isNullOrEmpty(s3BucketName)) {
+            Map<String, Object> mustFilter = new HashMap<>();
+            mustFilter.put(PacmanUtils.convertAttributetoKeyword(PacmanRuleConstants.NAME), s3BucketName);
+            mustFilter.put(PacmanUtils.convertAttributetoKeyword(PacmanRuleConstants.ACCOUNTID),accountId);
+            mustFilter.put(PacmanRuleConstants.LATEST, true);
+
+        try {
+            isPublicAccessEnabled = checkForS3BucketCloudtrailLogsPublicAccess(esUrl, mustFilter);
+        } catch (Exception e) {
+            logger.error("unable to determine", e);
+            throw new RuleExecutionFailedExeption("unable to determine" + e);
+        }
+
+            if (isPublicAccessEnabled) {
+                List<LinkedHashMap<String, Object>> issueList = new ArrayList<>();
+                LinkedHashMap<String, Object> issue = new LinkedHashMap<>();
+                Annotation annotation = null;
+                annotation = Annotation.buildAnnotation(ruleParam, Annotation.Type.ISSUE);
+                annotation.put(PacmanSdkConstants.DESCRIPTION,"Ensure S3 bucket that stores cloudtrail Logs is not publicly accessible");
+                annotation.put(PacmanRuleConstants.SEVERITY, severity);
+                annotation.put(PacmanRuleConstants.CATEGORY, category);
+                annotation.put(PacmanRuleConstants.NAME,resourceAttributes.get(PacmanRuleConstants.NAME));
+                issue.put(PacmanRuleConstants.VIOLATION_REASON,
+                        ruleParam.get(PacmanRuleConstants.RULE_ID) + " Violation Found!");
+                issueList.add(issue);
+                annotation.put(PacmanRuleConstants.ISSUE_DETAILS, issueList.toString());
+                logger.debug("S3 bucket that stores cloudtrail Logs is publicly accessible");
+                return new PolicyResult(PacmanSdkConstants.STATUS_FAILURE, PacmanRuleConstants.FAILURE_MESSAGE,
+                        annotation);
+            }
+        }
+        logger.debug("S3 bucket that stores cloudtrail Logs is not publicly accessible");
+        logger.debug("========DenyPublicAccessS3BucketStoresCloudTrailLogs ended=========");
+        return new PolicyResult(PacmanSdkConstants.STATUS_SUCCESS, PacmanRuleConstants.SUCCESS_MESSAGE);
+    }
+
+    private boolean checkForS3BucketCloudtrailLogsPublicAccess(String esUrl, Map<String, Object> mustFilter) throws Exception {
+        logger.info("Validating the resource data from elastic search. ES URL:{}, FilterMap : {}", esUrl, mustFilter);
+        boolean validationResult = false;
+        JsonParser parser = new JsonParser();
+        JsonObject resultJson = RulesElasticSearchRepositoryUtil.getQueryDetailsFromES(esUrl, mustFilter,
+                new HashMap<>(),
+                HashMultimap.create(), null, 0, new HashMap<>(), null, null);
+
+        if (resultJson.has(PacmanRuleConstants.HITS)) {
+            String hitsString = resultJson.get(PacmanRuleConstants.HITS).toString();
+            JsonObject hitsJson = JsonParser.parseString(hitsString).getAsJsonObject();
+            JsonArray hitsJsonArray = hitsJson.getAsJsonObject().get(PacmanRuleConstants.HITS).getAsJsonArray();
+            if (!hitsJsonArray.isEmpty()) {
+                    JsonObject source = (JsonObject) ((JsonObject) hitsJsonArray.get(0))
+                            .get(PacmanRuleConstants.SOURCE);
+                    if (source.has(PacmanRuleConstants.ES_BKT_POLICY_ATTRIBUTE)) {
+                        String bucketPolicyString = source.get(PacmanRuleConstants.ES_BKT_POLICY_ATTRIBUTE).getAsString();
+
+                        JsonObject bucketPolicyObject = null;
+                        try {
+                            bucketPolicyObject = new Gson().fromJson(bucketPolicyString, JsonObject.class);
+                        } catch (JsonSyntaxException e) {
+                            logger.error("Error parsing bucket policy JSON: {}", e.getMessage());
+                        }
+
+                        if (bucketPolicyObject != null && bucketPolicyObject.has(PacmanRuleConstants.STATEMENT)) {
+                            JsonArray bucketStatementList = bucketPolicyObject.getAsJsonArray(PacmanRuleConstants.STATEMENT);
+
+                            for (JsonElement bucketStatement : bucketStatementList) {
+                                JsonObject statementObject = bucketStatement.getAsJsonObject();
+
+                                String effect = statementObject.get(PacmanRuleConstants.EFFECT).getAsString();
+                                JsonObject principal = statementObject.getAsJsonObject(PacmanRuleConstants.PRINCIPAL);
+                                String service = principal.get(PacmanRuleConstants.SERVICE).getAsString();
+
+                                if (effect.equalsIgnoreCase("Allow") && service.equalsIgnoreCase("*")) {
+                                    validationResult = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        return validationResult;
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Checks the S3 bucket that stores cloudtrail Logs should not be publicly accessible.";
+    }
+}
---
+++ jobs/pacman-awsrules/src/test/java/com/tmobile/cloud/awsrules/cloudtrail/DenyPublicAccessS3BucketStoresCloudTrailLogsTest.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+        *Copyright <2023> Paladin Cloud, Inc or its affiliates. All Rights Reserved.
+        *
+        * Licensed under the Apache License, Version 2.0 (the "License"); You may not use
+        * this file except in compliance with the License. A copy of the License is located at
+        *
+        * http://www.apache.org/licenses/LICENSE-2.0
+        *
+        * or in the "license" file accompanying this file. This file is distributed on
+        * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or
+        * implied. See the License for the specific language governing permissions and
+        * limitations under the License.
+ ******************************************************************************/
+package com.tmobile.cloud.awsrules.cloudtrail;
+import com.tmobile.cloud.awsrules.utils.CommonTestUtils;
+import com.tmobile.cloud.awsrules.utils.PacmanUtils;
+import com.tmobile.pacman.commons.exception.InvalidInputException;
+import com.tmobile.pacman.commons.policy.Annotation;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({PacmanUtils.class, Annotation.class})
+public class DenyPublicAccessS3BucketStoresCloudTrailLogsTest {
+
+    @InjectMocks
+    DenyPublicAccessS3BucketStoresCloudTrailLogs denyPublicAccessS3BucketStoresCloudTrailLogs;
+
+    @Test
+    public void executeTest() throws Exception{
+        mockStatic(Annotation.class);
+        when(Annotation.buildAnnotation(anyObject(),anyObject())).thenReturn(CommonTestUtils.getMockAnnotation());
+        mockStatic(PacmanUtils.class);
+        when(PacmanUtils.doesAllHaveValue(anyString(),anyString())).thenReturn(
+                true);
+        assertThat(denyPublicAccessS3BucketStoresCloudTrailLogs.execute(getMapString("r_123 "),getMapString("r_123 ")), is(notNullValue()));
+        when(PacmanUtils.doesAllHaveValue(anyString(),anyString())).thenReturn(
+                false);
+        assertThatThrownBy(
+                () -> denyPublicAccessS3BucketStoresCloudTrailLogs.execute(getMapString("r_123 "),getMapString("r_123 "))).isInstanceOf(InvalidInputException.class);
+    }
+
+    @Test
+    public void getHelpTextTest(){
+        assertThat(denyPublicAccessS3BucketStoresCloudTrailLogs.getHelpText(), is(notNullValue()));
+    }
+
+    public static Map<String, String> getMapString(String passRuleResourceId) {
+        Map<String, String> commonMap = new HashMap<>();
+        commonMap.put("executionId", "1234");
+        commonMap.put("_resourceid", passRuleResourceId);
+        commonMap.put("severity", "critical");
+        commonMap.put("ruleCategory", "security");
+        commonMap.put("type", "Task");
+        commonMap.put("accountid", "12345");
+        commonMap.put("ruleId", "deny_public_access_to_s3_bucket_used_to_store_cloudtrail_logs");
+        commonMap.put("policyId","deny_public_access_to_s3_bucket_used_to_store_cloudtrail_logs");
+        commonMap.put("policyVersion", "version-1");
+        String bucketPolicyString = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Sid\":\"AWSCloudTrailAclCheck20150319\",\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"cloudtrail.amazonaws.com\"},\"Action\":\"s3:GetBucketAcl\",\"Resource\":\"arn:aws:s3:::aws-cloudtrail-logs-50283-122123\",\"Condition\":{\"StringEquals\":{\"AWS:SourceArn\":\"arn:aws:cloudtrail:us-east-1:500222:trail/management-events\"}}},{\"Sid\":\"AWSCloudTrailWrite20150319\",\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"cloudtrail.amazonaws.com\"},\"Action\":\"s3:PutObject\",\"Resource\":\"arn:aws:s3:::aws-cloudtrail-logs-51211-12111/AWSLogs/5012111/*\",\"Condition\":{\"StringEquals\":{\"AWS:SourceArn\":\"arn:aws:cloudtrail:us-east-1:50012111:trail/management-events\",\"s3:x-amz-acl\":\"bucket-owner-full-control\"}}}]}";
+        commonMap.put("bucketpolicy",bucketPolicyString);
+        return commonMap;
+    }
+}
