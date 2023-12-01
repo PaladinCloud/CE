f7c0d0ca92f2fcccfb33b5ff26cbea4cf17c6a41
ershad-zemoso <ershad.tj@zemosolabs.com>
2023-12-01 17:15:55 +0530

    fix:[CORE-1702] Set default logger level to ERROR for notification lambdas

--- lambda-functions/notification-send-email-service/src/main/java/com/paladincloud/InvokeNotificationsApi.java
+++ lambda-functions/notification-send-email-service/src/main/java/com/paladincloud/InvokeNotificationsApi.java
@@ -1,44 +1,43 @@
 package com.paladincloud;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.paladincloud.utils.HttpUtil;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.paladincloud.utils.HttpUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Optional;
 
 public class InvokeNotificationsApi {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InvokeNotificationsApi.class);
 
     public String handleRequest(Map<String,Object> event, Context context)
     {
         Gson gsonObj = new Gson();
-        LambdaLogger logger = context.getLogger();
 
         String response = new String("hello paladin cloud");
-        logger.log("inside handleRequest "+event);
+        LOGGER.info("inside handleRequest "+event);
         String jsonStr = gsonObj.toJson(event);
-        System.out.println("jsonStr -- "+jsonStr);
+        LOGGER.info("jsonStr -- "+jsonStr);
         JsonParser jsonParser = new JsonParser();
         JsonObject policyParamsJson = (JsonObject) jsonParser.parse(jsonStr);
 
         JsonElement bodyJson = policyParamsJson.getAsJsonArray("Records").get(0);
-        System.out.println("bodyJson--"+bodyJson.toString());
+        LOGGER.info("bodyJson--"+bodyJson.toString());
         String payload = bodyJson.getAsJsonObject().get("Sns").getAsJsonObject().get("Message").getAsString();
 
-        logger.log("payload for invoking notifications "+payload);
+        LOGGER.info("payload for invoking notifications "+payload);
 
         try {
             String invokeNotificationUrl = System.getenv("INVOKE_NOTIFICATION_URL");
             String token = HttpUtil.getToken();
-            logger.log(" token -"+token);
+            LOGGER.info(" token -"+token);
             String notificationSettingsJson = HttpUtil.post(invokeNotificationUrl,payload,token,"Bearer");
-            logger.log("notification settings response -"+notificationSettingsJson);
+            LOGGER.info("notification settings response -"+notificationSettingsJson);
 
         } catch (Exception e) {
             throw new RuntimeException(e);
