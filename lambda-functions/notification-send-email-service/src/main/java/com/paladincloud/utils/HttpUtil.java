e0a6c3bf466bc231567caf3ed66c75ab247a9875
ershad-zemoso <ershad.tj@zemosolabs.com>
2023-12-07 15:13:28 +0530

    fix:[CORE-1702] Set default logger level to ERROR for notification lamdas

--- lambda-functions/notification-send-email-service/src/main/java/com/paladincloud/utils/HttpUtil.java
+++ lambda-functions/notification-send-email-service/src/main/java/com/paladincloud/utils/HttpUtil.java
@@ -34,6 +34,8 @@ import org.apache.http.ssl.TrustStrategy;
 import org.apache.http.util.EntityUtils;
 
 import com.google.common.base.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -41,6 +43,7 @@ import com.google.common.base.Strings;
  */
 public class HttpUtil {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpUtil.class);
     private static final String API_READ_SCOPE = "API_OPERATION/READ";
 
     private HttpUtil() {
@@ -65,14 +68,14 @@ public class HttpUtil {
             if (entity != null) {
                 // return it as a String
                 result = EntityUtils.toString(entity);
-                System.out.println(result);
+                LOGGER.info(result);
             }
             if( httpResponse.getStatusLine().getStatusCode()==HttpStatus.SC_UNAUTHORIZED){
                 throw new Exception("User unauthorized !!");
             }
 
         } catch (Exception exception) {
-            System.out.println("Error inside fetch notification settings " + exception.getMessage());
+            LOGGER.error("Error inside fetch notification settings " + exception.getMessage());
         }
         return result;
     }
@@ -109,7 +112,7 @@ public class HttpUtil {
                 return EntityUtils.toString(httpresponse.getEntity());
             }
         } catch (Exception e) {
-            System.out.println("Error getting the data " +e.getMessage());
+            LOGGER.error("Error getting the data " +e.getMessage());
             throw e;
         }
         return null;
@@ -132,7 +135,7 @@ public class HttpUtil {
                         }
                     }).build()).build();
         } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
-            System.out.println("Error getting getHttpClient "+e.getMessage());
+            LOGGER.error("Error getting getHttpClient "+e.getMessage());
         }
         return httpClient;
     }
@@ -141,7 +144,7 @@ public class HttpUtil {
             return new ObjectMapper().readValue(json, new TypeReference<Map<String, Object>>() {
             });
         } catch (IOException e) {
-            System.out.println("Error in parseJson "+e.getMessage());
+            LOGGER.error("Error in parseJson "+e.getMessage());
         }
         return new HashMap<>();
     }
@@ -173,7 +176,7 @@ public class HttpUtil {
                 .setDefaultCredentialsProvider(provider)
                 .build();
              CloseableHttpResponse httpResponse = httpClient.execute(request)) {
-            System.out.println("statuscode is "+httpResponse.getStatusLine().getStatusCode());
+            LOGGER.info("statuscode is "+httpResponse.getStatusLine().getStatusCode());
             if(httpResponse.getStatusLine().getStatusCode()!=200){
                 throw new Exception("Could not fetch topic arn for notification channels!");
             }
