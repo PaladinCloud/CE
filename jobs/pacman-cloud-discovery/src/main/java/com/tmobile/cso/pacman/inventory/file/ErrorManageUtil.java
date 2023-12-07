7792c4f8898d684cdc65d24fb05c66ef10f0177c
aishwaryaKulkarni97 <aishwarya.kulkarni@paladincloud.io>
2023-12-07 14:43:10 +0530

    fix:[CORE-1754] Omit ops and permission issue

--- jobs/pacman-cloud-discovery/src/main/java/com/tmobile/cso/pacman/inventory/file/ErrorManageUtil.java
+++ jobs/pacman-cloud-discovery/src/main/java/com/tmobile/cso/pacman/inventory/file/ErrorManageUtil.java
@@ -161,7 +161,7 @@ public class ErrorManageUtil {
     }
 
     private static void omitPermissionErrors(List<ErrorVH> errorVHList, Map<String, List<String>> assetPermissionMapping, ErrorVH errorVH, List<String> permissionIssues) {
-        List<String> exceptionList = Arrays.asList("AccessDeniedException", "SubscriptionRequiredException", "AWSSupportException", "Amazon Web Services Premium Support Subscription is required to use this service", "not subscribed to AWS Security Hub", "is not authorized to perform: sts:AssumeRole", "InsufficientPrivilegesException","ValidationError");
+        List<String> exceptionList = Arrays.asList("AccessDenied", "SubscriptionRequiredException", "AWSSupportException", "Amazon Web Services Premium Support Subscription is required to use this service", "not subscribed to AWS Security Hub", "is not authorized to perform: sts:AssumeRole", "InsufficientPrivilegesException","ValidationError","AuthorizationError");
         if (exceptionPresentInList(errorVH.getException(), exceptionList)) {
             shortenMessageForKMS(errorVH);
             if (permissionIssues != null && !errorVH.getType().equals("phd") && !errorVH.getType().equals("security hub")) {
@@ -186,7 +186,8 @@ public class ErrorManageUtil {
         if (errorVH.getException().contains("AWS was not able to validate the provided access credentials")
                 || errorVH.getException().contains("The security token included in the request is invalid")
                 || errorVH.getException().contains("Unable to execute HTTP request: elasticbeanstalk")
-                || errorVH.getException().contains("Rate exceeded")) {
+                || errorVH.getException().contains("Rate exceeded")
+                || errorVH.getException().contains("ListenerNotFound")) {
             errorVHList.remove(errorVH);
         }
     }
