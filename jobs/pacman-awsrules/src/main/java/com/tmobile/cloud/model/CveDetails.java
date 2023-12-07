55adda5293ceeadb5f9d4c54d6715e66755eb2e9
akashpaladin <114460784+akashpaladin@users.noreply.github.com>
2023-12-07 06:37:28 +0000

    [PLG-392] changes for adding contrast policy (#1918)
    * [PLG-392] changes for adding contrast policy
    
    * Changes related to review commets
    changed policy id
    
    * Changes for review comments
    
    * Changes related to review comments

---
+++ jobs/pacman-awsrules/src/main/java/com/tmobile/cloud/model/CveDetails.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ *  Copyright 2023 Paladin Cloud, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License.  You may obtain a copy
+ *  of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ ******************************************************************************/
+package com.tmobile.cloud.model;
+
+public class CveDetails {
+    private String id;
+    private String url;
+
+    public CveDetails() {
+    }
+
+    public CveDetails(String id, String url) {
+        this.id = id;
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id='" + id + '\'' +
+                ",url='" + url + '\'' +
+                '}';
+    }
+}
