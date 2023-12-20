74ea0d9eb81ebf430cf5074d28883e1bdcb2f342
JaysinhGo <jay@paladincloud.io>
2023-12-20 14:51:53 +0530

    feat/UIUX-569 : table search filter autofocus

--- webapp/src/app/shared/table-filters/table-filter-search/table-filter-search.component.ts
+++ webapp/src/app/shared/table-filters/table-filter-search/table-filter-search.component.ts
@@ -1,15 +1,19 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, AfterViewInit, Output, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
     selector: 'app-table-filter-search',
     templateUrl: './table-filter-search.component.html',
     styleUrls: ['./table-filter-search.component.css'],
 })
-export class TableFilterSearchComponent implements OnInit {
+export class TableFilterSearchComponent implements AfterViewInit {
     @Input() text = '';
     @Output() textChange = new EventEmitter<string>();
+    @ViewChild('filterSearchInput', { static: false }) filterSearchInput: ElementRef;
 
-    constructor() {}
+    constructor(private renderer: Renderer2) {}
 
-    ngOnInit(): void {}
+    ngAfterViewInit(): void {
+        // Focus on the input field when the component loads
+        this.renderer.selectRootElement(this.filterSearchInput.nativeElement).focus();
+    }
 }
