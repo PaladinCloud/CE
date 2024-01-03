5af16cfbd75dcc6cb46bf16ced81a58ecd175c05
Raviteja Mandala <raviteja.mandala@zemosolabs.com>
2024-01-03 18:48:07 +0530

    fix:[CORE-1832] if fre-auto-fix-tran-log index is not created yet and statistics api gets exception, then set the log level to info instead of error to avoid creating data alert

--- jobs/pacman-rule-engine-2.0/src/main/java/com/tmobile/pacman/common/PacmanSdkConstants.java
+++ jobs/pacman-rule-engine-2.0/src/main/java/com/tmobile/pacman/common/PacmanSdkConstants.java
@@ -682,4 +682,5 @@ public interface PacmanSdkConstants {
     String ACCOUNT_ID = "accountid";
     String TAGGING_MANDATORY_TAGS = "tagging.mandatoryTags";
     String POLICY_NAME = "policyName";
+    String AUTO_FIX_ENABLED = "autoFixEnabled";
 }
