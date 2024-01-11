91d0796caf9ece48119efa8d74efe5fafdf4d6aa
Pavel Lyubich <pavel@lyubich.com>
2024-01-10 21:42:40 -0800

    fixing asset types

--- jobs/azure-discovery/src/main/java/com/tmobile/pacbot/azure/inventory/collector/RegisteredApplicationInventoryCollector.java
+++ jobs/azure-discovery/src/main/java/com/tmobile/pacbot/azure/inventory/collector/RegisteredApplicationInventoryCollector.java
@@ -18,143 +18,147 @@ import java.util.List;
 @Component
 public class RegisteredApplicationInventoryCollector {
 
-	// constans for API data
-	private static final String VALUE = "value";
-	private static final String NEXT_DATASET = "@odata.nextLink";
+    // constants for API data
+    private static final String VALUE = "value";
+    private static final String NEXT_DATASET = "@odata.nextLink";
 
-	// constants for the application details
-	private static final String OBJECT_ID = "id";
-	private static final String APP_ID = "appId";
-	private static final String CREATE_DATETIME = "createdDateTime";
-	private static final String DISPLAYNAME = "displayName";
-	private static final String PUBLISHERDOMAIN = "publisherDomain";
-	private static final String CERTIFICATE_DATA = "keyCredentials";
-	private static final String SECRET_DATA = "passwordCredentials";
+    // constants for the application details
+    private static final String OBJECT_ID = "id";
+    private static final String APP_ID = "appId";
+    private static final String CREATE_DATETIME = "createdDateTime";
+    private static final String DISPLAYNAME = "displayName";
+    private static final String PUBLISHERDOMAIN = "publisherDomain";
+    private static final String CERTIFICATE_DATA = "keyCredentials";
+    private static final String SECRET_DATA = "passwordCredentials";
 
-	// constants for secret keys
-	private static final String CUSTOM_KEY_IDENTIFIER = "customKeyIdentifier";
-	private static final String END_DATETIME = "endDateTime";
-	private static final String START_DATETIME = "startDateTime";
-	private static final String KEY_ID = "keyId";
-	private static final String SECRET_TEXT = "secretText";
-	private static final String HINT = "hint";
+    // constants for secret keys
+    private static final String CUSTOM_KEY_IDENTIFIER = "customKeyIdentifier";
+    private static final String END_DATETIME = "endDateTime";
+    private static final String START_DATETIME = "startDateTime";
+    private static final String KEY_ID = "keyId";
+    private static final String SECRET_TEXT = "secretText";
+    private static final String HINT = "hint";
 
-	// constants for certificate data
-	private static final String TYPE = "type";
-	private static final String USAGE = "usage";
-	private static final String KEY = "key";
+    // constants for certificate data
+    private static final String TYPE = "type";
+    private static final String USAGE = "usage";
+    private static final String KEY = "key";
 
-	private static final String API_URL_TEMPLATE = "https://graph.microsoft.com/beta/applications";
-	private static final String TOKEN_TYPE = "Bearer";
+    private static final String API_URL_TEMPLATE = "https://graph.microsoft.com/beta/applications";
+    private static final String TOKEN_TYPE = "Bearer";
 
-	public List<RegisteredApplicationVH> fetchAzureRegisteredApplication() {
-		List<RegisteredApplicationVH> registeredApplicationList = new ArrayList<>();
-		String accessToken;
-		try {
-			accessToken = AzureCredentialManager.getGraphApiAuthToken();
-		} catch (Exception e1) {
-			return registeredApplicationList;
-		}
-		
-		String url = API_URL_TEMPLATE;
-		try {
-			do {
-				String registeredApplicationString = CommonUtils.doHttpGet(url, TOKEN_TYPE, accessToken);
-				JsonObject responseObj = new JsonParser().parse(registeredApplicationString).getAsJsonObject();
-				registeredApplicationList.addAll(createRegisteredApplicationInfo(responseObj.getAsJsonArray(VALUE)));
-				url = responseObj.has(NEXT_DATASET) ? responseObj.get(NEXT_DATASET).getAsString() : null;
-			} while (!StringUtils.isEmpty(url));
-			System.out.println("Registered Application Collected " + registeredApplicationList.size());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println("Error in collecting Registered application list");
-			e.printStackTrace();
-		}
-		return registeredApplicationList;
+    public List<RegisteredApplicationVH> fetchAzureRegisteredApplication() {
+        List<RegisteredApplicationVH> registeredApplicationList = new ArrayList<>();
+        String accessToken;
+        try {
+            accessToken = AzureCredentialManager.getGraphApiAuthToken();
+        } catch (Exception e1) {
+            return registeredApplicationList;
+        }
 
-	}
+        String url = API_URL_TEMPLATE;
+        try {
+            do {
+                String registeredApplicationString = CommonUtils.doHttpGet(url, TOKEN_TYPE, accessToken);
+                JsonObject responseObj = new JsonParser().parse(registeredApplicationString).getAsJsonObject();
+                registeredApplicationList.addAll(createRegisteredApplicationInfo(responseObj.getAsJsonArray(VALUE)));
+                url = responseObj.has(NEXT_DATASET) ? responseObj.get(NEXT_DATASET).getAsString() : null;
+            } while (!StringUtils.isEmpty(url));
+            System.out.println("Registered Application Collected " + registeredApplicationList.size());
+        } catch (Exception e) {
+            System.out.println("Error in collecting Registered application list");
+            e.printStackTrace();
+        }
 
-	/**
-	 * To create the list of register applications fromt the register application json
-	 * @param registeredApplicationJsonArray
-	 * @return
-	 */
-	private List<RegisteredApplicationVH> createRegisteredApplicationInfo(JsonArray registeredApplicationJsonArray) {
+        return registeredApplicationList;
+    }
 
-		List<RegisteredApplicationVH> registeredApplicationList = new ArrayList<>();
-		for (JsonElement registeredApplicationElement : registeredApplicationJsonArray) {
+    /**
+     * To create the list of register applications from the register application json
+     *
+     * @param registeredApplicationJsonArray register application json array
+     * @return list of register application
+     */
+    private List<RegisteredApplicationVH> createRegisteredApplicationInfo(JsonArray registeredApplicationJsonArray) {
 
-			RegisteredApplicationVH registeredApplication = new RegisteredApplicationVH();
-			JsonObject registeredApplicationInfo = registeredApplicationElement.getAsJsonObject();
+        List<RegisteredApplicationVH> registeredApplicationList = new ArrayList<>();
+        for (JsonElement registeredApplicationElement : registeredApplicationJsonArray) {
 
-			registeredApplication.setObjectId(getStringValueforJsonElement(registeredApplicationInfo.get(OBJECT_ID)));
-			registeredApplication.setAppId(getStringValueforJsonElement(registeredApplicationInfo.get(APP_ID)));
-			registeredApplication.setCreatedDateTime(getStringValueforJsonElement(registeredApplicationInfo.get(CREATE_DATETIME)));
-			registeredApplication.setDisplayName(getStringValueforJsonElement(registeredApplicationInfo.get(DISPLAYNAME)));
-			registeredApplication.setPublisherDomain(getStringValueforJsonElement(registeredApplicationInfo.get(PUBLISHERDOMAIN)));
-			registeredApplication.setCertificateList(createRegisterApplicationCertificateList(
-					registeredApplicationInfo.getAsJsonArray(CERTIFICATE_DATA)));
-			registeredApplication.setSecretList(createRegisterApplicationSecretList(
-					registeredApplicationInfo.getAsJsonArray(SECRET_DATA)));
+            RegisteredApplicationVH registeredApplication = new RegisteredApplicationVH();
+            JsonObject registeredApplicationInfo = registeredApplicationElement.getAsJsonObject();
 
-			registeredApplicationList.add(registeredApplication);
-		}
-		return registeredApplicationList;
-	}
+            registeredApplication.setObjectId(getStringValueForJsonElement(registeredApplicationInfo.get(OBJECT_ID)));
+            registeredApplication.setAppId(getStringValueForJsonElement(registeredApplicationInfo.get(APP_ID)));
+            registeredApplication.setCreatedDateTime(getStringValueForJsonElement(registeredApplicationInfo.get(CREATE_DATETIME)));
+            registeredApplication.setDisplayName(getStringValueForJsonElement(registeredApplicationInfo.get(DISPLAYNAME)));
+            registeredApplication.setPublisherDomain(getStringValueForJsonElement(registeredApplicationInfo.get(PUBLISHERDOMAIN)));
+            registeredApplication.setCertificateList(createRegisterApplicationCertificateList(
+                    registeredApplicationInfo.getAsJsonArray(CERTIFICATE_DATA)));
+            registeredApplication.setSecretList(createRegisterApplicationSecretList(
+                    registeredApplicationInfo.getAsJsonArray(SECRET_DATA)));
 
-	/**
-	 * to create the register application certificate list for an application from the certificate json
-	 * @param certificateJsonArray
-	 * @return
-	 */
-	private List<RegAppCertificateVH> createRegisterApplicationCertificateList(JsonArray certificateJsonArray) {
-		List<RegAppCertificateVH> regAppCertificateList = new ArrayList<>();
-		
-		for (JsonElement certificateJsonElement : certificateJsonArray) {
-			RegAppCertificateVH regAppCertificate = new RegAppCertificateVH();
-			JsonObject regAppCertificateJsonObject = certificateJsonElement.getAsJsonObject();
-			
-			regAppCertificate.setCustomKeyIdentifier(getStringValueforJsonElement(regAppCertificateJsonObject.get(CUSTOM_KEY_IDENTIFIER)));
-			regAppCertificate.setEndDateTime(getStringValueforJsonElement(regAppCertificateJsonObject.get(END_DATETIME)));
-			regAppCertificate.setDisplayName(getStringValueforJsonElement(regAppCertificateJsonObject.get(DISPLAYNAME)));
-			regAppCertificate.setKey(getStringValueforJsonElement(regAppCertificateJsonObject.get(KEY)));
-			regAppCertificate.setStartDateTime(getStringValueforJsonElement(regAppCertificateJsonObject.get(START_DATETIME)));
-			regAppCertificate.setType(getStringValueforJsonElement(regAppCertificateJsonObject.get(TYPE)));
-			regAppCertificate.setUsage(getStringValueforJsonElement(regAppCertificateJsonObject.get(USAGE)));
-			regAppCertificate.setKeyId(getStringValueforJsonElement(regAppCertificateJsonObject.get(KEY_ID)));
-			
-			regAppCertificateList.add(regAppCertificate);
-		}
+            registeredApplicationList.add(registeredApplication);
+        }
 
-		return regAppCertificateList;
-	}
+        return registeredApplicationList;
+    }
 
-	/**
-	 * to create the register application secret list for an application from the secret json
-	 * @param secretJsonArray
-	 * @return
-	 */
-	private List<RegAppSecretVH> createRegisterApplicationSecretList(JsonArray secretJsonArray) {
-		List<RegAppSecretVH> regAppSecretList = new ArrayList<>();
+    /**
+     * to create the register application certificate list for an application from the certificate json
+     *
+     * @param certificateJsonArray certificate json array
+     * @return app certificate list
+     */
+    private List<RegAppCertificateVH> createRegisterApplicationCertificateList(JsonArray certificateJsonArray) {
+        List<RegAppCertificateVH> regAppCertificateList = new ArrayList<>();
 
-		for (JsonElement secretJsonElement : secretJsonArray) {
-			RegAppSecretVH regAppSecret = new RegAppSecretVH();
-			JsonObject regAppSecretJsonObject = secretJsonElement.getAsJsonObject();
-			
-			regAppSecret.setCustomKeyIdentifier(getStringValueforJsonElement(regAppSecretJsonObject.get(CUSTOM_KEY_IDENTIFIER)));
-			regAppSecret.setDisplayName(getStringValueforJsonElement(regAppSecretJsonObject.get(DISPLAYNAME)));
-			regAppSecret.setEndDateTime(getStringValueforJsonElement(regAppSecretJsonObject.get(END_DATETIME)));
-			regAppSecret.setHint(getStringValueforJsonElement(regAppSecretJsonObject.get(HINT)));
-			regAppSecret.setKeyId(getStringValueforJsonElement(regAppSecretJsonObject.get(KEY_ID)));
-			regAppSecret.setSecretText(getStringValueforJsonElement(regAppSecretJsonObject.get(SECRET_TEXT)));
-			regAppSecret.setStartDateTime(getStringValueforJsonElement(regAppSecretJsonObject.get(START_DATETIME)));
-			
-			regAppSecretList.add(regAppSecret);
-		}
-		return regAppSecretList;
-	}
-	
-	private String getStringValueforJsonElement (JsonElement jsonElement) {
-		return jsonElement.isJsonNull() ? null : jsonElement.getAsString();
-	}
+        for (JsonElement certificateJsonElement : certificateJsonArray) {
+            RegAppCertificateVH regAppCertificate = new RegAppCertificateVH();
+            JsonObject regAppCertificateJsonObject = certificateJsonElement.getAsJsonObject();
+
+            regAppCertificate.setCustomKeyIdentifier(getStringValueForJsonElement(regAppCertificateJsonObject.get(CUSTOM_KEY_IDENTIFIER)));
+            regAppCertificate.setEndDateTime(getStringValueForJsonElement(regAppCertificateJsonObject.get(END_DATETIME)));
+            regAppCertificate.setDisplayName(getStringValueForJsonElement(regAppCertificateJsonObject.get(DISPLAYNAME)));
+            regAppCertificate.setKey(getStringValueForJsonElement(regAppCertificateJsonObject.get(KEY)));
+            regAppCertificate.setStartDateTime(getStringValueForJsonElement(regAppCertificateJsonObject.get(START_DATETIME)));
+            regAppCertificate.setType(getStringValueForJsonElement(regAppCertificateJsonObject.get(TYPE)));
+            regAppCertificate.setUsage(getStringValueForJsonElement(regAppCertificateJsonObject.get(USAGE)));
+            regAppCertificate.setKeyId(getStringValueForJsonElement(regAppCertificateJsonObject.get(KEY_ID)));
+
+            regAppCertificateList.add(regAppCertificate);
+        }
+
+        return regAppCertificateList;
+    }
+
+    /**
+     * to create the register application secret list for an application from the secret json
+     *
+     * @param secretJsonArray secret json array
+     * @return app secret list
+     */
+    private List<RegAppSecretVH> createRegisterApplicationSecretList(JsonArray secretJsonArray) {
+        List<RegAppSecretVH> regAppSecretList = new ArrayList<>();
+
+        for (JsonElement secretJsonElement : secretJsonArray) {
+            RegAppSecretVH regAppSecret = new RegAppSecretVH();
+            JsonObject regAppSecretJsonObject = secretJsonElement.getAsJsonObject();
+
+            regAppSecret.setCustomKeyIdentifier(getStringValueForJsonElement(regAppSecretJsonObject.get(CUSTOM_KEY_IDENTIFIER)));
+            regAppSecret.setDisplayName(getStringValueForJsonElement(regAppSecretJsonObject.get(DISPLAYNAME)));
+            regAppSecret.setEndDateTime(getStringValueForJsonElement(regAppSecretJsonObject.get(END_DATETIME)));
+            regAppSecret.setHint(getStringValueForJsonElement(regAppSecretJsonObject.get(HINT)));
+            regAppSecret.setKeyId(getStringValueForJsonElement(regAppSecretJsonObject.get(KEY_ID)));
+            regAppSecret.setSecretText(getStringValueForJsonElement(regAppSecretJsonObject.get(SECRET_TEXT)));
+            regAppSecret.setStartDateTime(getStringValueForJsonElement(regAppSecretJsonObject.get(START_DATETIME)));
+
+            regAppSecretList.add(regAppSecret);
+        }
+
+        return regAppSecretList;
+    }
+
+    private String getStringValueForJsonElement(JsonElement jsonElement) {
+        return jsonElement.isJsonNull() ? null : jsonElement.getAsString();
+    }
 }
