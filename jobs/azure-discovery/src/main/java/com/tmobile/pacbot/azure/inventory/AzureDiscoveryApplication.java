cc94ac4a9fea41b7164ccabc154f5802f2e84089
ershad-zemoso <ershad.tj@zemosolabs.com>
2024-01-24 19:23:43 +0530

    fix:[CORE-1881] EnableCaching needs proxying through interface

--- jobs/azure-discovery/src/main/java/com/tmobile/pacbot/azure/inventory/AzureDiscoveryApplication.java
+++ jobs/azure-discovery/src/main/java/com/tmobile/pacbot/azure/inventory/AzureDiscoveryApplication.java
@@ -1,13 +1,18 @@
 package com.tmobile.pacbot.azure.inventory;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
 
 @Configuration
+@EnableCaching
 @ComponentScan({"com.tmobile.pacbot.azure.inventory", "com.tmobile.pacman.commons.database", "com.tmobile.pacman.commons.secrets"})
 public class AzureDiscoveryApplication {
 
@@ -16,6 +21,11 @@ public class AzureDiscoveryApplication {
         AzureFetchOrchestrator orchestrator = context.getBean(AzureFetchOrchestrator.class);
 
         return orchestrator.orchestrate();
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("PolicyDefinitionVH");
     }
 }
 
