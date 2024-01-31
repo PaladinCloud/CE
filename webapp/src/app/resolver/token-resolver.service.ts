b192400243934dc7f7237e9f44fd7fb4a94ea1c0
JaysinhGo <jay@paladincloud.io>
2024-01-31 10:50:26 +0530

    Removed token logs and added loading image

--- webapp/src/app/resolver/token-resolver.service.ts
+++ webapp/src/app/resolver/token-resolver.service.ts
@@ -6,56 +6,49 @@ import { Location } from '@angular/common';
 import { AwsCognitoService } from './../core/services/aws-cognito.service';
 import { DataCacheService } from '../core/services/data-cache.service';
 
-
 @Injectable()
 export class TokenResolverService implements Resolve<any> {
+    constructor(
+        private location: Location,
+        private awsCognitoService: AwsCognitoService,
+        private dataCacheService: DataCacheService,
+    ) {}
 
-  constructor(private location: Location,
-    private awsCognitoService: AwsCognitoService,
-    private dataCacheService: DataCacheService) { }
+    resolve(): Observable<any | null> {
+        const urlParams: URLSearchParams = new URLSearchParams(window.location.search);
+        const code: string = urlParams.get('code');
 
-  resolve(): Observable<any | null> {
+        if (!code) {
+            return of(null);
+        }
 
-    const urlParams: URLSearchParams = new URLSearchParams(window.location.search);
-    const code: string = urlParams.get('code');
-
-    if (!code) {
-      return of(null);
+        return this.getTokenDetailsFromCognito(code).pipe(
+            finalize(() => {
+                this.location.replaceState(window.location.pathname);
+            }),
+        );
     }
 
-    return this.getTokenDetailsFromCognito(code).pipe(
-      finalize(() => {
-        this.location.replaceState(window.location.pathname);
-      })
-    );
-  }
+    getTokenDetailsFromCognito(code: string): Observable<any | null> {
+        return this.awsCognitoService.getTokenDetailsFromCognito(code).pipe(
+            switchMap((response: any) => {
+                let currentUserLoginDetails = {
+                    access_token: response.access_token,
+                    refresh_token: response.refresh_token,
+                    id_token: response.id_token,
+                    success: true,
+                    token_type: response.token_type,
+                    expires_in: response.expires_in,
+                };
 
-  getTokenDetailsFromCognito(code: string): Observable<any | null> {
-    console.log('Fething token from cognito, code is', code);
-    return this.awsCognitoService.getTokenDetailsFromCognito(code).pipe(
-      switchMap((response: any) => {
-
-        let currentUserLoginDetails =
-        {
-          "access_token": response.access_token,
-          "refresh_token": response.refresh_token,
-          "id_token": response.id_token,
-          "success": true,
-          "token_type": response.token_type,
-          "expires_in": response.expires_in
-        }
-
-        this.dataCacheService.setCurrentUserLoginDetails(JSON.stringify(currentUserLoginDetails));
-        if (response) {
-          console.log('Recieved response in token resolver');
-        }
-
-        return of(response);
-      }),
-      catchError((error) => {
-        return error;
-      })
-    );
-
-  }
+                this.dataCacheService.setCurrentUserLoginDetails(
+                    JSON.stringify(currentUserLoginDetails),
+                );
+                return of(response);
+            }),
+            catchError((error) => {
+                return error;
+            }),
+        );
+    }
 }
