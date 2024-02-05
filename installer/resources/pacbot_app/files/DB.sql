9beff1c95db9c7f12207c61956dffb7cfa5249d5
Nithin R <nithin.r@paladincloud.io>
2024-02-05 16:42:13 +0530

    chore:testing

--- installer/resources/pacbot_app/files/DB.sql
+++ installer/resources/pacbot_app/files/DB.sql
@@ -3278,3 +3278,5 @@ UPDATE cf_Target SET displayName='IAM User' WHERE targetName in ('iamuser','iamu
 UPDATE cf_Target SET displayName='KMS Key' WHERE targetName in ('kms','kmskey');
 UPDATE cf_Target SET displayName='Subnet' WHERE targetName in ('subnet','subnets');
 UPDATE cf_Target SET displayName='VM' WHERE targetName in ('virtualmachine','vminstance');
+
+DELETE IGNORE FROM pac_config_properties where cfkey  in ('apiauthinfo');
