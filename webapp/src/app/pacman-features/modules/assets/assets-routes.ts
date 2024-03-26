cbad1b4902cfce198770955a82a1cca8be98b100
sidharthjain-zemoso <sidharth.jain@zemosolabs.com>
2024-03-26 17:22:15 +0530

    refact: cleaned up assets module

--- webapp/src/app/pacman-features/modules/assets/assets-routes.ts
+++ webapp/src/app/pacman-features/modules/assets/assets-routes.ts
@@ -1,10 +1,8 @@
-import { PermissionGuardService } from 'src/app/core/services/permission-guard.service';
 import { AuthGuardService } from 'src/app/shared/services/auth-guard.service';
 import { AssetDashboardComponent } from './asset-dashboard/asset-dashboard.component';
 import { AssetDetailsComponent } from './asset-details/asset-details.component';
 import { AssetDistributionComponent } from './asset-distribution/asset-distribution.component';
 import { AssetListComponent } from './asset-list/asset-list.component';
-import { OnpremAssetsComponent } from './onprem-assets/onprem-assets.component';
 
 export const ASSETS_ROUTES = [
     {
@@ -39,13 +37,4 @@ export const ASSETS_ROUTES = [
         },
         canActivate: [AuthGuardService],
     },
-    {
-        path: 'update-assets',
-        component: OnpremAssetsComponent,
-        canActivate: [AuthGuardService, PermissionGuardService],
-        data: {
-            title: 'Update Asset Data',
-            roles: ['ROLE_ONPREM_ADMIN'],
-        },
-    },
-];
+  ];
