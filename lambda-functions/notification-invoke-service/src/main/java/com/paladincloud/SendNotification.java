0f17ed4ef86c03ccb86ececa48e8dfddb2424e73
Kevin Tavog <165935671+kevin-paladin@users.noreply.github.com>
2026-01-06 12:48:36 -0800

    Send all events, don't bail on errors
    Ensure each item in the list is processed standalone and won't have any impact on other items in the list.
    Ensure each record is processed, not only the first record

--- lambda-functions/notification-invoke-service/src/main/java/com/paladincloud/SendNotification.java
+++ lambda-functions/notification-invoke-service/src/main/java/com/paladincloud/SendNotification.java
@@ -6,65 +6,80 @@ import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.sns.model.PublishResult;
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.util.List;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-import java.util.Map;
 
-
-public class SendNotification implements RequestHandler<Map<String,Object>, String> {
+public class SendNotification implements RequestHandler<Map<String, Object>, String> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SendNotification.class);
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     /**
-     *Below method places all incoming notification requests into topic which are consumed
-     * by subsequent lambda functions.
-     *
+     * Below method places all incoming notification requests into topic which are consumed by
+     * subsequent lambda functions.
      */
     @Override
-    public String handleRequest(Map<String,Object> event, Context context)
-    {
-        String response = new String("hello paladin cloud");
-        LOGGER.info("EVENT: " + gson.toJson(event));
+    public String handleRequest(Map<String, Object> event, Context context) {
+        String response = "hello paladin cloud";
+        LOGGER.info("EVENT: {}", gson.toJson(event));
         AmazonSNS client = AmazonSNSClientBuilder.standard().build();
         try {
             String snsTopicArn = System.getenv("SNS_TOPIC_ARN");
-            String payloadString = (String)event.get("body");
-            if(payloadString==null || payloadString.length()==0){
+            String payloadString = (String) event.get("body");
+            if (payloadString == null || payloadString.isEmpty()) {
                 String jsonStr = gson.toJson(event);
-                LOGGER.info("jsonStr -- "+jsonStr);
-                JsonParser jsonParser = new JsonParser();
-                JsonObject policyParamsJson = (JsonObject) jsonParser.parse(jsonStr);
-                JsonElement bodyJson = policyParamsJson.getAsJsonArray("Records").get(0);
-                LOGGER.info("bodyJson--"+bodyJson.toString());
-                payloadString = bodyJson.getAsJsonObject().get("Sns").getAsJsonObject().get("Message").getAsString();
-            }
+                LOGGER.info("jsonStr -- {}", jsonStr);
+                JsonObject policyParamsJson = (JsonObject) JsonParser.parseString(jsonStr);
+                JsonArray records = policyParamsJson.getAsJsonArray("Records");
 
-            Object payloadObj = gson.fromJson(payloadString,Object.class);
-            if(payloadObj instanceof List){
-                List<Object> notificationRequestList = gson.fromJson(payloadString,List.class);
-                notificationRequestList.stream().forEach(obj -> {
-                    String notificationRequestStr = gson.toJson(obj);
-                    LOGGER.info("notificationsrequeststr for list---"+notificationRequestStr);
-                    PublishRequest request = new PublishRequest(snsTopicArn,notificationRequestStr);
-                    PublishResult result = client.publish(request);
-                    LOGGER.info("message sent with id "+result.getMessageId());
+                records.forEach(body -> {
+                    String payload = body.getAsJsonObject()
+                        .get("Sns").getAsJsonObject()
+                        .get("Message").getAsString();
+                    sendPayload(payload, client, snsTopicArn);
                 });
+            } else {
+                sendPayload(payloadString, client, snsTopicArn);
             }
-            else{
-
-                LOGGER.info("notificationsrequeststr for single event---"+payloadString);
-                PublishRequest request = new PublishRequest(snsTopicArn,payloadString);
-                PublishResult result = client.publish(request);
-                LOGGER.info("message sent with id "+result.getMessageId());
-            }
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
         return response;
     }
+
+    private void sendPayload(String payloadString, AmazonSNS client, String snsTopicArn) {
+        Object payloadObj = gson.fromJson(payloadString, Object.class);
+        if (payloadObj instanceof List) {
+            List<Object> notificationRequestList = gson.fromJson(payloadString, List.class);
+            notificationRequestList.stream().forEach(obj -> {
+                try {
+                    String notificationRequestStr = gson.toJson(obj);
+                    LOGGER.info("notificationsrequeststr for list---{}", notificationRequestStr);
+                    PublishRequest request = new PublishRequest(snsTopicArn, notificationRequestStr);
+                    PublishResult result = client.publish(request);
+                    LOGGER.info("message sent with id {}", result.getMessageId());
+                } catch (Exception ex) {
+                    LOGGER.error("Failed sending message: {}", obj, ex);
+                }
+            });
+        } else {
+            try {
+                LOGGER.info("notificationsrequeststr for single event---{}", payloadString);
+                PublishRequest request = new PublishRequest(snsTopicArn, payloadString);
+                PublishResult result = client.publish(request);
+                LOGGER.info("message sent with id {}", result.getMessageId());
+            } catch (Exception ex) {
+                LOGGER.error("Failed sending message: {}", payloadString, ex);
+            }
+        }
     }
+}
