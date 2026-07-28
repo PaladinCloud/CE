c7e459c717d92de9a4ef5018979b797060326910
Kevin Tavog <165935671+kevin-paladin@users.noreply.github.com>
2026-07-28 10:28:37 -0700

    Add a first pass at full rule exemptions

--- jobs/pacman-rule-engine-2.0/src/main/java/com/tmobile/pacman/service/AutoExemptions.java
+++ jobs/pacman-rule-engine-2.0/src/main/java/com/tmobile/pacman/service/AutoExemptions.java
@@ -1,5 +1,8 @@
 package com.tmobile.pacman.service;
 
+import com.google.gson.Gson;
+import lombok.Getter;
+
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -9,6 +12,7 @@ public class AutoExemptions {
     static public final String PARAMS_EXPIRE_DATE_FIELD = "exemptionexpireddate";
     static public final String PARAMS_REASON_FIELD = "exemptionReason";
     static public final String PARAMS_ACCOUNTS_FIELD = "exemptionAccounts";
+    static public final String PARAMS_RULES_FIELD = "exemptionRules";
 
 
     static public Rule ruleFromPolicyParams(Map<String, String> params) {
@@ -17,65 +21,128 @@ public class AutoExemptions {
         String expireDate = params.getOrDefault(PARAMS_EXPIRE_DATE_FIELD, null);
         String reason = params.getOrDefault(PARAMS_REASON_FIELD, "");
 
-        String accountsStr = params.getOrDefault(PARAMS_ACCOUNTS_FIELD, "");
-        List<String> accounts = Arrays.asList(accountsStr.split(",")).stream()
-                .map(s -> s.trim())
-                .collect(Collectors.toList());
+        // If both accounts and rules field are set, default to using the rules
+        String rulesStr = params.getOrDefault(PARAMS_RULES_FIELD, null);
+        if (rulesStr != null) {
+            // json array, each entry is a clause - any clause that's true is a match.
+            // within a clause, each property must be true
+            Gson gson = new Gson();
+            List<Map<String, Object>> clauses = gson.fromJson(rulesStr, List.class);
+            return Rule.createRulesWithClauses(
+                    Boolean.parseBoolean(enabledStr),
+                    expireDate,
+                    reason,
+                    clauses);
+        } else {
+            String accountsStr = params.getOrDefault(PARAMS_ACCOUNTS_FIELD, "");
+            List<String> accounts = Arrays.stream(accountsStr.split(","))
+                    .map(String::trim)
+                    .collect(Collectors.toList());
 
-        return new Rule(
-                Boolean.parseBoolean(enabledStr),
-                expireDate,
-                reason,
-                accounts);
+            return Rule.createAccountsRule(
+                    Boolean.parseBoolean(enabledStr),
+                    expireDate,
+                    reason,
+                    accounts);
+        }
     }
 
     static public class Rule {
         protected boolean enabled;
+        @Getter
         protected String expireDate;
+        @Getter
         protected String reason;
         protected List<String> accounts;
+        protected List<Map<String, Object>> clauses;
 
-        static private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+        @Getter
+        protected String matchExplanation;
+
+        static private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
         static {
             sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
         }
 
-        public Rule(boolean enabled, String expireDate, String reason, List<String> accounts) {
+        static Rule createAccountsRule(boolean enabled, String expireDate, String reason, List<String> accounts) {
+            Rule rule = new Rule(enabled, expireDate, reason);
+            rule.accounts = accounts;
+            return rule;
+        }
+
+        static Rule createRulesWithClauses(boolean enabled, String expireDate, String reason, List<Map<String, Object>> clauses) {
+            Rule rule = new Rule(enabled, expireDate, reason);
+            rule.clauses = clauses;
+            return rule;
+        }
+
+        private Rule(boolean enabled, String expireDate, String reason) {
             this.enabled = enabled;
             this.expireDate = expireDate;
             this.reason = reason;
-            this.accounts = accounts;
-        }
-
-        public String getReason() {
-            return reason;
-        }
-
-        public String getExpireDate() {
-            return expireDate;
         }
 
         public boolean isExempted(Map<String, String> asset) {
             if (asset == null) {
+                matchExplanation = "Invalid asset";
                 return false;
             }
 
             if (!this.enabled) {
+                matchExplanation = "Rule is disabled";
                 return false;
             }
 
             if (this.expireDate != null && !this.expireDate.isEmpty()) {
                 String today = sdf.format(new Date());
                 if (today.compareTo(this.expireDate) > 0) {
+                    matchExplanation = "Rule has expired";
                     return false;
                 }
             }
 
-            if (accounts.contains(getAccountId(asset))) {
-                return true;
+            if (clauses != null) {
+                for (int idx = 0; idx < clauses.size(); idx++) {
+                    Map<String, Object> clause = clauses.get(idx);
+                    if (doesClauseMatch(clause, asset)) {
+                        matchExplanation = String.format("Matches clause #%d", idx + 1);
+                        return true;
+                    }
+                    matchExplanation = "No clauses matched";
+                }
+            } else if (accounts != null) {
+                String accountId = getAccountId(asset);
+                if (accounts.contains(accountId)) {
+                    matchExplanation = String.format("Matches account %s", accountId);
+                    return true;
+                }
+
+                matchExplanation = "No accounts match";
             }
 
             return false;
+        }
+
+        private boolean doesClauseMatch(Map<String, Object> clause, Map<String, String> asset) {
+            if (clauses.isEmpty()) {
+                return false;
+            }
+
+            for (Map.Entry<String, Object> entry : clause.entrySet()) {
+                Object assetValue = getAssetField(entry.getKey(), asset);
+                if (!entry.getValue().equals(assetValue)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private Object getAssetField(String field, Map<String, String> asset) {
+            if (field.equals("accountid")) {
+                return asset.getOrDefault("account_id", asset.get("accountid"));
+            }
+            return asset.get(field);
         }
 
         private String getAccountId(Map<String, String> asset) {
