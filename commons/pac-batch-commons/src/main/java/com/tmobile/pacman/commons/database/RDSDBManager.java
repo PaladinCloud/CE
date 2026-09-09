8c06b7ebe5e829572932afd35ab925fc6acd450a
Arun Kumar <104547029+arunpaladin@users.noreply.github.com>
2026-09-09 16:15:55 -0700

    fix:[PEA-3222] Fail jobs if database connection fails

--- commons/pac-batch-commons/src/main/java/com/tmobile/pacman/commons/database/RDSDBManager.java
+++ commons/pac-batch-commons/src/main/java/com/tmobile/pacman/commons/database/RDSDBManager.java
@@ -15,29 +15,29 @@ import java.util.*;
 @Component
 public class RDSDBManager {
 
-    /** The Constant dbURL. */
-	@Value("${spring.datasource.url}")
-    private  String dbUrl ;
-	
-	@Value("${spring.datasource.username}")
-    private  String dbUser ;
-	
-	@Value("${spring.datasource.password}")
-	private  String dbPassword ;
-	
+    /**
+     * The Constant dbURL.
+     */
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUser;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
     private static Logger log = LoggerFactory.getLogger(RDSDBManager.class);
-    
-    
+
+
     /**
      * Gets the connection.
      *
      * @return the connection
-     * @throws ClassNotFoundException
-     *             the class not found exception
-     * @throws SQLException
-     *             the SQL exception
+     * @throws ClassNotFoundException the class not found exception
+     * @throws SQLException           the SQL exception
      */
-    private Connection getConnection() throws  SQLException {
+    private Connection getConnection() throws SQLException {
         Connection conn = null;
         Properties props = new Properties();
         props.setProperty("user", dbUser);
@@ -50,70 +50,76 @@ public class RDSDBManager {
     /**
      * Execute query.
      *
-     * @param query
-     *            the query
+     * @param query the query
      * @return the list
      */
     public List<Map<String, String>> executeQuery(String query) {
         List<Map<String, String>> results = new ArrayList<>();
-        try(
-            Connection conn = getConnection();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);){
-            ResultSetMetaData rsmd = rs.getMetaData();
-            int columnCount = rsmd.getColumnCount();
-            Map<String, String> data;
-            while (rs.next()) {
-                data = new LinkedHashMap<>();
-                for (int i = 1; i <= columnCount; i++) {
-                    data.put(rsmd.getColumnName(i), rs.getString(i));
+        try (Connection conn = getConnection()) {
+            try (Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery(query)) {
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnCount = rsmd.getColumnCount();
+                Map<String, String> data;
+                while (rs.next()) {
+                    data = new LinkedHashMap<>();
+                    for (int i = 1; i <= columnCount; i++) {
+                        data.put(rsmd.getColumnName(i), rs.getString(i));
+                    }
+                    results.add(data);
                 }
-                results.add(data);
+            } catch (Exception ex) {
+                log.error(ex.getMessage());
             }
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        } 
+        } catch (Exception e) {
+            log.error("Failed getting SQL connection", e);
+            throw new RuntimeException(e);
+        }
         return results;
     }
 
-    public int executeUpdate(String query, List<Object> paramList){
-        try(
-            Connection conn = getConnection();
-            PreparedStatement preparedStatement = conn.prepareStatement(query);){
-            for(int i=0;i<paramList.size();i++){
-                preparedStatement.setObject(i+1,paramList.get(i));
+    public int executeUpdate(String query, List<Object> paramList) {
+        try (Connection conn = getConnection()) {
+            try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+                for (int i = 0; i < paramList.size(); i++) {
+                    preparedStatement.setObject(i + 1, paramList.get(i));
+                }
+                return preparedStatement.executeUpdate();
+            } catch (Exception ex) {
+                log.error(ex.getMessage());
             }
-            return preparedStatement.executeUpdate();
-        } catch (Exception ex) {
-        log.error(ex.getMessage());
-    }
+        } catch (Exception e) {
+            log.error("Failed getting SQL connection", e);
+            throw new RuntimeException(e);
+        }
         return 0;
     }
 
     public List<Map<String, String>> executeQueryWithParam(String query, List<Object> paramList) {
         List<Map<String, String>> results = new ArrayList<>();
-        try(
-                Connection conn = getConnection();
-                PreparedStatement preparedStatement = conn.prepareStatement(query);
-                ){
-            for(int i=0;i<paramList.size();i++){
-                preparedStatement.setObject(i+1,paramList.get(i));
-            }
-            ResultSet rs = preparedStatement.executeQuery();
-            ResultSetMetaData rsmd = rs.getMetaData();
-            int columnCount = rsmd.getColumnCount();
-            Map<String, String> data;
-            while (rs.next()) {
-                data = new LinkedHashMap<>();
-                for (int i = 1; i <= columnCount; i++) {
-                    data.put(rsmd.getColumnName(i), rs.getString(i));
+        try (Connection conn = getConnection()) {
+            try (PreparedStatement preparedStatement = conn.prepareStatement(query)) {
+                for (int i = 0; i < paramList.size(); i++) {
+                    preparedStatement.setObject(i + 1, paramList.get(i));
                 }
-                results.add(data);
+                ResultSet rs = preparedStatement.executeQuery();
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnCount = rsmd.getColumnCount();
+                Map<String, String> data;
+                while (rs.next()) {
+                    data = new LinkedHashMap<>();
+                    for (int i = 1; i <= columnCount; i++) {
+                        data.put(rsmd.getColumnName(i), rs.getString(i));
+                    }
+                    results.add(data);
+                }
+            } catch (Exception ex) {
+                log.error("Error Executing Query", ex);
             }
-        } catch (Exception ex) {
-            log.error("Error Executing Query",ex);
+        } catch (Exception e) {
+            log.error("Failed getting SQL connection", e);
+            throw new RuntimeException(e);
         }
         return results;
     }
-
 }
